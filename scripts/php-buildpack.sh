f2826c7a8e1329ff1f8fd1de2ae85a7d54a607ca
Anthony Emengo <aemengo@pivotal.io>
2015-05-08 17:05:44 -0400

    Add php-buildpack tests to Concourse
    [#94115382]
    
    Signed-off-by: Sai To Yeung <saitoyeung@gmail.com>

---
+++ scripts/php-buildpack.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+set -e
+
+pushd deployments-buildpacks
+  bundle
+  source ./bin/switch $DEPLOYMENT_NAME
+popd
+
+cd php-buildpack
+pip install -r requirements.txt 
+
+./run_tests.sh
+
+for stack in $STACKS; do
+  ../ci-tools/buildpack-build --uncached --stack=$stack --host=$DEPLOYMENT_NAME.cf-app.com
+  ../ci-tools/buildpack-build --cached --stack=$stack --host=$DEPLOYMENT_NAME.cf-app.com
+done
