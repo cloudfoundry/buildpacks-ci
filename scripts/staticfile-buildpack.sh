141a9aa91c6e48f7f0e95c9444ebf0c980675328
Anthony Emengo <aemengo@pivotal.io>
2015-05-12 10:23:56 -0400

    Add staticfile buildpack to concourse
    [#94114698]
    
    Signed-off-by: JT Archie <jtarchie@gmail.com>

---
+++ scripts/staticfile-buildpack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+set -e
+
+pushd deployments-buildpacks
+  bundle
+  source ./bin/switch $DEPLOYMENT_NAME
+popd
+
+cd staticfile-buildpack
+
+for stack in $STACKS; do
+  ../ci-tools/buildpack-build --uncached --stack=$stack --host=$DEPLOYMENT_NAME.cf-app.com
+  ../ci-tools/buildpack-build --cached --stack=$stack --host=$DEPLOYMENT_NAME.cf-app.com
+done
