7cacfe29c08851370844fb96e68486615944beac
JT Archie <jtarchie@gmail.com>
2015-06-08 16:00:41 -0400

    rename buildpack zip file and upload to Pivnet

--- scripts/release/pivnet_fat_buildpack.sh
+++ scripts/release/pivnet_fat_buildpack.sh
@@ -8,9 +8,10 @@ cd buildpack
 export BUNDLE_GEMFILE=cf.Gemfile
 bundle install
 bundle exec buildpack-packager cached --use-custom-manifest=manifest-including-unsupported.yml
+rename 's/([a-z]+)_buildpack/$1-including-unsupported_buildpack/' *_buildpack-cached-v*.zip
 
-# ../ci-tools/upload_to_pivnet \
-  # "$PIVNET_PRODUCT_NAME" \
-  # `cat VERSION` \
-  # *_buildpack-cached-v*.zip \
-  # RECENT_CHANGES < /dev/null
+../ci-tools/upload_to_pivnet \
+  "$PIVNET_PRODUCT_NAME" \
+  `cat VERSION` \
+  *_buildpack-cached-v*.zip \
+  RECENT_CHANGES < /dev/null
