137bfcbe4dd6487f0135097a543464929ae9157a
Matt Horan <mhoran@pivotal.io>
2015-06-10 10:51:58 -0400

    Enable ccache for binary-builder
    Signed-off-by: Colin Jackson <colin.s.jackson90@gmail.com>

--- scripts/binary-builder.sh
+++ scripts/binary-builder.sh
@@ -2,7 +2,19 @@
 
 set -e
 
-cd binary-builder
+apt-get -y install ccache
+export PATH=/usr/lib/ccache:$PATH
+
+if [ -f binary-builder-compiler-cache/ccache.tgz ]; then
+  tar xzf binary-builder-compiler-cache/ccache.tgz
+fi
+
+export CCACHE_DIR=`pwd`/.ccache
+
+pushd binary-builder
 gem install bundler --no-ri --no-rdoc
 bundle install -j4
 bundle exec rspec
+popd
+
+tar czf binary-builder-compiler-cache/ccache.tgz .ccache
