90fa898ca62553809f4f0c12a17c97f1b99646ab
David Morhovich <dmorhovich@pivotal.io>
2015-10-23 11:53:42 -0400

    Missing apt-get update

--- scripts/push-binary.rb
+++ scripts/push-binary.rb
@@ -7,7 +7,7 @@ unless file_path
   exit
 end
 
-`apt-get -y install awscli`
+`apt-get update && apt-get -y install awscli`
 file_name = File.basename(file_path)
 if `aws s3 ls s3://pivotal-buildpacks/concourse-binaries/#{binary_name}/`.include? file_name
   puts "Binary #{file_name} has already been detected on s3. Skipping upload for this file."
