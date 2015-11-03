51d78ca232d575cfd737591030c5bce5ce9f7838
David Morhovich <dmorhovich@pivotal.io>
2015-11-03 15:44:32 -0500

    add SHA256SUM.txt back to finalize buildpack
    Signed-off-by: JT Archie <jtarchie@gmail.com>

--- spec/finalize-buildpack/finalize-buildpack_spec.rb
+++ spec/finalize-buildpack/finalize-buildpack_spec.rb
@@ -7,7 +7,6 @@ describe 'finalize-buildpack task' do
     @tasks = fly("execute -c tasks/finalize-buildpack.yml -i buildpacks-ci=. -i buildpack=./spec/finalize-buildpack -i pivotal-buildpacks-cached=./spec/finalize-buildpack")
     @id = @tasks.split("\n").first.split(' ').last
     puts "finalize-buildpack task id=#{@id}"
-    sleep(5)
   end
 
     it 'should emit shasum in CHANGELOG' do
@@ -23,5 +22,10 @@ describe 'finalize-buildpack task' do
       output = fly("i -b #{@id} -s one-off -- bash -c 'cat /tmp/build/*/tag'")
       version = File.read('./spec/finalize-buildpack/VERSION')
       expect(output).to be == "v#{version}"
+    end
+
+    it 'emits a SHA256.txt file' do
+      output = fly("i -b #{@id} -s one-off -- bash -c 'cat /tmp/build/*/pivotal-buildpacks-cached/*.SHA256SUM.txt'")
+      expect(output).to be == "8965f5f7a2af993f1e0f66a5bf41d5edf0f957368ce7333af6af82dfc8e88c27  staticfile_buildpack-cached-v1.2.1.zip"
     end
 end
