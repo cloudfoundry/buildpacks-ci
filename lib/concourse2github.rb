c3cdba9cde76b3f8012952af99ecf9c09c067058
Min Su Han <glide1@gmail.com>
2015-12-15 12:25:33 -0500

    post comment on tracker story on a success/fail build
    [#109672588]

---
+++ lib/concourse2github.rb
@@ -0,0 +1,32 @@
+require 'rest-client'
+require 'json'
+
+class Concourse2Github
+  def initialize(git_path:, api_token:, project_id:)
+    @project_id = project_id
+    @api_token = api_token
+    @git_path = git_path
+  end
+
+  def story_id
+    @story_id ||= begin
+                    Dir.chdir(@git_path) do
+                      commit = `git show`
+                      match = commit.match(/\[#(\d+)\]/)
+                      match[1] if match
+                    end
+                  end
+  end
+
+  def process!
+    return unless story_id
+
+    RestClient.post(
+      "https://www.pivotracker.com/services/v5/projects/#{@project_id}/stories/#{story_id}/comments",
+      {text: "Concourse pipeline passed: https://buildpacks.ci.cf-app.com/builds/#{ENV['BUILD_ID']}"}.to_json,
+      :content_type => :json,
+      'X-TrackerToken' => @api_token
+    )
+  end
+
+end
