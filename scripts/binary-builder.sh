d2b62c166ed9e1121570a2a98eb526e15fc55aab
JT Archie <jtarchie@gmail.com>
2016-02-10 17:38:20 -0500

    remove cache, again

--- scripts/binary-builder.sh
+++ scripts/binary-builder.sh
@@ -2,15 +2,6 @@
 
 set -ex
 
-apt-get -y install ccache
-export PATH=/usr/lib/ccache:$PATH
-
-if [ -f binary-builder-compiler-cache/ccache.tgz ]; then
-  tar xzf binary-builder-compiler-cache/ccache.tgz
-fi
-
-export CCACHE_DIR=`pwd`/.ccache
-
 pushd binary-builder
 
 ls spec/**/*_spec.rb | split -l $(expr `ls spec/**/*_spec.rb | wc -l` / $TOTAL_GROUPS)
@@ -20,5 +11,3 @@ gem install bundler --no-ri --no-rdoc
 bundle install -j4
 bundle exec rspec $specs
 popd
-
-tar czf ccache.tgz .ccache
