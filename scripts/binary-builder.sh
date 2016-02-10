dac81bfa3bb4d3ecac3ad7d7e901f1b5c77f7063
JT Archie <jtarchie@gmail.com>
2016-02-10 16:59:06 -0500

    set a task for binary-builder

--- scripts/binary-builder.sh
+++ scripts/binary-builder.sh
@@ -1,20 +1,24 @@
 #!/bin/bash -l
 
-set -e
+set -ex
 
-apt-get -y install ccache
-export PATH=/usr/lib/ccache:$PATH
+# apt-get -y install ccache
+# export PATH=/usr/lib/ccache:$PATH
 
-if [ -f binary-builder-compiler-cache/ccache.tgz ]; then
-  tar xzf binary-builder-compiler-cache/ccache.tgz
-fi
+# if [ -f binary-builder-compiler-cache/ccache.tgz ]; then
+#   tar xzf binary-builder-compiler-cache/ccache.tgz
+# fi
 
-export CCACHE_DIR=`pwd`/.ccache
+# export CCACHE_DIR=`pwd`/.ccache
 
 pushd binary-builder
+
+ls spec/**/*_spec.rb | split -l $(expr `ls spec/**/*_spec.rb | wc -l` / $TOTAL_GROUPS)
+specs=$(cat $(ls x* | sed -n "$CURRENT_GROUP p"))
+
 gem install bundler --no-ri --no-rdoc
 bundle install -j4
-bundle exec rspec
+bundle exec rspec $specs
 popd
 
 tar czf ccache.tgz .ccache
