8970fd7b0426e647aa518dd8e6be119e69ca8c9b
James Wen <jrw2175@columbia.edu>
2016-04-01 11:07:13 -0400

    Create cflinuxfs2-rootfs bosh release blobs and final release commit
    - Upload cflinuxfs2-rootfs bosh release blobs
    - Create cflinuxfs2-rootfs bosh release and commit to github
    
    [#115888451]
    
    Signed-off-by: David Jahn <david.a.jahn@gmail.com>

--- scripts/bosh-releases/create-bosh-release.sh
+++ scripts/bosh-releases/create-bosh-release.sh
@@ -6,27 +6,26 @@ version=$(cat version/number)
 pushd $RELEASE_DIR
 
 if [ -n "${SECRET_ACCESS_KEY:+1}" ]; then
-echo "creating private.yml..."
-cat > config/private.yml <<EOF
+  echo "creating private.yml..."
+  cat > config/private.yml <<EOF
 ---
 blobstore:
   s3:
     access_key_id: $ACCESS_KEY_ID
     secret_access_key: $SECRET_ACCESS_KEY
 EOF
-fi
+  fi
 
-rm -f config/blobs.yml
-bosh -n add blob ../$BLOB_GLOB $BLOB_NAME
-bosh -n upload blobs
+  rm -f config/blobs.yml
+  bosh -n add blob ../$BLOB_GLOB $BLOB_NAME
+  bosh -n upload blobs
 
-git add config/blobs.yml
-git commit -m "Updating blobs for $RELEASE_NAME $version"
+  git add config/blobs.yml
+  git commit -m "Updating blobs for $RELEASE_NAME $version"
 
-bosh -n create release --final --version $version --name $RELEASE_NAME --force
-git add releases/**/*-$version.yml releases/**/index.yml
-git commit -m "Final release for $BLOB_NAME at $version"
-
+  bosh -n create release --final --version $version --name $RELEASE_NAME --force
+  git add releases/**/*-$version.yml releases/**/index.yml
+  git commit -m "Final release for $BLOB_NAME at $version"
 popd
 
 rsync -a release/ release-artifacts
---
+++ scripts/bosh-releases/create-cflinuxfs2-rootfs-release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -ex
+
+stacks_version=$(cat version/number)
+bosh_release_version=$(cat cflinuxfs2-rootfs-release-version/number)
+
+pushd $RELEASE_DIR
+  if [ -n "${SECRET_ACCESS_KEY:+1}" ]; then
+  echo "creating private.yml..."
+  cat > config/private.yml <<EOF
+---
+blobstore:
+  s3:
+    access_key_id: $ACCESS_KEY_ID
+    secret_access_key: $SECRET_ACCESS_KEY
+EOF
+  fi
+
+  rm -f config/blobs.yml
+  bosh -n add blob ../$BLOB_GLOB $BLOB_NAME
+  bosh -n upload blobs
+
+  git add config/blobs.yml
+  git commit -m "Updating blobs for $RELEASE_NAME bosh release version $bosh_release_version"
+
+  bosh -n create release --final --version $bosh_release_version --name $RELEASE_NAME --force
+  git add releases/**/*-$bosh_release_version.yml releases/**/index.yml
+  git commit -m "Final $RELEASE_NAME bosh release version $bosh_release_version, containing cflinuxfs2 version $stacks_version"
+popd
+
+rsync -a release/ release-artifacts
