6686a6cf9957aeaaa052eaf4212454e9d01c5ec8
John Shahid <jvshahid@gmail.com>
2016-04-04 18:20:21 -0400

    create a release tarball

--- scripts/bosh-releases/create-cflinuxfs2-rootfs-release.sh
+++ scripts/bosh-releases/create-cflinuxfs2-rootfs-release.sh
@@ -23,7 +23,7 @@ EOF
   git add config/blobs.yml
   git commit -m "Updating blobs for $RELEASE_NAME bosh release version $bosh_release_version"
 
-  bosh -n create release --final --version $bosh_release_version --name $RELEASE_NAME --force
+  bosh -n create release --final --version $bosh_release_version --name $RELEASE_NAME --with-tarball
   git add releases/**/*-$bosh_release_version.yml releases/**/index.yml
   git commit -m "Final $RELEASE_NAME bosh release version $bosh_release_version, containing cflinuxfs2 version $stacks_version"
 popd
