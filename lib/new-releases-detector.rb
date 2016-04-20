e7c27662d75d094f5374838b207fac58bf20098b
James Wen <jrw2175@columbia.edu>
2016-04-20 15:32:20 -0400

    Add specific updated dependency versions to Tracker Story title
    - Refactor NewReleasesDetector to post to tracker and slack
    - Now posts to slack once for each new version
    
    [#117441295]
    
    Signed-off-by: Gabriel Ramirez <gabriel.e.ramirez@gmail.com>

--- lib/new-releases-detector.rb
+++ lib/new-releases-detector.rb
@@ -4,15 +4,61 @@ require 'octokit'
 require 'open-uri'
 require 'yaml'
 
+buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+require "#{buildpacks_ci_dir}/lib/slack-client"
+require "#{buildpacks_ci_dir}/lib/tracker-client"
+
 class NewReleasesDetector
   attr_reader :new_releases_dir
+  attr_reader :dependency_tags
 
   def initialize(new_releases_dir)
     @new_releases_dir = new_releases_dir
+    @dependency_tags = generate_dependency_tags(new_releases_dir)
   end
 
-  def perform!
+  def post_to_slack
+    slack_client = SlackClient.new(
+      ENV['SLACK_WEBHOOK'],
+      ENV['SLACK_CHANNEL'],
+      'dependency-notifier'
+    )
+    dependency_tags.each do |dependency, versions|
+      versions.each do |version|
+        new_dependency_version_output = "There is a new update to the *#{dependency}* dependency: version *#{version}*\n"
+        slack_client.post_to_slack new_dependency_version_output
+      end
+    end
+  end
+
+  def post_to_tracker
+    tracker_client = TrackerClient.new(
+      ENV['TRACKER_API_TOKEN'],
+      ENV['TRACKER_PROJECT_ID'],
+      ENV['TRACKER_REQUESTER_ID'].to_i
+    )
+    dependency_tags.each do |dependency, versions|
+      tracker_story_title = "Build and/or Include new releases: #{dependency} #{versions.join(', ')}"
+      tracker_story_description = "We have #{versions.count} new releases for **#{dependency}**:\n**version #{versions.join(', ')}**\n See the google doc https://sites.google.com/a/pivotal.io/cloudfoundry-buildpacks/check-lists/adding-a-new-dependency-release-to-a-buildpack for info on building a new release binary and adding it to the buildpack manifest file."
+
+      tracker_client.post_to_tracker tracker_story_title, tracker_story_description
+    end
+  end
+
+  private
+
+  def configure_octokit
+    Octokit.auto_paginate = true
+    Octokit.configure do |c|
+      c.login    = ENV.fetch('GITHUB_USERNAME')
+      c.password = ENV.fetch('GITHUB_PASSWORD')
+    end
+  end
+
+  def generate_dependency_tags(new_releases_dir)
     configure_octokit
+    dependency_tags = {}
+
     tags.each do |current_dependency, get_tags|
       current_tags = get_tags.call
 
@@ -26,26 +72,13 @@ class NewReleasesDetector
       diff_tags = current_tags - previous_tags
 
       if diff_tags.any?
+        dependency_tags[current_dependency] = diff_tags
         File.write(filename, current_tags.to_yaml)
-        puts "There are *#{diff_tags.length}* new updates to the *#{current_dependency}* dependency:\n"
-        diff_tags.each do |tag|
-          puts "- version *#{tag}*\n"
-        end
-
       else
         warn "There are no new updates to the *#{current_dependency}* dependency"
       end
     end
-  end
-
-  private
-
-  def configure_octokit
-    Octokit.auto_paginate = true
-    Octokit.configure do |c|
-      c.login    = ENV.fetch('GITHUB_USERNAME')
-      c.password = ENV.fetch('GITHUB_PASSWORD')
-    end
+    dependency_tags
   end
 
   def tags
--- spec/lib/new-releases-detector_spec.rb
+++ spec/lib/new-releases-detector_spec.rb
@@ -4,12 +4,26 @@ require 'tmpdir'
 require_relative '../../lib/new-releases-detector'
 
 describe NewReleasesDetector do
-  before { allow(subject).to receive(:warn) {} }
-
   def to_tags(names)
     names.collect { |n| OpenStruct.new(name: n) }
   end
   let(:new_releases_dir) { Dir.mktmpdir }
+  let(:openjdk_yaml_filename) { "#{new_releases_dir}/openjdk.yaml" }
+  let(:github_username) { 'github_username' }
+  let(:github_password) { 'github_password1!' }
+
+  before do
+    allow_any_instance_of(described_class).to receive(:warn) {}
+    allow(Octokit).to receive(:configure)
+    allow(Octokit).to receive(:tags).and_return([])
+    allow_any_instance_of(described_class).to receive(:open).with(/python/).and_return(double(read: { 'tags' => [] }.to_json))
+    allow_any_instance_of(described_class).to receive(:open).with(/openjdk/).and_return(double(read: {}.to_yaml))
+    allow(File).to receive(:exist?).and_call_original
+    allow(File).to receive(:write).and_call_original
+
+    allow(ENV).to receive(:fetch).with('GITHUB_USERNAME').and_return(github_username)
+    allow(ENV).to receive(:fetch).with('GITHUB_PASSWORD').and_return(github_password)
+  end
 
   subject { described_class.new(new_releases_dir) }
 
@@ -17,130 +31,113 @@ describe NewReleasesDetector do
     it 'sets new_releases_dir' do
       expect(subject.new_releases_dir).to eq(new_releases_dir)
     end
-  end
 
-  context '#perform!' do
-    let(:github_username) { 'github_username' }
-    let(:github_password) { 'github_password1!' }
-
-    before do
-      allow(Octokit).to receive(:tags).and_return([])
-      allow(subject).to receive(:open).with(/python/).and_return(double(read: { 'tags' => [] }.to_json))
-      allow(subject).to receive(:open).with(/openjdk/).and_return(double(read: {}.to_yaml))
-
-      allow(ENV).to receive(:fetch).with('GITHUB_USERNAME').and_return(github_username)
-      allow(ENV).to receive(:fetch).with('GITHUB_PASSWORD').and_return(github_password)
-    end
-
-    context 'configures Octokit' do
+    context 'when there are new releases' do
       before do
-      end
-
-      it 'sets the autopaginate to true' do
-        expect(Octokit).to receive(:auto_paginate=).with(true).at_least(:once)
-        subject.perform!
-      end
-    end
-
-    context 'for python' do
-      it 'outputs new releases' do
-        expect(subject).to receive(:open)
-          .with('https://hg.python.org/cpython/json-tags')
-          .and_return(double(read: { tags: [{ tag: 'a' }, { tag: 'b' }] }.to_json))
-
-        expect do
-          subject.perform!
-        end.to output("There are *2* new updates to the *python* dependency:\n" \
-                    "- version *a*\n" \
-                    "- version *b*\n"
-                     ).to_stdout
-      end
-
-      context 'when there are no new releases' do
-        before { allow(subject).to receive(:warn).and_call_original }
-
-        it 'outputs there are no new updates' do
-          expect(subject).to receive(:open)
-            .with('https://hg.python.org/cpython/json-tags')
-            .and_return(double(read: { tags: [{ tag: 'a' }, { tag: 'b' }] }.to_json))
-
-          subject.perform!
-
-          expect do
-            subject.perform!
-          end.to output(/There are no new updates to the \*python\* dependency\n/).to_stderr
-        end
-      end
-    end
-
-    context 'for openjdk' do
-      it 'outputs new releases' do
-        expect(subject).to receive(:open)
+        allow(File).to receive(:exist?).with(openjdk_yaml_filename).and_return(true)
+        allow(YAML).to receive(:load_file).with(openjdk_yaml_filename).and_return(['v1', 'v2'])
+        allow_any_instance_of(described_class).to receive(:open)
           .with('https://download.run.pivotal.io/openjdk/trusty/x86_64/index.yml')
-          .and_return(double(read: { v1: 1, v2: 2 }.to_yaml))
+          .and_return(double(read: { 'v1' => 1, 'v2' => 2, 'v3' => 3, 'v4' => 4 }.to_yaml))
+        python_yaml_filename = "#{new_releases_dir}/python.yaml"
+        allow(File).to receive(:exist?).with(python_yaml_filename).and_return(true)
+        allow(YAML).to receive(:load_file).with(python_yaml_filename).and_return(['a', 'b'])
+        allow_any_instance_of(described_class).to receive(:open)
+          .with('https://hg.python.org/cpython/json-tags')
+          .and_return(double(read: { tags: [{ tag: 'a' }, { tag: 'b' }, { tag: 'c'}] }.to_json))
+      end
 
-        expect do
-          subject.perform!
-        end.to output("There are *2* new updates to the *openjdk* dependency:\n" \
-                    "- version *v1*\n" \
-                    "- version *v2*\n"
-                     ).to_stdout
+      it 'sets dependency_tags to a hash of dependencies as keys and array of diffs as values' do
+        expect(subject.dependency_tags).to eq({openjdk: ['v3', 'v4'], python: ['c']})
       end
 
       it 'writes to a file the latest releases' do
-        expect(subject).to receive(:open)
+        expect(File).to receive(:write).with(openjdk_yaml_filename, "---\n- v1\n- v2\n- v3\n- v4\n")
+        subject
+      end
+    end
+
+    context 'when there are no new releases' do
+      before do
+        allow_any_instance_of(described_class).to receive(:warn).and_call_original
+        allow(File).to receive(:exist?).with(openjdk_yaml_filename).and_return(true)
+        allow(YAML).to receive(:load_file).with(openjdk_yaml_filename).and_return(['v1', 'v2'])
+        allow_any_instance_of(described_class).to receive(:open)
           .with('https://download.run.pivotal.io/openjdk/trusty/x86_64/index.yml')
-          .and_return(double(read: { v1: 1, v2: 2 }.to_yaml))
-
-        subject.perform!
-        expect(YAML.load_file("#{new_releases_dir}/openjdk.yaml")).to eq [:v1, :v2]
+          .and_return(double(read: { 'v1' => 1, 'v2' => 2}.to_yaml))
       end
 
-      context 'when there are new releases' do
-        it 'only displays the latest release to STDOUT' do
-          expect(subject).to receive(:open)
-            .with('https://download.run.pivotal.io/openjdk/trusty/x86_64/index.yml')
-            .and_return(double(read: { v1: 1, v2: 2 }.to_yaml))
-
-          subject.perform!
-          expect(subject).to receive(:open)
-            .with('https://download.run.pivotal.io/openjdk/trusty/x86_64/index.yml')
-            .and_return(double(read: { v1: 1, v2: 2, v3: 3 }.to_yaml))
-
-          expect do
-            subject.perform!
-          end.to output("There are *1* new updates to the *openjdk* dependency:\n" \
-                      "- version *v3*\n"
-                       ).to_stdout
-        end
+      it 'sets dependency_tags to an empty hash' do
+        expect(subject.dependency_tags).to eq({})
       end
 
-      context 'when there are no new releases' do
-        before { allow(subject).to receive(:warn).and_call_original }
+      it 'outputs to stderr that there are no new updates' do
+        expect do
+          subject
+        end.to output(/There are no new updates to the \*openjdk\* dependency\n/).to_stderr
+      end
+    end
 
-        it 'outputs there are no new updates' do
-          expect(subject).to receive(:open)
-            .with('https://download.run.pivotal.io/openjdk/trusty/x86_64/index.yml')
-            .and_return(double(read: { v1: 1, v2: 2 }.to_yaml))
+    context 'configures Octokit' do
+      it 'sets the autopaginate to true' do
+        expect(Octokit).to receive(:auto_paginate=).with(true).at_least(:once)
+        subject
+      end
+    end
+  end
 
-          subject.perform!
+  describe '#post_to_slack' do
+    let(:slack_client) { double(:slack_client) }
 
-          expect do
-            subject.perform!
-          end.to output(/There are no new updates to the \*openjdk\* dependency\n/).to_stderr
-        end
+    before do
+      allow(SlackClient).to receive(:new).and_return(slack_client)
+      allow_any_instance_of(described_class).to receive(:generate_dependency_tags).with(new_releases_dir).and_return(dependency_tags)
+    end
 
-        it 'does not changes the contents of the file' do
-          expect(subject).to receive(:open)
-            .with('https://download.run.pivotal.io/openjdk/trusty/x86_64/index.yml')
-            .and_return(double(read: { v1: 1, v2: 2 }.to_yaml))
+    context 'with new versions for a dependency' do
+      let(:dependency_tags) { { python: ['a', 'b']} }
 
-          subject.perform!
+      it 'posts to slack for each new release of that dependency' do
+        expect(slack_client).to receive(:post_to_slack).with("There is a new update to the *python* dependency: version *a*\n")
+        expect(slack_client).to receive(:post_to_slack).with("There is a new update to the *python* dependency: version *b*\n")
+        subject.post_to_slack
+      end
+    end
 
-          expect do
-            subject.perform!
-          end.to_not change { File.read("#{new_releases_dir}/openjdk.yaml") }
-        end
+    context 'with no new versions for a dependency' do
+      let(:dependency_tags) { { } }
+
+      it 'posts to slack for each new release of that dependency' do
+        expect(slack_client).to_not receive(:post_to_slack)
+        subject.post_to_slack
+      end
+    end
+  end
+
+  describe '#post_to_tracker' do
+    let(:tracker_client) { double(:tracker_client) }
+
+    before do
+      allow(TrackerClient).to receive(:new).and_return(tracker_client)
+      allow_any_instance_of(described_class).to receive(:generate_dependency_tags).with(new_releases_dir).and_return(dependency_tags)
+    end
+
+    context 'with new versions for a dependency' do
+      let(:dependency_tags) { { python: ['a', 'b']} }
+
+      it 'post one story to slack with all new releases of that dependency' do
+        expect(tracker_client).to receive(:post_to_tracker).with("Build and/or Include new releases: python a, b",
+                                                                 "We have 2 new releases for **python**:\n**version a, b**\n See the google doc https://sites.google.com/a/pivotal.io/cloudfoundry-buildpacks/check-lists/adding-a-new-dependency-release-to-a-buildpack for info on building a new release binary and adding it to the buildpack manifest file.")
+        subject.post_to_tracker
+      end
+    end
+
+    context 'with no new versions for a dependency' do
+      let(:dependency_tags) { { } }
+
+      it 'posts to slack for each new release of that dependency' do
+        expect(tracker_client).to_not receive(:post_to_tracker)
+        subject.post_to_tracker
       end
     end
   end
