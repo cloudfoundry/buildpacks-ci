a57ff120e74933151616b3942b4931f2a2baabd3
James Wen <jrw2175@columbia.edu>
2016-05-10 16:37:33 -0400

    Remove bin/switch invocation and deployments-buildpacks from buildpack pipeline template
    - We only needed this before for the machete internet traffic matcher,
      but we now use Docker instead of iptables
    
    [#114957289]
    
    Signed-off-by: David Jahn <david.a.jahn@gmail.com>

--- spec/test-buildpack/test-buildpack_spec.rb
+++ spec/test-buildpack/test-buildpack_spec.rb
@@ -6,13 +6,8 @@ describe 'test-buildpack task' do
     execute('-c tasks/test-buildpack.yml ' \
             '-p ' \
             '-i buildpacks-ci=. -i buildpack=./spec/test-buildpack ' \
-            '-i deployments-buildpacks=./spec/test-buildpack ' \
             '-i cf-environments=./spec/test-buildpack',               'DEPLOYMENT_NAME' => 'test',
                                                                       'STACKS' => 'cflinuxfs2')
-  end
-
-  it 'setups the deployment environment' do
-    expect(@output).to include 'DEPLOYMENT NAME: test'
   end
 
   it 'targets a stack and deployment name' do
