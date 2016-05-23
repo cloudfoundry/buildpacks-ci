7fb5e41d2c872add061d48541596624d669e4c97
Sam Smith <sesmith177@gmail.com>
2016-05-23 17:58:24 -0400

    WIP, adding automation of composer to binary-builder pipeline
    [#119926939]
    
    Signed-off-by: Gabriel Ramirez <gabriel.e.ramirez@gmail.com>

--- scripts/build-binary.rb
+++ scripts/build-binary.rb
@@ -15,24 +15,32 @@ builds_yaml_artifacts = File.join(Dir.pwd, 'builds-yaml-artifacts')
 builds_path  = File.join(builds_dir, "#{binary_name}-builds.yml")
 builds       = YAML.load_file(builds_path)
 latest_build = builds[binary_name].shift
+version = builds['version']
 
 unless latest_build
   puts "There are no new builds for #{binary_name} requested."
   exit
 end
 
-flags = "--name=#{binary_name}"
-latest_build.each_pair do |key, value|
-  flags << %( --#{key}="#{value}")
+if binary_name == "composer" then
+  download_url = "https://getcomposer.org/download/#{version}/composer.phar"
+  system("curl #{download_url} -o binary-builder/composer-#{version}.phar")
+  system("cp binary-builder/* binary-builder-artifacts/")
+else
+  flags = "--name=#{binary_name}"
+  latest_build.each_pair do |key, value|
+    flags << %( --#{key}="#{value}")
+  end
+
+  Dir.chdir('binary-builder') do
+    system("./bin/binary-builder #{flags}")
+    system('tar -zcf build.tgz -C /tmp ./x86_64-linux-gnu/')
+  end
+  system('cp binary-builder/*.tgz binary-builder-artifacts/')
 end
 
-Dir.chdir('binary-builder') do
-  system("./bin/binary-builder #{flags}")
-  system('tar -zcf build.tgz -C /tmp ./x86_64-linux-gnu/')
-end
-system('cp binary-builder/*.tgz binary-builder-artifacts/')
-
-filename = Dir["binary-builder/#{binary_name}-*.tgz"].first
+ext = binary_name == "composer" ? "*.phar" : "-*.tgz"
+filename = Dir["binary-builder/#{binary_name + ext}"].first
 md5sum   = Digest::MD5.file(filename).hexdigest
 shasum   = Digest::SHA256.file(filename).hexdigest
 git_msg  = "Build #{binary_name} - #{latest_build['version']}\n\nfilename: #{filename}, md5: #{md5sum}, sha256: #{shasum}"
--- spec/lib/dependency-build-enqueuer_spec.rb
+++ spec/lib/dependency-build-enqueuer_spec.rb
@@ -5,7 +5,6 @@ require_relative '../../lib/buildpack-dependency'
 require 'yaml'
 
 describe DependencyBuildEnqueuer do
-  let(:dependency)               { "godep" }
   let(:new_releases_dir)         { Dir.mktmpdir }
   let(:binary_builds_dir)        { Dir.mktmpdir }
 
@@ -16,37 +15,77 @@ describe DependencyBuildEnqueuer do
     let(:builds_file)              { File.join(binary_builds_dir, "#{dependency}-builds.yml") }
     let(:sha256)                   { "sha256-mocked" }
 
+
+    context "godep" do
+      let(:dependency)               { "godep" }
+      before do
+        godep_versions = %w(v60 v61 v62)
+        File.open(dependency_versions_file, "w") do |file|
+          file.write godep_versions.to_yaml
+        end
+        godep_builds = {godep: []}
+        File.open(builds_file, "w") do |file|
+          file.write godep_builds.to_yaml
+        end
+        allow(described_class).to receive(:build_verification_for).with("godep", "v62").and_return(['sha256', sha256])
+      end
+
+      it "enqueues a build for the latest dep version in the correlated builds yml file" do
+        subject.enqueue_build
+
+        builds = YAML.load_file(builds_file)
+        enqueued_builds = builds['godep']
+        expect(enqueued_builds.count).to eq(1)
+        expect(enqueued_builds.first['version']).to eq("v62")
+        expect(enqueued_builds.first['sha256']).to eq("sha256-mocked")
+      end
+    end
+
+  context "composer" do
+    let(:dependency)               { "composer" }
     before do
-      godep_versions = %w(v60 v61 v62)
+      composer_versions = %w(1.1.0-RC 1.0.3 1.1.1 1.1.1-alpha1)
       File.open(dependency_versions_file, "w") do |file|
-        file.write godep_versions.to_yaml
+        file.write composer_versions.to_yaml
       end
-      godep_builds = {godep: []}
+      composer_builds = {composer: []}
       File.open(builds_file, "w") do |file|
-        file.write godep_builds.to_yaml
+        file.write composer_builds.to_yaml
       end
-      allow(described_class).to receive(:build_verification_for).with("godep", "v62").and_return(['sha256', sha256])
+      allow(described_class).to receive(:build_verification_for).with("composer", "1.1.1").and_return(['sha256', sha256])
     end
 
     it "enqueues a build for the latest dep version in the correlated builds yml file" do
       subject.enqueue_build
 
       builds = YAML.load_file(builds_file)
-      enqueued_builds = builds['godep']
+      enqueued_builds = builds['composer']
       expect(enqueued_builds.count).to eq(1)
-      expect(enqueued_builds.first['version']).to eq("v62")
+      expect(enqueued_builds.first['version']).to eq("1.1.1")
       expect(enqueued_builds.first['sha256']).to eq("sha256-mocked")
     end
   end
+end
 
   describe '#latest_version_for_dependency' do
     context "godep" do
+      let(:dependency)               { "godep" }
       let(:dependency_versions) { %w(v60 v61 v62 v102) }
 
       it 'returns the latest godep version in the passed versions' do
         latest_version = described_class.latest_version_for_dependency(dependency, dependency_versions)
 
         expect(latest_version).to eq("v102")
+      end
+    end
+    context "composer" do
+      let(:dependency)               { "composer" }
+      let(:dependency_versions) { %w(1.1.0-RC 1.0.3 1.1.1 1.1.1-alpha1) }
+
+      it 'returns the latest godep version in the passed versions' do
+        latest_version = described_class.latest_version_for_dependency(dependency, dependency_versions)
+
+        expect(latest_version).to eq("1.1.1")
       end
     end
   end
