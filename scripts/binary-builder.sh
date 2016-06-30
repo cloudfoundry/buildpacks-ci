f35940673540112fba802ecf42fcbd4750397970
Olivier ORAND <olivier.orand@orange.com>
2016-06-30 18:06:52 +0200

    set a rubygem mirror on binary-builder.sh and recreate-bosh-lite script in bosh-lite-cf-edge tasks using it
    A new concourse credentials is required rubygem-mirror.
    To use a mirror, add
       rubygem-mirror: https://gems.bmonkeys.net
    or without mirror, add
       rubygem-mirror: none

--- scripts/binary-builder.sh
+++ scripts/binary-builder.sh
@@ -8,6 +8,10 @@ ls spec/**/*_spec.rb | split -l $(expr `ls spec/**/*_spec.rb | wc -l` / $TOTAL_G
 specs=$(cat $(ls x* | sed -n "$CURRENT_GROUP p"))
 
 gem install bundler --no-ri --no-rdoc
+if [ "${RUBYGEM_MIRROR}" != "none" ]
+then
+    bundle config mirror.https://rubygems.org ${RUBYGEM_MIRROR}
+fi
 bundle install -j4
 bundle exec rspec $specs
 popd
