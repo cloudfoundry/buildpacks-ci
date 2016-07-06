389e53536e6088e4bee4713260e628bd4cd253f5
James Wen <jrw2175@columbia.edu>
2016-07-06 14:09:53 -0400

    Repress git cloning output for concourse filter
    [#120319485]
    
    Signed-off-by: Forest Eckhardt <forest.eckhardt@gmail.com>

--- build/filter.sh
+++ build/filter.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-git clone https://github.com/geramirez/concourse-filter
+git clone https://github.com/geramirez/concourse-filter &>/dev/null
 pushd concourse-filter
-  go build
+  go build &>/dev/null
   CREDENTIAL_FILTER_PARAMS=STACKS,DEPLOYMENT_NAME
   export CREDENTIAL_FILTER_WHITELIST="$CREDENTIAL_FILTER_PARAMS,CREDENTIAL_FILTER_WHITELIST,GEM_HOME,TERM,USER,BUNDLE_APP_CONFIG,PATH,RUBY_DOWNLOAD_SHA256,PWD,LANG,RUBY_MAJOR,RUBYGEMS_VERSION,SHLVL,HOME,RUBY_VERSION,BUNDLER_VERSION,OLDPWD"
   exec &> >(./concourse-filter)
