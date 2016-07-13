4ba66ebbb9f1e14390fca9c6477ceee971fc615f
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-07-13 17:48:21 -0400

    Have automatic manifest updates point dependency urls to buildpacks.cloudfoundry.org
    [#121757949]
    
    Signed-off-by: James Wen <jrw2175@columbia.edu>

--- lib/buildpack-dependency-updater.rb
+++ lib/buildpack-dependency-updater.rb
@@ -39,12 +39,15 @@ class BuildpackDependencyUpdater
   def get_dependency_info
     git_commit_message = GitClient.last_commit_message(binary_builds_dir)
 
+    buildpack_dependencies_host_domain = ENV.fetch('BUILDPACK_DEPENDENCIES_HOST_DOMAIN', nil)
+    raise 'No host domain set via BUILDPACK_DEPENDENCIES_HOST_DOMAIN' unless buildpack_dependencies_host_domain
+
     /.*filename:\s+binary-builder\/(#{dependency}-(.*)-linux-x64.tgz).*md5:\s+(\w*)\,.*/.match(git_commit_message)
     dependency_filename = $1
     dependency_version = $2
     md5 = $3
 
-    url = "https://pivotal-buildpacks.s3.amazonaws.com/concourse-binaries/#{dependency}/#{dependency_filename}"
+    url ="https://#{buildpack_dependencies_host_domain}/concourse-binaries/#{dependency}/#{dependency_filename}"
 
     [dependency_version, url, md5]
   end
@@ -96,9 +99,12 @@ class BuildpackDependencyUpdater::Composer < BuildpackDependencyUpdater
   def get_dependency_info
     git_commit_message = GitClient.last_commit_message(binary_builds_dir)
 
+    buildpack_dependencies_host_domain = ENV.fetch('BUILDPACK_DEPENDENCIES_HOST_DOMAIN', nil)
+    raise 'No host domain set via BUILDPACK_DEPENDENCIES_HOST_DOMAIN' unless buildpack_dependencies_host_domain
+
     dependency_version = git_commit_message[/filename:\s+binary-builder\/composer-([\d\.]*).phar/, 1]
     md5 = git_commit_message[/md5:\s+(\w+)/, 1]
-    url ="https://pivotal-buildpacks.s3.amazonaws.com/php/binaries/trusty/composer/#{dependency_version}/composer.phar"
+    url ="https://#{buildpack_dependencies_host_domain}/php/binaries/trusty/composer/#{dependency_version}/composer.phar"
 
     [dependency_version, url, md5]
   end
