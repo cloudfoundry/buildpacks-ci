9cd5351475ec356af6683f7e74b7866019b8f328
James Wen <jrw2175@columbia.edu>
2016-08-04 14:37:40 -0400

    Make machete specs job checkout a bosh-lite environment before execution
    - The integration specs for the have_logged matcher requires the deploying
      of apps
    
    [#126394949]
    
    Signed-off-by: Gabriel Ramirez <gabriel.e.ramirez@gmail.com>

--- scripts/machete.sh
+++ scripts/machete.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+DEPLOYMENT_NAME=${DEPLOYMENT_NAME:-`cat cf-environments/name`}
+DOMAIN_NAME=${DOMAIN_NAME:-cf-app.com}
+
 pushd buildpacks-ci
   ./scripts/start-docker
 popd
 
 pushd machete
+  ./scripts/cf_login_and_setup $DOMAIN_NAME
   bundle
   bundle exec rspec
 popd
