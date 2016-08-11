4bf29b7ac86d842ce6c405c33d4d5ed55eead55e
Jacques Chester <jchester@pivotal.io>
2016-08-11 15:13:28 -0400

    Add error throwing to GitClient.last_commit_message
    - Backfill spec
    
    [#127006189]
    
    Signed-off-by: James Wen <jrw2175@columbia.edu>

--- lib/git-client.rb
+++ lib/git-client.rb
@@ -1,6 +1,15 @@
+require 'open3'
+
 class GitClient
   def self.last_commit_message(dir)
-    Dir.chdir(dir) { `git log --format=%B -n 1 HEAD` }
+    Dir.chdir(dir) do
+      command = 'git log --format=%B -n 1 HEAD'
+      stdout_str, stderr_str, status = Open3.capture3(command)
+
+      raise "Could not get last commit message. STDERR was: #{stderr_str}" unless status.success?
+
+      stdout_str
+    end
   end
 
   def self.add_everything
