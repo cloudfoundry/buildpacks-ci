d2ff0ab27a2e8a473328653eddb032ec8da4397b
Jacques Chester <jchester@pivotal.io>
2016-08-11 14:54:43 -0400

    Extract 'git add -A' into GitClient.add_everything
    - From buildpack-dependency-updater.rb only, will do other replacements
      in future
    - Backfilled spec
    
    [#127006189]
    
    Signed-off-by: James Wen <jrw2175@columbia.edu>
    Signed-off-by: Jacques Chester <jchester@pivotal.io>

--- lib/git-client.rb
+++ lib/git-client.rb
@@ -2,4 +2,8 @@ class GitClient
   def self.last_commit_message(dir)
     Dir.chdir(dir) { `git log --format=%B -n 1 HEAD` }
   end
+
+  def self.add_everything
+    raise 'Could not add files' unless system('git add -A')
+  end
 end
