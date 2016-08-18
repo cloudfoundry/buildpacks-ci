c39a9c09075f80e8cca6d68d7017fe66bc9319c1
James Wen <jrw2175@columbia.edu>
2016-08-18 18:38:48 -0400

    Create verify-buildpack-binaries job
    - Verify buildpack release manifests and dependency md5s
    
    [#128159787]

--- scripts/verify_released_manifest_dependency_md5s.rb
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env ruby
-
-buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-require_relative "#{buildpacks_ci_dir}/lib/buildpack-binary-md5-validator"
-
-buildpack_dir = "/Users/pivotal/workspace/ruby-buildpack"
-BuildpackBinaryMD5Validator.run!(buildpack_dir)
