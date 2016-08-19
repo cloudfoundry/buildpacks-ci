331fc253e1c0871a6909b70cc2ba28634c0b2a95
James Wen <jrw2175@columbia.edu>
2016-08-19 16:17:00 -0400

    Add whitelist to avoid failing on replaced binary uris that have already vetted
    [#128159787]

--- scripts/verify-buildpack-binaries.rb
+++ scripts/verify-buildpack-binaries.rb
@@ -3,8 +3,12 @@
 buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 require_relative "#{buildpacks_ci_dir}/lib/buildpack-binary-md5-validator"
 
+# uris that should not be checked because the binaries are already replaced and
+# we have vetted these uris/agree that they're not a concern
+whitelist_file = File.join(Dir.pwd, 'binary-verification-whitelist', 'whitelist.yml')
+
 Dir["*-buildpack"].each do |buildpack|
   puts "Now validating md5s for the #{buildpack}"
   buildpack_dir = File.join(Dir.pwd, buildpack)
-  BuildpackBinaryMD5Validator.run!(buildpack_dir)
+  BuildpackBinaryMD5Validator.run!(buildpack_dir, whitelist_file)
 end
