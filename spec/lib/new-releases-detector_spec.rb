26c95483f4bc09dee8f0c845577150ef9a46b080
Sam Smith <sesmith177@gmail.com>
2016-08-23 12:07:29 -0400

    DRY up new-releases-detector spec with shared examples
    [#128516897]
    
    Signed-off-by: James Wen <jrw2175@columbia.edu>

--- spec/lib/new-releases-detector_spec.rb
+++ spec/lib/new-releases-detector_spec.rb
@@ -9,15 +9,9 @@ describe NewReleasesDetector do
     names.collect { |n| OpenStruct.new(name: n) }
   end
 
-  let(:new_releases_dir)      { Dir.mktmpdir }
-  let(:openjdk_yaml_filename) { "#{new_releases_dir}/openjdk.yaml" }
-  let(:openjdk_yaml_file_new) { "#{new_releases_dir}/openjdk-new.yaml" }
-  let(:python_yaml_filename)  { "#{new_releases_dir}/python.yaml" }
-  let(:python_yaml_file_new)  { "#{new_releases_dir}/python-new.yaml" }
-  let(:node_yaml_filename)    { "#{new_releases_dir}/node.yaml" }
-  let(:node_yaml_file_new)    { "#{new_releases_dir}/node-new.yaml" }
-  let(:github_username)       { 'github_username' }
-  let(:github_password)       { 'github_password1!' }
+  let(:new_releases_dir) { Dir.mktmpdir }
+  let(:github_username)  { 'github_username' }
+  let(:github_password)  { 'github_password1!' }
 
   before do
     allow_any_instance_of(described_class).to receive(:warn) {}
@@ -35,79 +29,125 @@ describe NewReleasesDetector do
 
   subject { described_class.new(new_releases_dir) }
 
+  shared_examples_for 'there are new versions to potentially build' do
+    let(:yaml_filename)     { "#{new_releases_dir}/#{dependency}.yaml"}
+    let(:yaml_new_filename) { "#{new_releases_dir}/#{dependency}-new.yaml"}
+    let(:all_versions)      { old_versions + new_versions }
+
+    before do
+      allow(File).to receive(:exist?).with(yaml_filename).and_return(true)
+      allow(YAML).to receive(:load_file).with(yaml_filename).and_return(old_versions)
+      allow_any_instance_of(described_class).to receive(:open)
+        .with(new_releases_source)
+        .and_return(new_releases_response)
+    end
+
+    it 'sets dependency_tags to a hash of dependencies as keys and array of diffs as values' do
+      expect(subject.dependency_tags).to eq({dependency => new_versions})
+    end
+
+    it 'writes to a file the latest releases' do
+      expect(File).to receive(:write).with(yaml_filename, all_versions.to_yaml)
+      subject
+    end
+
+    it 'writes the diff in releases to a file' do
+      expect(File).to receive(:write).with(yaml_new_filename, new_versions.to_yaml)
+      subject
+    end
+  end
+
+  shared_examples_for 'there are no new versions to potentially build' do
+    let(:yaml_filename) { "#{new_releases_dir}/#{dependency}.yaml"}
+    let(:new_versions)  { [] }
+
+    before do
+      allow_any_instance_of(described_class).to receive(:warn).and_call_original
+      allow(File).to receive(:exist?).with(yaml_filename).and_return(true)
+      allow(YAML).to receive(:load_file).with(yaml_filename).and_return(old_versions)
+      allow_any_instance_of(described_class).to receive(:open)
+        .with(new_releases_source)
+        .and_return(new_releases_response)
+    end
+
+    it 'sets dependency_tags to an empty hash' do
+      expect(subject.dependency_tags).to eq({})
+    end
+
+    it 'outputs to stderr that there are no new updates' do
+      expect do
+        subject
+      end.to output(/There are no new updates to the \*#{dependency}\* dependency\n/).to_stderr
+    end
+  end
+
   describe '#initialize' do
     it 'sets new_releases_dir' do
       expect(subject.new_releases_dir).to eq(new_releases_dir)
-    end
-
-    context 'when there are new releases' do
-      before do
-        allow(File).to receive(:exist?).with(openjdk_yaml_filename).and_return(true)
-        allow(YAML).to receive(:load_file).with(openjdk_yaml_filename).and_return(%w(v1 v2))
-        allow_any_instance_of(described_class).to receive(:open)
-          .with('https://download.run.pivotal.io/openjdk/trusty/x86_64/index.yml')
-          .and_return(double(read: { 'v1' => 1, 'v2' => 2, 'v3' => 3, 'v4' => 4 }.to_yaml))
-
-        python_yaml_filename = "#{new_releases_dir}/python.yaml"
-        allow(File).to receive(:exist?).with(python_yaml_filename).and_return(true)
-        allow(YAML).to receive(:load_file).with(python_yaml_filename).and_return(%w(a b))
-        allow_any_instance_of(described_class).to receive(:open)
-          .with('https://hg.python.org/cpython/json-tags')
-          .and_return(double(read: { tags: [{ tag: 'a' }, { tag: 'b' }, { tag: 'c' }] }.to_json))
-
-        node_yaml_filename = "#{new_releases_dir}/node.yaml"
-        allow(File).to receive(:exist?).with(node_yaml_filename).and_return(true)
-        allow(YAML).to receive(:load_file).with(node_yaml_filename).and_return(%w(1.2.3 1.2.4))
-        allow_any_instance_of(described_class).to receive(:open)
-          .with('https://nodejs.org/dist/index.json')
-          .and_return(double(read: [{ 'version' => 'v1.2.3'}, { 'version' => 'v1.2.4'}, { 'version' => 'v1.2.5'}].to_json))
-
-      end
-
-      it 'sets dependency_tags to a hash of dependencies as keys and array of diffs as values' do
-        expect(subject.dependency_tags).to eq(openjdk: %w(v3 v4), python: ['c'], node: ['1.2.5'])
-      end
-
-      it 'writes to a file the latest releases' do
-        expect(File).to receive(:write).with(node_yaml_filename, "---\n- 1.2.3\n- 1.2.4\n- 1.2.5\n")
-        expect(File).to receive(:write).with(openjdk_yaml_filename, "---\n- v1\n- v2\n- v3\n- v4\n")
-        expect(File).to receive(:write).with(python_yaml_filename, "---\n- a\n- b\n- c\n")
-        subject
-      end
-
-      it 'writes the diff in releases to a file' do
-        expect(File).to receive(:write).with(node_yaml_file_new, "---\n- 1.2.5\n")
-        expect(File).to receive(:write).with(openjdk_yaml_file_new, "---\n- v3\n- v4\n")
-        expect(File).to receive(:write).with(python_yaml_file_new, "---\n- c\n")
-        subject
-      end
-    end
-
-    context 'when there are no new releases' do
-      before do
-        allow_any_instance_of(described_class).to receive(:warn).and_call_original
-        allow(File).to receive(:exist?).with(openjdk_yaml_filename).and_return(true)
-        allow(YAML).to receive(:load_file).with(openjdk_yaml_filename).and_return(%w(v1 v2))
-        allow_any_instance_of(described_class).to receive(:open)
-          .with('https://download.run.pivotal.io/openjdk/trusty/x86_64/index.yml')
-          .and_return(double(read: { 'v1' => 1, 'v2' => 2 }.to_yaml))
-      end
-
-      it 'sets dependency_tags to an empty hash' do
-        expect(subject.dependency_tags).to eq({})
-      end
-
-      it 'outputs to stderr that there are no new updates' do
-        expect do
-          subject
-        end.to output(/There are no new updates to the \*openjdk\* dependency\n/).to_stderr
-      end
     end
 
     context 'configures Octokit' do
       it 'sets the autopaginate to true' do
         expect(Octokit).to receive(:auto_paginate=).with(true).at_least(:once)
         subject
+      end
+    end
+
+    context 'for openjdk' do
+      let(:dependency)          { :openjdk }
+      let(:old_versions)        { %w(v1 v2) }
+      let(:new_releases_source) { 'https://download.run.pivotal.io/openjdk/trusty/x86_64/index.yml' }
+
+      context 'when there are new releases' do
+        let(:new_versions)          { %w(v3 v4) }
+        let(:new_releases_response) { double(read: { 'v1' => 1, 'v2' => 2, 'v3' => 3, 'v4' => 4 }.to_yaml) }
+
+        it_behaves_like 'there are new versions to potentially build'
+      end
+
+      context 'when there are no new releases' do
+        let(:new_releases_response) { double(read: { 'v1' => 1, 'v2' => 2 }.to_yaml) }
+
+        it_behaves_like 'there are no new versions to potentially build'
+      end
+    end
+
+    context 'for python' do
+      let(:dependency)          { :python }
+      let(:old_versions)        { %w(a b) }
+      let(:new_versions)        { %w(c) }
+      let(:new_releases_source) { 'https://hg.python.org/cpython/json-tags' }
+
+      context 'when there are new releases' do
+        let(:new_versions)          { %w(c) }
+        let(:new_releases_response) { double(read: { tags: [{ tag: 'a' }, { tag: 'b' }, { tag: 'c' }] }.to_json) }
+
+        it_behaves_like 'there are new versions to potentially build'
+      end
+
+      context 'when there are no new releases' do
+        let(:new_releases_response) { double(read: { tags: [{ tag: 'a' }, { tag: 'b' }] }.to_json) }
+
+        it_behaves_like 'there are no new versions to potentially build'
+      end
+    end
+
+    context 'for node' do
+      let(:dependency)          { :node }
+      let(:old_versions)        { %w(1.2.3 1.2.4) }
+      let(:new_releases_source) { 'https://nodejs.org/dist/index.json' }
+
+      context 'when there are new releases' do
+        let(:new_versions)          { %w(1.2.5) }
+        let(:new_releases_response) { double(read: [{ 'version' => 'v1.2.3'}, { 'version' => 'v1.2.4'}, { 'version' => 'v1.2.5'}].to_json) }
+
+        it_behaves_like 'there are new versions to potentially build'
+      end
+
+      context 'when there are no new releases' do
+        let(:new_releases_response) { double(read: [{ 'version' => 'v1.2.3'}, { 'version' => 'v1.2.4'}].to_json) }
+
+        it_behaves_like 'there are no new versions to potentially build'
       end
     end
   end
@@ -141,7 +181,7 @@ describe NewReleasesDetector do
   end
 
   describe '#post_to_tracker' do
-    let(:tracker_client) { double(:tracker_client) }
+    let(:tracker_client)             { double(:tracker_client) }
     let(:buildpack_dependency_tasks) { [:snake, :lizard] }
 
     before do
