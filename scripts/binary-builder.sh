dedf3426a44dffe62fee94ab54482e0965782ce6
Sam Smith <sesmith177@gmail.com>
2016-08-25 15:11:03 -0400

    Configure AWS CLI correctly for binary-builder specs
    - Fix typo in AWS_ACCESS_KEY --> AWS_ACCESS_KEY_ID
    - Specify aws region
    - Ensure AWS CLI is installed before binary builder oracle specs
    
    [#128038243]
    
    Signed-off-by: James Wen <jrw2175@columbia.edu>

--- scripts/binary-builder.sh
+++ scripts/binary-builder.sh
@@ -9,8 +9,9 @@ pushd binary-builder
   gem install bundler --no-ri --no-rdoc
   bundle config mirror.https://rubygems.org ${RUBYGEM_MIRROR}
   bundle install -j4
-  
+
   if [ ${RUN_ORACLE_PHP_TESTS-false} = "true" ]; then
+    apt-get update && apt-get -y install awscli
     bundle exec rspec $specs
   else
     bundle exec rspec $specs --tag ~run_oracle_php_tests
