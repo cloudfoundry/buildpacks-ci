81c55fe0096a0c58237c4fa7a02be3159d6d43a2
James Wen <jrw2175@columbia.edu>
2016-08-25 12:05:32 -0400

    Configure binary-builder pipeline to optionally run PHP Oracle tests
    - Specify run-oracle-php-tests: true in public-config.yml to run these
      tests, which require access to an S3 bucket containing the Oracle
      client and sdk files
    
    - Specify run-oracle-php-tests: false in public-config.yml to skip these
      tests
    
    - This setting is applied during execution of
      `bin/update-all-the-pipelines`
    
    [#128038243]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- scripts/binary-builder.sh
+++ scripts/binary-builder.sh
@@ -3,12 +3,16 @@
 set -ex
 
 pushd binary-builder
+  ls spec/**/*_spec.rb | split -l $(expr `ls spec/**/*_spec.rb | wc -l` / $TOTAL_GROUPS)
+  specs=$(cat $(ls x* | sed -n "$CURRENT_GROUP p"))
 
-ls spec/**/*_spec.rb | split -l $(expr `ls spec/**/*_spec.rb | wc -l` / $TOTAL_GROUPS)
-specs=$(cat $(ls x* | sed -n "$CURRENT_GROUP p"))
-
-gem install bundler --no-ri --no-rdoc
-bundle config mirror.https://rubygems.org ${RUBYGEM_MIRROR}
-bundle install -j4
-bundle exec rspec $specs
+  gem install bundler --no-ri --no-rdoc
+  bundle config mirror.https://rubygems.org ${RUBYGEM_MIRROR}
+  bundle install -j4
+  
+  if [ ${RUN_ORACLE_PHP_TESTS-false} = "true" ]; then
+    bundle exec rspec $specs
+  else
+    bundle exec rspec $specs --tag ~run_oracle_php_tests
+  fi
 popd
