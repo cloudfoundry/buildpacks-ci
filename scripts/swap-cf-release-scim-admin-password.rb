bf200f05c9fa0ac206c91c5d7864411b761e689f
Sam Smith <sesmith177@gmail.com>
2016-08-29 17:59:15 -0400

    Change way that CI_CF_PASSWORD gets interpolated
    [#129412237]
    
    Signed-off-by: James Wen <jrw2175@columbia.edu>

---
+++ scripts/swap-cf-release-scim-admin-password.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+
+cf_release_dir = ARGV[0]
+cf_manifest_file = ARGV[1]
+
+Dir.chdir(cf_release_dir) do
+  cf_manifest_contents = File.read(cf_manifest_file)
+
+  to_swap = <<-SCIM
+    scim:
+      external_groups: null
+      groups: null
+      userids_enabled: true
+      users:
+      - groups:
+        - scim.write
+        - scim.read
+        - openid
+        - cloud_controller.admin
+        - clients.read
+        - clients.write
+        - doppler.firehose
+        - routing.router_groups.read
+        - routing.router_groups.write
+        name: admin
+        password: admin
+  SCIM
+
+  swapped = <<-SCIM
+    scim:
+      external_groups: null
+      groups: null
+      userids_enabled: true
+      users:
+      - groups:
+        - scim.write
+        - scim.read
+        - openid
+        - cloud_controller.admin
+        - clients.read
+        - clients.write
+        - doppler.firehose
+        - routing.router_groups.read
+        - routing.router_groups.write
+        name: admin
+        password: #{ENV['CI_CF_PASSWORD']}
+  SCIM
+
+  cf_manifest_contents.gsub!(to_swap, swapped)
+
+  File.write(cf_manifest_file, cf_manifest_contents)
+end
