d0b930909c2bd8b3722c415fb29e2209bab9981f
James Wen <jrw2175@columbia.edu>
2016-08-30 10:28:39 -0400

    Keep old CI_CF_PASSWORD injection method as well
    - For CF Release 241 and before, the old injection method is required
      because the uaa scim users value is a one liner.
    - For after CF Release 241, scim uaa users is multi-line and requires a
      different injection method
    
    [#129412237]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- scripts/swap-cf-release-scim-admin-password.rb
+++ scripts/swap-cf-release-scim-admin-password.rb
@@ -6,7 +6,10 @@ cf_manifest_file = ARGV[1]
 Dir.chdir(cf_release_dir) do
   cf_manifest_contents = File.read(cf_manifest_file)
 
-  to_swap = <<-SCIM
+  if cf_manifest_contents.match /admin\|admin\|scim\.write,scim\.read/
+    cf_manifest_contents.gsub!('admin|admin', 'admin|' + ENV['CI_CF_PASSWORD'])
+  else
+    to_swap = <<-SCIM
     scim:
       external_groups: null
       groups: null
@@ -24,9 +27,9 @@ Dir.chdir(cf_release_dir) do
         - routing.router_groups.write
         name: admin
         password: admin
-  SCIM
+    SCIM
 
-  swapped = <<-SCIM
+    swapped = <<-SCIM
     scim:
       external_groups: null
       groups: null
@@ -44,9 +47,10 @@ Dir.chdir(cf_release_dir) do
         - routing.router_groups.write
         name: admin
         password: #{ENV['CI_CF_PASSWORD']}
-  SCIM
+    SCIM
 
-  cf_manifest_contents.gsub!(to_swap, swapped)
+    cf_manifest_contents.gsub!(to_swap, swapped)
+  end
 
   File.write(cf_manifest_file, cf_manifest_contents)
 end
