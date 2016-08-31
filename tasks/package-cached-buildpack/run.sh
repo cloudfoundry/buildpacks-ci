915cbfaebdabe77e4bbadf71e9e54f7afcb0a320
Sam Smith <sesmith177@gmail.com>
2016-08-31 15:58:41 -0400

    Add jobs + tasks to upload dotnet core buildpack to pivnet
    - These tasks use the new task directory structure [#126369115]
    - Created new pivotal-cf/buildpacks-ci-robots repo for Concourse jobs to
      use
    - Uses pivnet resource for uploading to pivnet
    
    [#128507343]
    
    Signed-off-by: Jacques Chester <jchester@pivotal.io>
    Signed-off-by: Sam Smith <sesmith177@gmail.com>
    Signed-off-by: James Wen <jrw2175@columbia.edu>

---
+++ tasks/package-cached-buildpack/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+pushd buildpack-master
+  export BUNDLE_GEMFILE=cf.Gemfile
+  bundle config mirror.https://rubygems.org ${RUBYGEM_MIRROR}
+  bundle install
+  bundle exec buildpack-packager --cached
+
+  VERSION=$(cat VERSION)
+popd
+
+TIMESTAMP=$(date +%s)
+
+CACHED_BUILDPACK="buildpack-master/dotnet-core_buildpack-cached-v$VERSION.zip"
+CACHED_TIMESTAMP_BUILDPACK="buildpack-artifacts/dotnet-core_buildpack-cached-v$VERSION+$TIMESTAMP.zip"
+
+mv ${CACHED_BUILDPACK} ${CACHED_TIMESTAMP_BUILDPACK}
+
+echo md5: "`md5sum $CACHED_TIMESTAMP_BUILDPACK`"
+echo sha256: "`sha256sum $CACHED_TIMESTAMP_BUILDPACK`"
