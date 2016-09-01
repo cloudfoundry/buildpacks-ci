bfc104d93bf07d786b33205a1924563708bf4a67
Olivier ORAND <olivier.orand@orange.com>
2016-09-01 17:18:48 +0200

    use RUBYGEM_MIRROR  with gem install and optimize bundler install

--- scripts/binary-builder-integration.sh
+++ scripts/binary-builder-integration.sh
@@ -3,6 +3,9 @@
 set -ex
 
 pushd binary-builder
+  if [ -n "${RUBYGEM_MIRROR}" ]; then
+    gem sources --clear-all --add ${RUBYGEM_MIRROR}
+  fi
   gem install bundler --no-ri --no-rdoc
   bundle config mirror.https://rubygems.org ${RUBYGEM_MIRROR}
   bundle install --jobs=$(nproc)
