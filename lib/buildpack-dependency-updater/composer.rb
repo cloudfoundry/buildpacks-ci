cb4f9698b5cfa6031bfddad626438c0a164482a8
Anna Thornton <athornton@pivotal.io>
2016-09-12 11:33:20 -0400

    Update default_versions for composer in php buildpack
    [#130202321]
    
    Signed-off-by: James Wen <jrw2175@columbia.edu>

--- lib/buildpack-dependency-updater/composer.rb
+++ lib/buildpack-dependency-updater/composer.rb
@@ -11,4 +11,14 @@ class BuildpackDependencyUpdater::Composer < BuildpackDependencyUpdater
 
     [dependency_version, url, md5]
   end
+
+  def perform_dependency_specific_changes;
+    buildpack_manifest["default_versions"].delete_if { |dep| dep["name"] == dependency }
+    default_dependency_hash = {
+      "name" => dependency,
+      "version" => dependency_version
+    }
+    buildpack_manifest["default_versions"] << default_dependency_hash
+  end
+
 end
