3eeb643f528ff58bd32f57a37358109f32fd0841
James Wen <jrw2175@columbia.edu>
2016-09-19 17:14:33 -0400

    Fix commit-shas script errors
    [#129962149]
    
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- scripts/release/commit-shas.rb
+++ scripts/release/commit-shas.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 require 'fileutils'
+require_relative '../../lib/git-client'
 
 latest_shasum_file = Dir["buildpack-artifacts/*.SHA256SUM.txt"].first
 system("rsync -a buildpack-checksums/ sha-artifacts")
 FileUtils.copy(latest_shasum_file, './sha-artifacts')
+
+shasum_basename = File.basename(latest_shasum_file, ".SHA256SUM.txt")
 
 Dir.chdir('sha-artifacts') do
   GitClient.add_everything
