945d418985607f87fef676b08f8cbda3f744de70
James Wen <jrw2175@columbia.edu>
2016-09-21 18:36:09 -0400

    Extract recreate-bosh-lite into BoshLiteRecreator class
    - Move associated task to new Concourse task directory structure
    
    [#128991329]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

---
+++ tasks/recreate-bosh-lite/run.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+require_relative '../../lib/bosh-lite-recreator.rb'
+
+iaas = ENV['IAAS'] || 'aws'
+deployment_id = ENV['DEPLOYMENT_NAME']
+domain_name = ENV['DOMAIN_NAME'] || 'cf-app.com'
+bosh_lite_user = ENV['BOSH_USER']
+bosh_lite_password = ENV['BOSH_PASSWORD']
+bosh_lite_deployment_name = ENV["#{iaas.upcase}_BOSH_LITE_NAME"]
+bosh_private_key = ENV['BOSH_PRIVATE_KEY']
+
+bosh_lite_url = "https://#{deployment_id}.#{domain_name}"
+
+if iaas == 'azure' || iaas == 'gcp'
+  bosh_director_user = ENV["#{iaas.upcase}_BOSH_DIRECTOR_USER"]
+  bosh_director_password = ENV["#{iaas.upcase}_BOSH_DIRECTOR_PASSWORD"]
+  bosh_director_target = "bosh.buildpacks-#{iaas}.ci.#{domain_name}"
+elsif iaas == 'aws'
+  bosh_director_user = bosh_director_password = bosh_director_target = nil
+else
+  puts "Please specify IAAS=(aws|azure|gcp)"
+  exit 1
+end
+
+exit 1 unless system "rsync -a deployments-buildpacks/ deployments-buildpacks-artifacts"
+
+Dir.chdir ('deployments-buildpacks-artifacts') do
+  rubygem_mirror = ENV['RUBYGEM_MIRROR']
+  exit 1 unless system "bundle config mirror.https://rubygems.org #{rubygem_mirror}"
+  num_cores = `nproc`.strip
+  exit 1 unless system "bundle install --jobs=#{num_cores} --retry 5"
+end
+
+deployment_dir = File.join(Dir.pwd,'deployments-buildpacks-artifacts', 'deployments', deployment_id)
+
+recreator = BoshLiteRecreator.new(iaas: iaas,
+                                  deployment_dir: deployment_dir,
+                                  deployment_id: deployment_id,
+                                  bosh_lite_user: bosh_lite_user,
+                                  bosh_lite_password: bosh_lite_password,
+                                  bosh_lite_deployment_name: bosh_lite_deployment_name,
+                                  bosh_lite_url: bosh_lite_url,
+                                  bosh_director_user: bosh_director_user,
+                                  bosh_director_password: bosh_director_password,
+                                  bosh_director_target: bosh_director_target,
+                                  bosh_private_key: bosh_private_key
+                                 )
+
+recreator.run!
