7efcbd5bc893400d0ae0eae1fb171156add1d3e3
Anna Thornton <athornton@pivotal.io>
2016-10-07 11:20:34 -0400

    Generalize Pivnet metadata writer
    - it now can be used for any buildpack, not just .NET core
    
    [#131727809]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- tasks/write-buildpack-pivnet-metadata/run.rb
+++ tasks/write-buildpack-pivnet-metadata/run.rb
@@ -2,16 +2,18 @@
 
 root_dir      = Dir.pwd
 
-require_relative 'pivnet-metadata-writer'
+require_relative 'buildpack-pivnet-metadata-writer'
 require_relative "#{root_dir}/buildpacks-ci/lib/git-client"
 
-metadata_dir  = File.join(root_dir, 'pivnet-dotnet-core-metadata', 'pivnet-metadata')
+buildpack = ENV['BUILDPACK']
+metadata_dir  = File.join(root_dir, 'pivnet-buildpack-metadata', 'pivnet-metadata')
 buildpack_dir = File.join(root_dir, 'buildpack-master')
+recent_changes_file = File.join(root_dir, 'buildpack-artifacts', 'RECENT_CHANGES')
 
 buildpack_files = ''
 
 Dir.chdir('pivotal-buildpack-cached') do
-  buildpack_files = Dir["dotnet-core_buildpack-cached-v*.zip"]
+  buildpack_files = Dir["#{buildpack}_buildpack-cached-v*.zip"]
 end
 
 if buildpack_files.count != 1
@@ -21,13 +23,13 @@ if buildpack_files.count != 1
 else
   cached_buildpack_filename = buildpack_files.first
 
-  writer = PivnetMetadataWriter.new(metadata_dir, buildpack_dir, cached_buildpack_filename)
+  writer = PivnetMetadataWriter.new(buildpack, metadata_dir, buildpack_dir, cached_buildpack_filename, recent_changes_filename)
   writer.run!
 
   Dir.chdir(metadata_dir) do
-    GitClient.add_file('dotnet-core.yml')
-    GitClient.safe_commit("Create Pivnet release metadata for .NET buildpack v#{writer.get_version}")
+    GitClient.add_file("#{buildpack}.yml")
+    GitClient.safe_commit("Create Pivnet release metadata for #{buildpack.capitalize} buildpack v#{writer.get_version}")
   end
 
-  system("rsync -a pivnet-dotnet-core-metadata/ pivnet-dotnet-core-metadata-artifacts")
+  system("rsync -a pivnet-buildpack-metadata/ pivnet-buildpack-metadata-artifacts")
 end
