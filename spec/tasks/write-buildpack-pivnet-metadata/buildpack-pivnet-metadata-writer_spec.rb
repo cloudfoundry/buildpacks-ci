60bb328d35714d755e6504495a2b246eaa16db61
Anna Thornton <athornton@pivotal.io>
2016-10-07 11:20:34 -0400

    Generalize Pivnet metadata writer
    - it now can be used for any buildpack, not just .NET core
    
    [#131727809]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- spec/tasks/write-buildpack-pivnet-metadata/buildpack-pivnet-metadata-writer_spec.rb
+++ spec/tasks/write-buildpack-pivnet-metadata/buildpack-pivnet-metadata-writer_spec.rb
@@ -7,63 +7,135 @@ require_relative '../../../tasks/write-buildpack-pivnet-metadata/buildpack-pivne
 
 
 describe BuildpackPivnetMetadataWriter do
+  let(:buildpack)                 { 'some_buildpack' }
   let(:root_dir)                  { Dir.mktmpdir }
   let(:buildpack_dir)             { File.join(root_dir, 'buildpack-master') }
-  let(:metadata_dir)              { File.join(root_dir, 'pivnet-dotnet-core-metadata', 'pivnet-metadata') }
-  let(:cached_buildpack_filename) { 'dotnet-core_buildpack-cached-v1.0.0+1472744399.zip' }
-  let(:yaml_path)                 { File.join(metadata_dir, 'dotnet-core.yml') }
+  let(:metadata_dir)              { File.join(root_dir, 'pivnet-buildpack-metadata', 'pivnet-metadata') }
+  let(:cached_buildpack_filename) { "#{buildpack}_buildpack-cached-v#{version}+#{timestamp}.zip" }
+  let(:yaml_path)                 { File.join(metadata_dir, "#{buildpack}.yml") }
   let(:yaml_contents)             { YAML.load_file(yaml_path) }
-  let(:version)                   { "1.0.0" }
+  let(:timestamp)                 { '1111111111' }
+  let(:recent_changes_filename)   { File.join(root_dir,'buildpack-artifacts', 'RECENT_CHANGES') }
+  let(:recent_changes)            { 'some_changes' }
 
   before do
     FileUtils.mkdir_p(buildpack_dir)
     FileUtils.mkdir_p(metadata_dir)
+    FileUtils.mkdir_p(File.join(root_dir,'buildpack-artifacts'))
+    File.write(recent_changes_filename, recent_changes)
   end
 
   after do
     FileUtils.rm_rf(root_dir)
   end
 
-  subject { described_class.new(metadata_dir, buildpack_dir, cached_buildpack_filename) }
+  subject { described_class.new(buildpack, metadata_dir, buildpack_dir, cached_buildpack_filename, recent_changes_filename) }
 
   describe '#get_version' do
+    let(:version)                   { "1.6.0" }
     let(:version_file) { File.join(buildpack_dir, 'VERSION') }
 
     before { File.write(version_file, version) }
 
     it "returns the buildpack version" do
-      expect(subject.get_version).to eq('1.0.0')
+      expect(subject.get_version).to eq('1.6.0')
     end
   end
 
   describe '#run!' do
 
-    before { allow(subject).to receive(:get_version).and_return(version) }
+    context 'the buildpack is dotnet core' do
+      let(:version)                   { "1.0.0" }
+      let(:buildpack)                 { 'dotnet-core' }
+      let(:recent_changes)            { '.NET Core Buildpack BETA for PCF' }
 
-    it "writes a yaml file" do
-      subject.run!
-      expect(File.exist? yaml_path).to be_truthy
+      before { allow(subject).to receive(:get_version).and_return(version) }
+
+      it "writes a yaml file" do
+        subject.run!
+        expect(File.exist? yaml_path).to be_truthy
+      end
+
+      it "writes the release metadata to the file" do
+        subject.run!
+        release = yaml_contents['release']
+        expect(release['version']).to eq '.NET Core 1.0.0 (BETA)'
+        expect(release['release_type']).to eq 'Beta Release'
+        expect(release['eula_slug']).to eq 'pivotal_beta_eula'
+        expect(release['release_notes_url']).to eq "https://github.com/cloudfoundry-incubator/dotnet-core-buildpack/releases/tag/v1.0.0"
+        expect(release['availability']).to eq 'Admins Only'
+      end
+
+      it 'writes the product files metadata to the file' do
+        subject.run!
+        product_files = yaml_contents['product_files']
+        expect(product_files.count).to eq 1
+
+        product_file = product_files.first
+        expect(product_file['file']).to eq File.join('pivotal-buildpack-cached', cached_buildpack_filename)
+        expect(product_file['upload_as']).to eq '.NET Core Buildpack BETA (offline)'
+        expect(product_file['description']).to eq '.NET Core Buildpack BETA for PCF'
+      end
     end
 
-    it "writes the release metadata to the file" do
-      subject.run!
-      release = yaml_contents['release']
-      expect(release['version']).to eq '.NET Core 1.0.0 (BETA)'
-      expect(release['release_type']).to eq 'Beta Release'
-      expect(release['eula_slug']).to eq 'pivotal_beta_eula'
-      expect(release['release_notes_url']).to eq "https://github.com/cloudfoundry-community/dotnet-core-buildpack/releases/tag/v1.0.0"
-      expect(release['availability']).to eq 'Admins Only'
-    end
+    context 'the buildpack is ruby' do
+      let(:version)                   { "1.7.45" }
+      let(:buildpack)                 { 'ruby' }
+      let(:recent_changes) do
+        <<~RECENT_CHANGES
+        * Added a feature
+          (https://www.pivotaltracker.com/story/show/130945067)
 
-    it 'writes the product files metadata to the file' do
-      subject.run!
-      product_files = yaml_contents['product_files']
-      expect(product_files.count).to eq 1
+        * Updated a dependency
+          (https://www.pivotaltracker.com/story/show/131225127)
+        Packaged binaries:
 
-      product_file = product_files.first
-      expect(product_file['file']).to eq File.join('pivotal-buildpack-cached', cached_buildpack_filename)
-      expect(product_file['upload_as']).to eq '.NET Core Buildpack BETA (offline)'
-      expect(product_file['description']).to eq '.NET Core Buildpack BETA for PCF'
+        | name                       | version                  | cf_stacks  |
+        |----------------------------|--------------------------|------------|
+        | ruby                       | 2.1.8                    | cflinuxfs2 |
+        | ruby                       | 2.1.9                    | cflinuxfs2 |
+
+        Default binary versions:
+
+        | name | version |
+        |------|---------|
+        | ruby | 2.3.1   |
+        | node | 4.6.0   |
+          * SHA256: 64d73148c0ae8704b266ae83d8863453982c2468195d39637d7cc53ea52d8c19
+        RECENT_CHANGES
+      end
+
+      before { allow(subject).to receive(:get_version).and_return(version) }
+
+      it "writes a yaml file" do
+        subject.run!
+        expect(File.exist? yaml_path).to be_truthy
+      end
+
+      it "writes the release metadata to the file" do
+        subject.run!
+        release = yaml_contents['release']
+        expect(release['version']).to eq 'Ruby 1.7.45'
+        expect(release['release_type']).to eq 'Minor Release'
+        expect(release['eula_slug']).to eq 'pivotal_software_eula'
+        expect(release['release_notes_url']).to eq "https://github.com/cloudfoundry/ruby-buildpack/releases/tag/v1.7.45"
+        expect(release['availability']).to eq 'All Users'
+      end
+
+      it 'writes the product files metadata to the file' do
+        subject.run!
+        product_files = yaml_contents['product_files']
+        expect(product_files.count).to eq 1
+
+        product_file = product_files.first
+        expect(product_file['file']).to eq File.join('pivotal-buildpack-cached', cached_buildpack_filename)
+        expect(product_file['upload_as']).to eq 'Ruby Buildpack (offline)'
+        expect(product_file['description']).to match /Added a feature/
+        expect(product_file['description']).to match /Updated a dependency/
+        expect(product_file['description']).to match /Packaged binaries:/
+        expect(product_file['description']).to match /Default binary versions:/
+        expect(product_file['description']).to match /SHA256:/
+      end
     end
   end
 end
--- tasks/write-buildpack-pivnet-metadata/buildpack-pivnet-metadata-writer.rb
+++ tasks/write-buildpack-pivnet-metadata/buildpack-pivnet-metadata-writer.rb
@@ -4,12 +4,14 @@ require 'yaml'
 
 class BuildpackPivnetMetadataWriter
 
-  attr_reader :output_dir, :buildpack_dir, :cached_buildpack_filename
+  attr_reader :buildpack, :output_dir, :buildpack_dir, :cached_buildpack_filename, :recent_changes_filename
 
-  def initialize(output_dir, buildpack_dir, cached_buildpack_filename)
+  def initialize(buildpack, output_dir, buildpack_dir, cached_buildpack_filename, recent_changes_filename)
+    @buildpack = buildpack
     @output_dir = output_dir
     @buildpack_dir = buildpack_dir
     @cached_buildpack_filename = cached_buildpack_filename
+    @recent_changes_filename = recent_changes_filename
   end
 
   def get_version
@@ -19,20 +21,20 @@ class BuildpackPivnetMetadataWriter
   end
 
   def run!
-    metadata_yml = File.join(output_dir, 'dotnet-core.yml')
+    metadata_yml = File.join(output_dir, "#{buildpack}.yml")
 
     metadata = {}
     metadata['release'] = {
-      'version' => ".NET Core #{get_version} (BETA)",
-      'release_type' => 'Beta Release',
-      'eula_slug' => 'pivotal_beta_eula',
-      'release_notes_url' => "https://github.com/cloudfoundry-community/dotnet-core-buildpack/releases/tag/v#{get_version}",
-      'availability' => 'Admins Only'
+      'version' => product_version,
+      'release_type' => release_type,
+      'eula_slug' => eula_slug,
+      'release_notes_url' => release_notes_url,
+      'availability' => availability
     }
     metadata['product_files'] = [ {
       'file' => File.join('pivotal-buildpack-cached', cached_buildpack_filename),
-      'upload_as' => '.NET Core Buildpack BETA (offline)',
-      'description' => '.NET Core Buildpack BETA for PCF'
+      'upload_as' => display_name,
+      'description' => description
     } ]
 
     puts "Writing metadata to #{metadata_yml}"
@@ -43,3 +45,56 @@ class BuildpackPivnetMetadataWriter
   end
 end
 
+private
+
+def product_version
+  if buildpack == 'dotnet-core'
+    ".NET Core #{get_version} (BETA)"
+  else
+    "#{buildpack.capitalize} #{get_version}"
+  end
+end
+
+def release_type
+  if buildpack == 'dotnet-core'
+    "Beta Release"
+  else
+    "Minor Release"
+  end
+end
+
+def eula_slug
+  if buildpack == "dotnet-core"
+    "pivotal_beta_eula"
+  else
+    "pivotal_software_eula"
+  end
+end
+
+def release_notes_url
+  if buildpack == "dotnet-core"
+    "https://github.com/cloudfoundry-incubator/dotnet-core-buildpack/releases/tag/v#{get_version}"
+  else
+    "https://github.com/cloudfoundry/#{buildpack}-buildpack/releases/tag/v#{get_version}"
+  end
+end
+
+def availability
+  if buildpack == "dotnet-core"
+    "Admins Only"
+  else
+    "All Users"
+  end
+end
+
+def display_name
+  if buildpack == "dotnet-core"
+    ".NET Core Buildpack BETA (offline)"
+  else
+    "#{buildpack.capitalize} Buildpack (offline)"
+  end
+end
+
+def description
+  File.read(recent_changes_filename)
+end
