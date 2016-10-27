7cf3bb7e68b0a387f88ab7c24c4cc869101fd729
Sam Smith <sesmith177@gmail.com>
2016-10-27 18:01:45 -0400

    Back up one directory before calling tagger
    Since script is invoked using bundle exec, the current directory is
    buildpacks-ci. Back up one directory so the current dirctory is the task
    root.
    
    [#128018683]

--- tasks/detect-and-upload/run.rb
+++ tasks/detect-and-upload/run.rb
@@ -2,10 +2,11 @@
 
 require_relative 'buildpack-tagger'
 
-buildpack_dir = File.join(Dir.pwd, 'buildpack')
-buildpack_name = ENV['BUILDPACK_NAME']
-git_repo_org = ENV['GIT_REPO_ORG']
+Dir.chdir('..') do
+  buildpack_dir = File.join(Dir.pwd, 'buildpack')
+  buildpack_name = ENV['BUILDPACK_NAME']
+  git_repo_org = ENV['GIT_REPO_ORG']
 
-tagger = BuildpackTagger.new(buildpack_dir, buildpack_name, git_repo_org)
-tagger.run!
-
+  tagger = BuildpackTagger.new(buildpack_dir, buildpack_name, git_repo_org)
+  tagger.run!
+end
