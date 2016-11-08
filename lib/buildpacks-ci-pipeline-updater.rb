fc1500a8fe4b9088490a38618f30f54d0d18cbdc
Anna Thornton <athornton@pivotal.io>
2016-11-08 10:41:09 -0500

    Reorganise BuildpacksCIPipelineUpdater
    To emphasise the public methods for this class.
    
    - Make private:
       - #header
       - #get_cf_version_from_deployment_name
       - #check_if_lastpass_installed
       - #credential_filenames
    - Move #parse_args and #get_config down
    
    Signed-off-by: Jacques Chester <jchester@pivotal.io>

--- lib/buildpacks-ci-pipeline-updater.rb
+++ lib/buildpacks-ci-pipeline-updater.rb
@@ -4,37 +4,6 @@ require 'yaml'
 require 'optparse'
 
 class BuildpacksCIPipelineUpdater
-  def check_if_lastpass_installed
-    raise '`brew install lastpass-cli` is required' if `which lpass` == ''
-  end
-
-  def parse_args(args)
-    # Argument parsing
-    specified_options = {}
-    opt_parser = OptionParser.new do |opts|
-      opts.banner = "Usage: ./bin/update-all-the-pipelines [options]"
-
-      opts.on("--include=INCLUDE", "-iINCLUDE", "Update pipelines if their names include this string") do |include_string|
-        specified_options[:include] = include_string
-      end
-
-      opts.on("--exclude=EXCLUDE", "-eEXCLUDE", "Skip pipelines if their names include this string") do |exclude_string|
-        specified_options[:exclude] = exclude_string
-      end
-
-      opts.on("--template=TEMPLATE", "-tTEMPLATE", "Only update pipelines from the specified template") do |template_string|
-        specified_options[:template] = template_string
-      end
-    end
-    opt_parser.parse!(args)
-    specified_options
-  end
-
-  def header(msg)
-    print '*' * 10
-    puts " #{msg}"
-  end
-
   def set_pipeline(target_name:, name:, cmd:, pipeline_variable_filename: "", options:)
     pipeline_prefix = ENV['PIPELINE_PREFIX'] || ''
 
@@ -78,15 +47,6 @@ class BuildpacksCIPipelineUpdater
     end
   end
 
-  def get_cf_version_from_deployment_name(deployment_name)
-    matches = /(lts|edge)\-\d+(\-azure)?/.match(deployment_name)
-    if matches.nil?
-      puts 'Your config/bosh-lite/*.yml files must be named in the following manner: edge-1.yml, edge-2.yml, lts-1.yml, lts-2.yml, etc.'
-      exit 1
-    end
-    matches[1]
-  end
-
   def update_bosh_lite_pipelines(target_name, options)
     header('For bosh-lite pipelines')
 
@@ -126,27 +86,6 @@ class BuildpacksCIPipelineUpdater
     end
   end
 
-  def get_config
-    public_config = YAML.load_file("public-config.yml")
-    lpass_config= {}
-    lpass_yaml_data=%x{lpass show #{credential_filenames[:lpass_concourse_private]} --notes && lpass show #{credential_filenames[:lpass_deployments_buildpacks]} --notes}
-    if $?.exitstatus != 0
-      puts "WARNING: ignoring lastpass config file. An error occured while processing #{LPASS_CONCOURSE_PRIVATE} and #{LPASS_DEPLOYMENTS_BUILDPACKS}"
-    else
-      lpass_config = YAML.load(lpass_yaml_data)
-    end
-    public_config.merge(lpass_config)
-  end
-
-  def credential_filenames
-    {
-      lpass_concourse_private:  ENV['LPASS_CONCOURSE_PRIVATE_FILE'] || 'Shared-Buildpacks/concourse-private.yml',
-      lpass_deployments_buildpacks: ENV['LPASS_DEPLOYMENTS_BUILDPACKS_FILE'] || 'Shared-Buildpacks/deployments-buildpacks.yml',
-      lpass_repos_private_keys: ENV['LPASS_REPOS_PRIVATE_KEYS_FILE'] || 'Shared-Buildpacks/buildpack-repos-private-keys.yml',
-      lpass_bosh_release_private_keys: ENV['LPASS_BOSH_RELEASE_PRIVATE_KEYS_FILE'] || 'Shared-Buildpacks/buildpack-bosh-release-repos-private-keys.yml'
-    }
-  end
-
   def run!(args)
     check_if_lastpass_installed
     options = parse_args(args)
@@ -160,5 +99,68 @@ class BuildpacksCIPipelineUpdater
     update_buildpack_pipelines(target_name, options)
 
     puts 'Thanks, The Buildpacks Team'
+  end
+
+  def get_config
+    public_config = YAML.load_file("public-config.yml")
+    lpass_config= {}
+    lpass_yaml_data=%x{lpass show #{credential_filenames[:lpass_concourse_private]} --notes && lpass show #{credential_filenames[:lpass_deployments_buildpacks]} --notes}
+    if $?.exitstatus != 0
+      puts "WARNING: ignoring lastpass config file. An error occured while processing #{LPASS_CONCOURSE_PRIVATE} and #{LPASS_DEPLOYMENTS_BUILDPACKS}"
+    else
+      lpass_config = YAML.load(lpass_yaml_data)
+    end
+    public_config.merge(lpass_config)
+  end
+
+  def parse_args(args)
+    # Argument parsing
+    specified_options = {}
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: ./bin/update-all-the-pipelines [options]"
+
+      opts.on("--include=INCLUDE", "-iINCLUDE", "Update pipelines if their names include this string") do |include_string|
+        specified_options[:include] = include_string
+      end
+
+      opts.on("--exclude=EXCLUDE", "-eEXCLUDE", "Skip pipelines if their names include this string") do |exclude_string|
+        specified_options[:exclude] = exclude_string
+      end
+
+      opts.on("--template=TEMPLATE", "-tTEMPLATE", "Only update pipelines from the specified template") do |template_string|
+        specified_options[:template] = template_string
+      end
+    end
+    opt_parser.parse!(args)
+    specified_options
+  end
+
+  private
+
+  def header(msg)
+    print '*' * 10
+    puts " #{msg}"
+  end
+
+  def get_cf_version_from_deployment_name(deployment_name)
+    matches = /(lts|edge)\-\d+(\-azure)?/.match(deployment_name)
+    if matches.nil?
+      puts 'Your config/bosh-lite/*.yml files must be named in the following manner: edge-1.yml, edge-2.yml, lts-1.yml, lts-2.yml, etc.'
+      exit 1
+    end
+    matches[1]
+  end
+
+  def check_if_lastpass_installed
+    raise '`brew install lastpass-cli` is required' if `which lpass` == ''
+  end
+
+  def credential_filenames
+    {
+      lpass_concourse_private:  ENV['LPASS_CONCOURSE_PRIVATE_FILE'] || 'Shared-Buildpacks/concourse-private.yml',
+      lpass_deployments_buildpacks: ENV['LPASS_DEPLOYMENTS_BUILDPACKS_FILE'] || 'Shared-Buildpacks/deployments-buildpacks.yml',
+      lpass_repos_private_keys: ENV['LPASS_REPOS_PRIVATE_KEYS_FILE'] || 'Shared-Buildpacks/buildpack-repos-private-keys.yml',
+      lpass_bosh_release_private_keys: ENV['LPASS_BOSH_RELEASE_PRIVATE_KEYS_FILE'] || 'Shared-Buildpacks/buildpack-bosh-release-repos-private-keys.yml'
+    }
   end
 end
