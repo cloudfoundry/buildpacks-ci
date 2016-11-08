9b8e7622de7de1fc1bf8b761a9a8b4d81f63cb26
Anna Thornton <athornton@pivotal.io>
2016-11-08 16:14:23 -0500

    Extract BuildpacksCIConfiguration
    From BuildpacksCIPipelineUpdateCommand#credential_filenames
    
    - Break apart entries into methods
    
    [#133741737]
    
    Signed-off-by: Jacques Chester <jchester@pivotal.io>

---
+++ lib/buildpacks-ci-configuration.rb
@@ -0,0 +1,17 @@
+class BuildpacksCIConfiguration
+  def concourse_private_filename
+    ENV['LPASS_CONCOURSE_PRIVATE_FILE'] || 'Shared-Buildpacks/concourse-private.yml'
+  end
+
+  def deployments_buildpacks_filename
+    ENV['LPASS_DEPLOYMENTS_BUILDPACKS_FILE'] || 'Shared-Buildpacks/deployments-buildpacks.yml'
+  end
+
+  def repos_private_keys_filename
+    ENV['LPASS_REPOS_PRIVATE_KEYS_FILE'] || 'Shared-Buildpacks/buildpack-repos-private-keys.yml'
+  end
+
+  def bosh_release_private_keys_filename
+    ENV['LPASS_BOSH_RELEASE_PRIVATE_KEYS_FILE'] || 'Shared-Buildpacks/buildpack-bosh-release-repos-private-keys.yml'
+  end
+end
--- lib/buildpacks-ci-pipeline-update-command.rb
+++ lib/buildpacks-ci-pipeline-update-command.rb
@@ -1,5 +1,10 @@
+require_relative 'buildpacks-ci-configuration'
+
 class BuildpacksCIPipelineUpdateCommand
   def run!(target_name:, name:, cmd:, pipeline_variable_filename: "", options:)
+
+    buildpacks_configuration = BuildpacksCIConfiguration.new
+
     pipeline_prefix = ENV['PIPELINE_PREFIX'] || ''
 
     text_to_include = options[:include]
@@ -16,22 +21,11 @@ class BuildpacksCIPipelineUpdateCommand
       set-pipeline \
       --pipeline=#{pipeline_prefix}#{name} \
       --config=<(#{cmd}) \
-      --load-vars-from=<(lpass show #{credential_filenames[:lpass_concourse_private]} --notes && lpass show #{credential_filenames[:lpass_deployments_buildpacks]} --notes && lpass show #{credential_filenames[:lpass_repos_private_keys]} --notes && lpass show #{credential_filenames[:lpass_bosh_release_private_keys]}) \
+      --load-vars-from=<(lpass show #{buildpacks_configuration.concourse_private_filename} --notes && lpass show #{buildpacks_configuration.deployments_buildpacks_filename} --notes && lpass show #{buildpacks_configuration.repos_private_keys_filename} --notes && lpass show #{buildpacks_configuration.bosh_release_private_keys_filename}) \
       --load-vars-from=public-config.yml \
     #{pipeline_specific_config}
     "}
 
     system "#{fly_cmd}"
-  end
-
-  private
-
-  def credential_filenames
-    {
-      lpass_concourse_private:  ENV['LPASS_CONCOURSE_PRIVATE_FILE'] || 'Shared-Buildpacks/concourse-private.yml',
-      lpass_deployments_buildpacks: ENV['LPASS_DEPLOYMENTS_BUILDPACKS_FILE'] || 'Shared-Buildpacks/deployments-buildpacks.yml',
-      lpass_repos_private_keys: ENV['LPASS_REPOS_PRIVATE_KEYS_FILE'] || 'Shared-Buildpacks/buildpack-repos-private-keys.yml',
-      lpass_bosh_release_private_keys: ENV['LPASS_BOSH_RELEASE_PRIVATE_KEYS_FILE'] || 'Shared-Buildpacks/buildpack-bosh-release-repos-private-keys.yml'
-    }
   end
 end
