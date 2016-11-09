f7386121f47299e6b0636bf0f359bb97b7fb1562
Jacques Chester <jchester@pivotal.io>
2016-11-09 14:13:19 -0500

    Rename name -> pipeline_name
    [#133741737]
    
    Signed-off-by: James Wen <jrw2175@columbia.edu>

--- lib/buildpacks-ci-pipeline-update-command.rb
+++ lib/buildpacks-ci-pipeline-update-command.rb
@@ -1,7 +1,7 @@
 require_relative 'buildpacks-ci-configuration'
 
 class BuildpacksCIPipelineUpdateCommand
-  def run!(concourse_target_name:, name:, cmd:, pipeline_variable_filename: "", options:)
+  def run!(concourse_target_name:, pipeline_name:, cmd:, pipeline_variable_filename: "", options:)
 
     buildpacks_configuration = BuildpacksCIConfiguration.new
 
@@ -9,17 +9,17 @@ class BuildpacksCIPipelineUpdateCommand
 
     text_to_include = options[:include]
     text_to_exclude = options[:exclude]
-    return if options.has_key?(:include) && !name.include?(text_to_include)
-    return if options.has_key?(:exclude) && name.include?(text_to_exclude)
+    return if options.has_key?(:include) && !pipeline_name.include?(text_to_include)
+    return if options.has_key?(:exclude) && pipeline_name.include?(text_to_exclude)
 
-    puts "   #{name} pipeline"
+    puts "   #{pipeline_name} pipeline"
 
     pipeline_specific_config = ""
     pipeline_specific_config ="--load-vars-from=#{pipeline_variable_filename}" unless pipeline_variable_filename.empty?
     fly_cmd = %{bash -c "fly \
       --target=#{concourse_target_name} \
       set-pipeline \
-      --pipeline=#{pipeline_prefix}#{name} \
+      --pipeline=#{pipeline_prefix}#{pipeline_name} \
       --config=<(#{cmd}) \
       --load-vars-from=<(lpass show #{buildpacks_configuration.concourse_private_filename} --notes && lpass show #{buildpacks_configuration.deployments_buildpacks_filename} --notes && lpass show #{buildpacks_configuration.repos_private_keys_filename} --notes && lpass show #{buildpacks_configuration.bosh_release_private_keys_filename}) \
       --load-vars-from=public-config.yml \
