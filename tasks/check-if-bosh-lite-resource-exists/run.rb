1e5ba77065aad338543032fed780b59560e75bc6
Anna Thornton <athornton@pivotal.io>
2016-11-11 17:13:26 -0500

    Add success message for check-if-bosh-lite-resource-exists
    [#129169949]
    
    Signed-off-by: James Wen <jrw2175@columbia.edu>

--- tasks/check-if-bosh-lite-resource-exists/run.rb
+++ tasks/check-if-bosh-lite-resource-exists/run.rb
@@ -9,11 +9,14 @@ resource_pools_dir = File.join(Dir.pwd, 'resource-pools')
 state_object = StateOfBoshLites.new
 state_object.get_states!(resource_pools_dir: resource_pools_dir)
 
+bosh_lite_name = deployment_id.split('.').first
 if state_object.bosh_lite_in_pool?(deployment_id)
-  bosh_lite_name = deployment_id.split('.').first
   puts "=========================================================================="
   puts "WARNING:"
   puts "You should trigger the #{bosh_lite_name} pipeline from the very beginning."
   puts "You should not be trying to re-run intermediate deploy/setup steps on an already functional BOSH Lite with CF and Diego successfully deployed to it."
   exit 1
+else
+  puts "#{bosh_lite_name} is not in the resource pool."
+  puts "Job proceeding."
 end
