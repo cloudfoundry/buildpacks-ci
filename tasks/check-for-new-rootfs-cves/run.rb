8a38c5fab0508bf63bcafce5a00b6403bf5cab58
Anna Thornton <athornton@pivotal.io>
2016-11-14 16:02:50 -0500

    Add stacks-nc CVE notification pipeline
    - This just updates the .yml file on orphan branch new-cves-stacks-nc,
    it doesn't send the emails etc. This because the set of CVEs that apply
    to stacks-nc should be a subset of those that apply to stacks.
    
    [#133855785]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- tasks/check-for-new-rootfs-cves/run.rb
+++ tasks/check-for-new-rootfs-cves/run.rb
@@ -15,11 +15,20 @@ stacks_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'stacks'))
 cves_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'output-new-cves'))
 cve_history = CVEHistory.new(cves_dir)
 
-notifiers = [SystemCVEEmailPreparerAndGithubIssueNotifier, SystemCVETrackerNotifier, SystemCVESlackNotifier]
+if ENV['STACK'] == 'stacks'
+  notifiers = [SystemCVEEmailPreparerAndGithubIssueNotifier, SystemCVETrackerNotifier, SystemCVESlackNotifier]
+elsif ENV['STACK'] == 'stacks-nc'
+  notifiers = []
+else
+  raise "Unspported stack: #{ENV['STACK']}"
+end
+
 StackCVENotifier.new(cve_history, cves_dir, stacks_dir).run!('Ubuntu 14.04', 'ubuntu14.04', notifiers)
 
-languages = ['ruby']
-languages.each do |language|
-  rss_cves = BuildpackCVETags.new(language).related_cves
-  BuildpackCVENotifier.run(language, rss_cves)
+if ENV['STACK'] == 'stacks'
+  languages = ['ruby']
+  languages.each do |language|
+    rss_cves = BuildpackCVETags.new(language).related_cves
+    BuildpackCVENotifier.run(language, rss_cves)
+  end
 end
