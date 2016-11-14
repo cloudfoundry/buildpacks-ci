b5cdf5cd69f8c11d0e7828d20d001e30c096d026
Anna Thornton <athornton@pivotal.io>
2016-11-14 16:36:30 -0500

    Add release-notes draft task to stacks pipeline
    - Create new class ReleaseNotesCreator to generate the release notes
    from the CVE yml file and the receipt diff.
    
    - Update the CVE yml file with the version of stacks that addresses that
    CVE
    
    [#133855785]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

---
+++ tasks/generate-stacks-release-notes/release-notes-creator.rb
@@ -0,0 +1,100 @@
+require 'yaml'
+require 'diffy'
+
+require_relative '../../lib/usn-release-notes.rb'
+
+class ReleaseNotesCreator
+
+  def initialize(cves_yaml_file, old_receipt_file, new_receipt_file)
+    @cves_yaml_file = cves_yaml_file
+    @old_receipt_file = old_receipt_file
+    @new_receipt_file = new_receipt_file
+  end
+
+  def cves
+    @cves ||= YAML.load_file(@cves_yaml_file)
+  end
+
+  def old_receipt
+    @old_receipt ||= File.read(@old_receipt_file).split("\n")[7..-1].join("\n")
+  end
+
+  def new_receipt
+    @new_receipt ||= File.read(@new_receipt_file).split("\n")[7..-1].join("\n")
+  end
+
+  def release_notes
+    text = "Notably, this release addresses:\n\n"
+    text += usn_release_notes_section + "\n"
+    text += receipt_diff_section
+    text
+  end
+
+  def usn_release_notes_section
+    text = ""
+    unreleased_usns.each do |usn|
+      text += UsnReleaseNotes.new(usn).text + "\n\n"
+    end
+    text
+  end
+
+  def unreleased_usns
+    cves.select do |cve|
+      cve['stack_release'] == 'unreleased'
+    end.map do |cve|
+      cve['title'].split(':').first
+    end
+  end
+
+  def receipt_diff_section
+    receipt_diff_array = Diffy::Diff.new(old_receipt, new_receipt).select do |line|
+      line.match(/^\+/) || line.match(/^-/)
+    end.map do |line|
+      line.strip.split("  ").select { |word| word !=""}.map { |word| word.strip }
+    end
+
+    receipt_diff = ""
+    format = format_string(receipt_diff_array)
+
+    receipt_diff_array.each do |line|
+      receipt_diff += (format % line).strip + "\n"
+    end
+
+    <<~MARKDOWN
+    ```
+    #{receipt_diff}```
+    MARKDOWN
+  end
+
+  private
+
+  def format_string(table)
+    num_columns = table.first.length
+    format_string = ""
+
+    max_lengths = (0...num_columns).map { 0 }
+    min_lengths = (0...num_columns).map { 999 }
+
+    table.each do |row|
+      (0...num_columns).each do |i|
+        if(row[i].length) > max_lengths[i]
+          max_lengths[i] = row[i].length
+        end
+
+        if(row[i].length) < min_lengths[i]
+          min_lengths[i] = row[i].length
+        end
+      end
+    end
+
+    (0...num_columns).each do |i|
+      if max_lengths[i] == min_lengths[i]
+        format_string += "%-#{max_lengths[i] + 2}s"
+      else
+        format_string += "%-#{max_lengths[i] + 1}s"
+      end
+    end
+
+    format_string
+  end
+end
