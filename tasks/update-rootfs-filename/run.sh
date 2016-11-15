0741eec854fbf653caaa1dcef1a28f35a0d6d19a
Sam Smith <sesmith177@gmail.com>
2016-11-15 09:41:47 -0500

    Allow shell globbing so copy succeeds

--- tasks/update-rootfs-filename/run.sh
+++ tasks/update-rootfs-filename/run.sh
@@ -9,4 +9,6 @@ SUFFIX="${ROOTFS_SUFFIX-}"
 source="stack-s3/cflinuxfs2$SUFFIX-*.tar.gz"
 destination=stack-archive/cflinuxfs2$SUFFIX-$(cat version/number).tar.gz
 
-mv "$source" "$destination"
+# here, we actually want globbing, so:
+# shellcheck disable=SC2086
+mv $source "$destination"
