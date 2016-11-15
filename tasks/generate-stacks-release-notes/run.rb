8182f670432d61953ce42ad9c59b0aefdb29b8a3
Anna Thornton <athornton@pivotal.io>
2016-11-15 16:43:41 -0500

    Authenticate Github connection for stacks-nc
    The repo is private, so we can't just grab the previous receipt from
    there.
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- tasks/generate-stacks-release-notes/run.rb
+++ tasks/generate-stacks-release-notes/run.rb
@@ -1,23 +1,30 @@
 #!/usr/bin/env ruby
 
+require 'octokit'
 require 'open-uri'
 require_relative 'release-notes-creator'
 require_relative '../../lib/git-client'
 
-if ENV['STACK'] == 'stacks'
-  github_repo = 'cloudfoundry/stacks'
-elsif ENV['STACK'] == 'stacks-nc'
-  github_repo = 'pivotal-cf/stacks-nc'
-else
-  raise "Unsupported stack: #{ENV['STACK']}"
-end
 
 previous_version = File.read('previous-stacks-release/version').strip
 new_version = File.read('version/number').strip
 
+if ENV['STACK'] == 'stacks'
+  old_receipt_uri = "https://raw.githubusercontent.com/cloudfoundry/stacks/#{previous_version}/cflinuxfs2/cflinuxfs2_receipt"
+elsif ENV['STACK'] == 'stacks-nc'
+  Octokit.configure do |c|
+    c.login    = ENV.fetch('GITHUB_USERNAME')
+    c.password = ENV.fetch('GITHUB_PASSWORD')
+  end
+
+  old_receipt_uri = Octokit.contents('pivotal-cf/stacks-nc', :path => 'cflinuxfs2/cflinuxfs2_receipt', :ref => previous_version)[:download_url]
+else
+  raise "Unsupported stack: #{ENV['STACK']}"
+end
+
 cve_yaml_file = 'new-cves/ubuntu14.04.yml'
 new_receipt_file = 'stacks/cflinuxfs2/cflinuxfs2_receipt'
-old_receipt_file = open("https://raw.githubusercontent.com/#{github_repo}/#{previous_version}/cflinuxfs2/cflinuxfs2_receipt")
+old_receipt_file = open(old_receipt_uri)
 
 body_file = 'release-body/body'
 notes = ReleaseNotesCreator.new(cve_yaml_file, old_receipt_file, new_receipt_file).release_notes
