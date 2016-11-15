aa90b560b2f43301fb720e0710ee3cc86dec327a
Sam Smith <sesmith177@gmail.com>
2016-11-15 09:14:30 -0500

    Add missing space to spec command

--- tasks/run-cflinuxfs2-nc-specs/run.sh
+++ tasks/run-cflinuxfs2-nc-specs/run.sh
@@ -10,7 +10,7 @@ pushd cflinuxfs2-rootfs-release
   export BUNDLE_GEMFILE=cf.Gemfile
   bundle install --jobs="$(nproc)" --no-cache
 
-  bundle exec buildpack-build cf_spec --host="$DEPLOYMENT_NAME.$BOSH_LITE_DOMAIN_NAME"--no-build --no-upload
+  bundle exec buildpack-build cf_spec --host="$DEPLOYMENT_NAME.$BOSH_LITE_DOMAIN_NAME" --no-build --no-upload
 
   # Ensure stacks-nc CF does not have multiple orgs when targeting for CATS
   cf delete-org -f pivotal
