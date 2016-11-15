61b7dcd5b9cbff63be68840a5f052e042149c860
James Wen <jrw2175@columbia.edu>
2016-11-15 18:58:56 -0500

    Fix relative lib path for verify-buildpack-binaries
    [#126369115]

--- tasks/verify-buildpack-binaries/run.rb
+++ tasks/verify-buildpack-binaries/run.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/env ruby
 
-buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 require_relative "#{buildpacks_ci_dir}/lib/buildpack-binary-md5-verifier"
 
 # uris that should not be checked because the binaries are already replaced and
