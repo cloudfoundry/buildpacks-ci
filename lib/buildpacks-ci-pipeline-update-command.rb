45962dbf1235553eb1eb61696ec71dd61a04c8ff
James Wen <jrw2175@columbia.edu>
2016-11-16 17:48:12 -0500

    Add git_repos_private_keys_filename credential file
    [#134472189]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- lib/buildpacks-ci-pipeline-update-command.rb
+++ lib/buildpacks-ci-pipeline-update-command.rb
@@ -21,7 +21,7 @@ class BuildpacksCIPipelineUpdateCommand
       set-pipeline \
       --pipeline=#{pipeline_prefix}#{pipeline_name} \
       --config=<(#{config_generation_command}) \
-      --load-vars-from=<(lpass show #{buildpacks_configuration.concourse_private_filename} --notes && lpass show #{buildpacks_configuration.deployments_buildpacks_filename} --notes && lpass show #{buildpacks_configuration.repos_private_keys_filename} --notes && lpass show #{buildpacks_configuration.bosh_release_private_keys_filename}) \
+      --load-vars-from=<(lpass show #{buildpacks_configuration.concourse_private_filename} --notes && lpass show #{buildpacks_configuration.deployments_buildpacks_filename} --notes && lpass show #{buildpacks_configuration.repos_private_keys_filename} --notes && lpass show #{buildpacks_configuration.git_repos_private_keys_filename} --notes && lpass show #{buildpacks_configuration.bosh_release_private_keys_filename}) \
       --load-vars-from=public-config.yml \
     #{pipeline_specific_config}
     "}
--- spec/lib/buildpacks-ci-configuration_spec.rb
+++ spec/lib/buildpacks-ci-configuration_spec.rb
@@ -69,6 +69,28 @@ describe BuildpacksCIConfiguration do
     end
   end
 
+  describe '#git_repos_private_keys_filename' do
+    subject { BuildpacksCIConfiguration.new.git_repos_private_keys_filename }
+
+    context 'configured by env variable' do
+      it 'returns the value of the env var' do
+        allow(ENV).to receive(:[]).with('LPASS_GIT_REPOS_PRIVATE_KEYS_FILE').and_return('keys.yml')
+
+        expect(subject).to eq('keys.yml')
+      end
+
+      it 'asks ENV for the value' do
+        expect(ENV).to receive(:[]).with('LPASS_GIT_REPOS_PRIVATE_KEYS_FILE')
+
+        subject
+      end
+    end
+
+    it 'has a default value' do
+      expect(subject).to eq('Shared-Buildpacks/git-repos-private-keys.yml')
+    end
+  end
+
   describe '#bosh_release_private_keys_filename' do
     subject { BuildpacksCIConfiguration.new.bosh_release_private_keys_filename }
 
