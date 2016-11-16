2523b261a94aa2070328a0c7093419c53f2ec799
James Wen <jrw2175@columbia.edu>
2016-11-15 19:58:21 -0500

    Migrate make-rootfs-smoke-test-manifest to task dir format
    [#126369115]

--- spec/tasks/make-rootfs-smoke-test-manifest/task_spec.rb
+++ spec/tasks/make-rootfs-smoke-test-manifest/task_spec.rb
@@ -6,17 +6,17 @@ require 'yaml'
 describe 'create bosh release task' do
   context 'when modifying release blobs' do
     before(:context) do
-      `git init ./spec/scripts/make-rootfs-smoke-test-manifest/deployments-buildpacks`
+      `git init ./spec/tasks/make-rootfs-smoke-test-manifest/deployments-buildpacks`
       execute(
-        '-c tasks/make-rootfs-smoke-test-manifest.yml ' \
+        '-c tasks/make-rootfs-smoke-test-manifest/task.yml ' \
         '-i buildpacks-ci=. ' \
-        '-i cflinuxfs2-rootfs-release=./spec/scripts/make-rootfs-smoke-test-manifest/cflinuxfs2-rootfs-release ' \
-        '-i deployments-buildpacks=./spec/scripts/make-rootfs-smoke-test-manifest/deployments-buildpacks ',
+        '-i cflinuxfs2-rootfs-release=./spec/tasks/make-rootfs-smoke-test-manifest/cflinuxfs2-rootfs-release ' \
+        '-i deployments-buildpacks=./spec/tasks/make-rootfs-smoke-test-manifest/deployments-buildpacks ',
         'DEPLOYMENT_NAME' => 'some_deployment')
     end
 
     after(:context) do
-      `rm -rf ./spec/scripts/make-rootfs-smoke-test-manifest/deployments-buildpacks/.git`
+      `rm -rf ./spec/tasks/make-rootfs-smoke-test-manifest/deployments-buildpacks/.git`
     end
 
     it 'adds the certs to the property_overrides.yml' do
