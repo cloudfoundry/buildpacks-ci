4d76a841046042be9f05fef0220c6e007032b8e4
James Wen <jrw2175@columbia.edu>
2016-11-15 20:56:37 -0500

    Do not chdir into non-existent scripts/notifications
    [#134085181]

--- tasks/check-for-new-buildpack-dependency-releases/run.rb
+++ tasks/check-for-new-buildpack-dependency-releases/run.rb
@@ -6,16 +6,14 @@ new_releases_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'new-rele
 
 require "#{buildpacks_ci_dir}/lib/new-releases-detector"
 
-Dir.chdir("#{buildpacks_ci_dir}/scripts/notifications") do
-  new_releases_detector = NewReleasesDetector.new(new_releases_dir)
-  new_releases_detector.post_to_tracker
-  new_releases_detector.post_to_slack
+new_releases_detector = NewReleasesDetector.new(new_releases_dir)
+new_releases_detector.post_to_tracker
+new_releases_detector.post_to_slack
 
 
-  unless new_releases_detector.changed_dependencies.empty?
-    Dir.chdir(new_releases_dir) do
-      raise 'command failed' unless system('git add -A')
-      raise 'command failed' unless system("git commit -m 'Updates for latest tags'")
-    end
+unless new_releases_detector.changed_dependencies.empty?
+  Dir.chdir(new_releases_dir) do
+    raise 'command failed' unless system('git add -A')
+    raise 'command failed' unless system("git commit -m 'Updates for latest tags'")
   end
 end
