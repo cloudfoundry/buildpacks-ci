32c94b2cb3f7766fbfb7ca1b65f9f5e476a26acd
James Wen <jrw2175@columbia.edu>
2016-11-15 19:45:48 -0500

    Migrate overwrite-cflinuxfs2-rootfs-release to task dir format
    [#126369115]

--- spec/tasks/overwrite-cflinuxfs2-rootfs-release/run_spec.rb
+++ spec/tasks/overwrite-cflinuxfs2-rootfs-release/run_spec.rb
@@ -7,7 +7,7 @@ describe 'make-rootfs' do
   old_path = ENV['PATH']
   old_rootfs_release = ENV['ROOTFS_RELEASE']
   ci_path = Dir.pwd
-  test_path = File.join(ci_path, '/spec/scripts/stacks/overwrite-cflinuxfs2-rootfs-release')
+  test_path = File.join(ci_path, '/spec/tasks/overwrite-cflinuxfs2-rootfs-release')
   blobs_dir = File.join(test_path, 'cflinuxfs2-rootfs-release/blobs')
   blob_destination = File.join(blobs_dir, 'rootfs/cflinuxfs2-1.49.0.tar.gz')
 
@@ -23,8 +23,8 @@ describe 'make-rootfs' do
 
   RSpec.shared_examples 'creates_the_blob' do
     it 'moves cflinuxfs2-*.tar.gz file from stack-s3 to cflinuxfs2-rootfs-release/blobs/rootfs/cflinuxfs2-[currentversion].tar.gz' do
-      Dir.chdir("#{ci_path}/spec/scripts/stacks/overwrite-cflinuxfs2-rootfs-release") do
-        system("#{ci_path}/scripts/stacks/overwrite-cflinuxfs2-rootfs-release")
+      Dir.chdir("#{ci_path}/spec/tasks/overwrite-cflinuxfs2-rootfs-release") do
+        system("#{ci_path}/tasks/overwrite-cflinuxfs2-rootfs-release/run.sh")
       end
       expect(File.exist?(blob_destination)).to eq(true)
       expect(File.read(blob_destination)).to eq('new-tarball')
