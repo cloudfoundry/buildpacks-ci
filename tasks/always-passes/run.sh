f07a64b85a382faf6ea93f695bb1df2a9f50fe8e
Jacques Chester <jchester@pivotal.io>
2016-11-30 10:51:29 -0500

    Add utility 'always-passes' task.
    [#134860397]

---
+++ tasks/always-passes/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo
+echo This task will always pass.
+echo
+echo It is intended as a placeholder, allowing jobs to be temporarily by-passed
+echo without disrupting the resource-passing logic of a pipeline.
+echo
+
+exit 0
