b20a263f33929ce93b453c2b1822796be3f2cff5
Sam Smith <sesmith177@gmail.com>
2016-12-19 12:25:11 -0500

    Update notifications-nc to use robots repo
    [#126710539]

--- tasks/check-for-new-rootfs-cves/run.rb
+++ tasks/check-for-new-rootfs-cves/run.rb
@@ -12,23 +12,27 @@ require "#{buildpacks_ci_dir}/lib/notifiers/system-cve-slack-notifier"
 require "#{buildpacks_ci_dir}/lib/notifiers/system-cve-email-preparer-and-github-issue-notifier"
 
 stacks_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'stacks'))
-cves_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'output-new-cves'))
-cve_history = CVEHistory.new(cves_dir)
 
 if ENV['STACK'] == 'stacks'
   notifiers = [SystemCVEEmailPreparerAndGithubIssueNotifier, SystemCVETrackerNotifier, SystemCVESlackNotifier]
+  cves_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'output-new-cves', 'new-cve-notifications'))
 elsif ENV['STACK'] == 'stacks-nc'
   notifiers = []
+  cves_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'output-new-cves', 'new-cves-stacks-nc'))
 else
   raise "Unspported stack: #{ENV['STACK']}"
 end
 
+cve_history = CVEHistory.new(cves_dir)
+
 StackCVENotifier.new(cve_history, cves_dir, stacks_dir).run!('Ubuntu 14.04', 'ubuntu14.04', notifiers)
 
 if ENV['STACK'] == 'stacks'
+  buildpack_cves_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'output-new-buildpack-cves', 'new-buildpack-cve-notifications'))
+
   languages = ['ruby']
   languages.each do |language|
     rss_cves = BuildpackCVETags.new(language).related_cves
-    BuildpackCVENotifier.run(language, rss_cves)
+    BuildpackCVENotifier.run(language, rss_cves, buildpack_cves_dir)
   end
 end
