cc05eb705d32a7cc0fb43029422e24b794d572f9
Sam Smith <sesmith177@gmail.com>
2017-01-03 17:30:21 -0500

    Don't look for AWS_BOSH_LITE_NAME
    Since AWS BOSH-lites aren't deployed by a BOSH director, this variable
    is not used
    
    [#134471969]

--- tasks/destroy-bosh-lite/run.rb
+++ tasks/destroy-bosh-lite/run.rb
@@ -5,17 +5,17 @@ require_relative '../../lib/bosh-lite-manager.rb'
 iaas = ENV.fetch('IAAS')
 deployment_id = ENV.fetch('DEPLOYMENT_NAME')
 domain_name = ENV.fetch('BOSH_LITE_DOMAIN_NAME')
-bosh_lite_deployment_name = ENV.fetch("#{iaas.upcase}_BOSH_LITE_NAME")
 bosh_private_key = ENV.fetch('BOSH_PRIVATE_KEY')
 
 bosh_lite_url = "https://#{deployment_id}.#{domain_name}"
 
 if iaas == 'azure' || iaas == 'gcp'
+  bosh_lite_deployment_name = ENV.fetch("#{iaas.upcase}_BOSH_LITE_NAME")
   bosh_director_user = ENV.fetch("#{iaas.upcase}_BOSH_DIRECTOR_USER")
   bosh_director_password = ENV.fetch("#{iaas.upcase}_BOSH_DIRECTOR_PASSWORD")
   bosh_director_target = "10.0.0.6"
 elsif iaas == 'aws'
-  bosh_director_user = bosh_director_password = bosh_director_target = nil
+  bosh_lite_deployment_name = bosh_director_user = bosh_director_password = bosh_director_target = nil
 else
   puts "Please specify IAAS=(aws|azure|gcp)"
   exit 1
