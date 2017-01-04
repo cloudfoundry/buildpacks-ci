331a024c22fe09c1844c0748cb73201880c5397b
Anna Thornton <athornton@pivotal.io>
2017-01-04 15:28:22 -0500

    Break out New Buildpack CVEs job from CVEs job and refactor
    - Rename all pipeline jobs to have no spaces
    - Make github/email, tracker, and slack cve notifiers general to be used
      by both jobs
    - Make CVEHistory general and usable by both jobs
    - Rename:
        cve-tags -> rootfs-cve-tags
        system-cve-tracker-notifier -> cve-tracker-notifier
        system-cve-slack-notifier -> cve-slack-notifier
        system-email-preparer-and-github-issue-notifier ->
          email-preparer-and-github-issue-notifier
        stacks-cve-notifier -> rootfs-cve-notifier
    - Remove buildpack-cve-notifier
    
    [#118768785]
    
    Signed-off-by: Keaty <kgross@pivotal.io>

---
+++ tasks/check-for-new-buildpack-cves/run.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+
+require "#{buildpacks_ci_dir}/lib/buildpack-cve-feed"
+require "#{buildpacks_ci_dir}/lib/cve-history"
+require "#{buildpacks_ci_dir}/lib/notifiers/cve-slack-notifier"
+require "#{buildpacks_ci_dir}/lib/notifiers/cve-email-preparer-and-github-issue-notifier"
+
+def notify!(language, new_cves, notifiers)
+  notifiers.each { |n| n.notify! new_cves, { :category => "buildpack-#{language}", :label => language } }
+end
+
+buildpack_cves_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'output-new-buildpack-cves', 'new-buildpack-cve-notifications'))
+
+languages = ['ruby']
+languages.each do |language|
+  all_cves = BuildpackCVEFeed.run(language)
+  past_cves = CVEHistory.read_yaml_cves(buildpack_cves_dir, "#{language}.yml")
+  new_cves = all_cves - past_cves
+
+  CVEHistory.write_yaml_cves(all_cves, buildpack_cves_dir, "#{language}.yml") unless new_cves.empty?
+
+  notify!(language, new_cves, [CVEEmailPreparerAndGithubIssueNotifier, CVESlackNotifier])
+end
--- tasks/check-for-new-rootfs-cves/run.rb
+++ tasks/check-for-new-rootfs-cves/run.rb
@@ -3,16 +3,15 @@
 
 buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 
-require "#{buildpacks_ci_dir}/lib/stack-cve-notifier"
-require "#{buildpacks_ci_dir}/lib/cve-history"
-require "#{buildpacks_ci_dir}/lib/notifiers/system-cve-tracker-notifier"
-require "#{buildpacks_ci_dir}/lib/notifiers/system-cve-slack-notifier"
-require "#{buildpacks_ci_dir}/lib/notifiers/system-cve-email-preparer-and-github-issue-notifier"
+require "#{buildpacks_ci_dir}/lib/rootfs-cve-notifier"
+require "#{buildpacks_ci_dir}/lib/notifiers/cve-tracker-notifier"
+require "#{buildpacks_ci_dir}/lib/notifiers/cve-slack-notifier"
+require "#{buildpacks_ci_dir}/lib/notifiers/cve-email-preparer-and-github-issue-notifier"
 
 stacks_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'stacks'))
 
 if ENV.fetch('STACK') == 'stacks'
-  notifiers = [SystemCVEEmailPreparerAndGithubIssueNotifier, SystemCVETrackerNotifier, SystemCVESlackNotifier]
+  notifiers = [CVEEmailPreparerAndGithubIssueNotifier, CVESlackNotifier, CVETrackerNotifier]
   cves_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'output-new-cves', 'new-cve-notifications'))
 elsif ENV.fetch('STACK') == 'stacks-nc'
   notifiers = []
@@ -21,6 +20,4 @@ else
   raise "Unsupported stack: #{ENV.fetch('STACK')}"
 end
 
-cve_history = CVEHistory.new(cves_dir)
-
-StackCVENotifier.new(cve_history, cves_dir, stacks_dir).run!('Ubuntu 14.04', 'ubuntu14.04', notifiers)
+RootFSCVENotifier.new(cves_dir, stacks_dir).run!('Ubuntu 14.04', 'ubuntu14.04', notifiers)
