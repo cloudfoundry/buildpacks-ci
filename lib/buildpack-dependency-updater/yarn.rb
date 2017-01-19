d9b85cf1dad852c877ac4155527f5558041ee5e1
Sam Smith <sesmith177@gmail.com>
2017-01-19 17:32:52 -0500

    Add yarn to buildpack dependency updater job
    [#136510177]

---
+++ lib/buildpack-dependency-updater/yarn.rb
@@ -0,0 +1,12 @@
+class BuildpackDependencyUpdater::Yarn < BuildpackDependencyUpdater
+  def perform_dependency_specific_changes
+    buildpack_manifest['default_versions'].delete_if { |dep| dep['name'] == dependency }
+
+    default_dependency_hash = {
+        'name' => dependency,
+        'version' => dependency_version
+    }
+
+    buildpack_manifest['default_versions'] << default_dependency_hash
+  end
+end
