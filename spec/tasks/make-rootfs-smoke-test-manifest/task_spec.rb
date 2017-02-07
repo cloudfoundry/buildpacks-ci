1be5335740bef5521719590d422d0ef67490304c
Sam Smith <sesmith177@gmail.com>
2017-02-07 11:14:40 -0500

    Update rootfs-smoke-test-spec
    - no longer uses deployments-buildpacks
    - no longer makes any git commits
    
    [#130673711]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- spec/tasks/make-rootfs-smoke-test-manifest/task_spec.rb
+++ spec/tasks/make-rootfs-smoke-test-manifest/task_spec.rb
@@ -6,27 +6,16 @@ require 'yaml'
 describe 'create bosh release task' do
   context 'when modifying release blobs' do
     before(:context) do
-      `git init ./spec/tasks/make-rootfs-smoke-test-manifest/deployments-buildpacks`
       execute(
         '-c tasks/make-rootfs-smoke-test-manifest/task.yml ' \
         '-i buildpacks-ci=. ' \
-        '-i cflinuxfs2-rootfs-release=./spec/tasks/make-rootfs-smoke-test-manifest/cflinuxfs2-rootfs-release ' \
-        '-i deployments-buildpacks=./spec/tasks/make-rootfs-smoke-test-manifest/deployments-buildpacks ',
+        '-i cflinuxfs2-rootfs-release=./spec/tasks/make-rootfs-smoke-test-manifest/cflinuxfs2-rootfs-release ',
         'DEPLOYMENT_NAME' => 'some_deployment')
     end
 
-    after(:context) do
-      `rm -rf ./spec/tasks/make-rootfs-smoke-test-manifest/deployments-buildpacks/.git`
-    end
-
     it 'adds the certs to the property_overrides.yml' do
-      manifest = run('cat /tmp/build/*/rootfs-smoke-test-manifest-artifacts/deployments/some_deployment/rootfs-smoke-test.yml').strip
+      manifest = run('cat /tmp/build/*/rootfs-smoke-test-manifest-artifacts/some_deployment/rootfs-smoke-test.yml').strip
       expect(manifest).to eq('manifest')
-    end
-
-    it 'commits the new deployment manifest' do
-      commit_msg = run('cd /tmp/build/*/rootfs-smoke-test-manifest-artifacts && git log -1 --format=oneline')
-      expect(commit_msg).to include('create rootfs with smoke test deployment manifest for some_deployment')
     end
   end
 end
