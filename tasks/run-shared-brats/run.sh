edf50e56d52a437f8f0165b572d13d7d1c058bf7
Anna Thornton <athornton@pivotal.io>
2017-02-16 10:33:57 -0500

    Use cf-space-resource for shared edge job
    - To ensure space deletion
    
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- tasks/run-shared-brats/run.sh
+++ tasks/run-shared-brats/run.sh
@@ -4,21 +4,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+SPACE=$(cat cf-space/name)
+
 pushd brats
   if [ ! -z "$RUBYGEM_MIRROR" ]; then
     bundle config mirror.https://rubygems.org "${RUBYGEM_MIRROR}"
   fi
   bundle install
   if [ -n "$CI_CF_PASSWORD" ]; then
-    space="brats-$LANGUAGE"
-
-    cf login -a "api.buildpacks-shared.cf-app.com" -u "$CI_CF_USERNAME" -p "$CI_CF_PASSWORD" -o pivotal -s "$space" --skip-ssl-validation || true
-
-    cf create-org pivotal
-    cf target -o pivotal
-
-    cf create-space "$space" -o pivotal
-    cf target -s "$space" -o pivotal
+    cf login -a "api.buildpacks-shared.cf-app.com" -u "$CI_CF_USERNAME" -p "$CI_CF_PASSWORD" -o pivotal -s "$SPACE" --skip-ssl-validation || true
   fi
 
   if [ -z "${STACK-}" ]; then
