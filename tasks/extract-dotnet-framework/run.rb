8f7e26231d32d02207bf17c906d5d037f2fad34f
Sam Smith <sesmith177@gmail.com>
2017-03-07 15:47:44 -0500

    Handle multiple dotnet-framework versions in SDK
    as of SDK v1.0.1, it now comes bundled with more than one version
    
    [#141229769]
    
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- tasks/extract-dotnet-framework/run.rb
+++ tasks/extract-dotnet-framework/run.rb
@@ -46,19 +46,18 @@ class ExtractDotnetFramework
   end
 
   def retar
-    dotnet_framework_versions = Dir["#{@dotnet_sdk_dir}/shared/Microsoft.NETCore.App/*"].map{ |f| Pathname.new(f).basename.to_s }
-
-    raise "Currently, we only expect one dotnet framework to be bundled with each sdk." if dotnet_framework_versions.size != 1
-
-    @dotnet_framework_version = dotnet_framework_versions.first
-
-    dotnet_framework_artifact_dir = File.expand_path(File.join(@buildpacks_ci_dir, '..', 'binary-builder-artifacts'))
-
-    @dotnet_framework_tar = File.join(dotnet_framework_artifact_dir, "dotnet-framework.#{@dotnet_framework_version}.linux-amd64.tar.gz")
+    @dotnet_framework_versions = Dir["#{@dotnet_sdk_dir}/shared/Microsoft.NETCore.App/*"].map{ |f| Pathname.new(f).basename.to_s }
 
     Dir.chdir(@dotnet_sdk_dir) do
-      raise "Tarring the dotnet framework failed" unless system("tar zcf #{@dotnet_framework_tar} shared host *.txt")
+      @dotnet_framework_verions.each do |version|
+        system("tar zcf #{dotnet_framework_tar(version)} shared host *.txt") or raise "Tarring the dotnet framework failed"
+      end
     end
+  end
+
+  def dotnet_framework_tar(version)
+    dotnet_framework_artifact_dir = File.expand_path(File.join(@buildpacks_ci_dir, '..', 'binary-builder-artifacts'))
+    File.join(dotnet_framework_artifact_dir, "dotnet-framework.#{version}.linux-amd64.tar.gz")
   end
 
   def write_yaml
@@ -69,35 +68,40 @@ class ExtractDotnetFramework
     framework_built_file = File.join(output_dir , 'binary-built-output', 'dotnet-framework-built.yml')
     framework_built = YAML.load_file(framework_built_file)
 
-    framework_built['dotnet-framework'].push({
-      'version' => @dotnet_framework_version,
-      'git-commit-sha' => @dotnet_sdk_git_sha,
-      'timestamp' => Time.now.utc.to_s
-    })
-
-    File.write(framework_built_file, framework_built.to_yaml)
-
-    md5sum = Digest::MD5.file(@dotnet_framework_tar).hexdigest
-    shasum = Digest::SHA256.file(@dotnet_framework_tar).hexdigest
-
-    git_msg = "Build dotnet-framework - #{@dotnet_framework_version}\n\n"
-
-    git_yaml = {
-      'filename' => File.basename(@dotnet_framework_tar),
-      'version' => @dotnet_framework_version,
-      'md5' => md5sum,
-      'sha256' => shasum,
-      'source url' => @dotnet_sdk_source_url,
-      'source git-commit-sha' => @dotnet_sdk_git_sha
-    }
-
-    git_msg += git_yaml.to_yaml
-
     Dir.chdir(output_dir) do
       GitClient.set_global_config('user.email', 'cf-buildpacks-eng@pivotal.io')
       GitClient.set_global_config('user.name', 'CF Buildpacks Team CI Server')
-      GitClient.add_file(framework_built_file)
-      GitClient.safe_commit(git_msg)
+    end
+
+    @dotnet_framework_versions.each do |version|
+      framework_built['dotnet-framework'].push({
+        'version' => version,
+        'git-commit-sha' => @dotnet_sdk_git_sha,
+        'timestamp' => Time.now.utc.to_s
+      })
+
+      File.write(framework_built_file, framework_built.to_yaml)
+
+      md5sum = Digest::MD5.file(dotnet_framework_tar(version)).hexdigest
+      shasum = Digest::SHA256.file(dotnet_framework_tar(version)).hexdigest
+
+      git_msg = "Build dotnet-framework - #{version}\n\n"
+
+      git_yaml = {
+        'filename' => File.basename(dotnet_framework_tar(version)),
+        'version' => version,
+        'md5' => md5sum,
+        'sha256' => shasum,
+        'source url' => @dotnet_sdk_source_url,
+        'source git-commit-sha' => @dotnet_sdk_git_sha
+      }
+
+      git_msg += git_yaml.to_yaml
+
+      Dir.chdir(output_dir) do
+        GitClient.add_file(framework_built_file)
+        GitClient.safe_commit(git_msg)
+      end
     end
   end
 end
