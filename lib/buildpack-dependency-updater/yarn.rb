7c19f26869a080bfeb0f6ba4e1ee14fee5c6523e
Sam Smith <sesmith177@gmail.com>
2017-03-08 11:31:03 -0500

    Clean up buildpack-dependency-updater
    - update default version of .NET SDK if sdk is msbuild
    - remove duplicated code in YarnDependencyUpdater
    
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- lib/buildpack-dependency-updater/yarn.rb
+++ lib/buildpack-dependency-updater/yarn.rb
@@ -1,12 +1,5 @@
 class BuildpackDependencyUpdater::Yarn < BuildpackDependencyUpdater
   def perform_dependency_specific_changes
-    buildpack_manifest['default_versions'].delete_if { |dep| dep['name'] == dependency }
-
-    default_dependency_hash = {
-        'name' => dependency,
-        'version' => dependency_version
-    }
-
-    buildpack_manifest['default_versions'] << default_dependency_hash
+    perform_default_versions_update
   end
 end
