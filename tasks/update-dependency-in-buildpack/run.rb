2457b6e3d2ca51a4dd77dc7b32addae176111d59
Dave Goddard <dave@goddard.id.au>
2017-03-08 14:09:54 -0500

    Change UpdateDependencyInBuildpack to output buildpack
    This simplfies re-using the buildpack rspec task
    
    [#141298707]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- tasks/update-dependency-in-buildpack/run.rb
+++ tasks/update-dependency-in-buildpack/run.rb
@@ -4,7 +4,7 @@
 buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 binary_built_out_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'built-out'))
 
-require_relative "#{buildpacks_ci_dir}/lib/update-dependency-in-buildpack-job.rb"
+require_relative "update-dependency-in-buildpack-job"
 
 update_job = UpdateDependencyInBuildpackJob.new(buildpacks_ci_dir, binary_built_out_dir)
 update_job.run!
