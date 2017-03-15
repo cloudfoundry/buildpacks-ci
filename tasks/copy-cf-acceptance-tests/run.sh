fd4b2e7a65ff94f908071dfd1c7fc605c8e1d358
Sam Smith <sesmith177@gmail.com>
2017-03-15 10:16:57 -0400

    Pull missing CATs commit before running CATs
    - Temporary fix while we wait for dev to progress to release-candidate
    
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- tasks/copy-cf-acceptance-tests/run.sh
+++ tasks/copy-cf-acceptance-tests/run.sh
@@ -6,4 +6,8 @@ set -o pipefail
 
 echo "Extracting cf-acceptance-tests from cf-release"
 
+pushd cf-release/src/github.com/cloudfoundry/cf-acceptance-tests/
+	git cherry-pick cb1a27ee39f4e91fd6fe394314bd0808b2a53f5e --no-commit
+popd
+
 rsync -a cf-release/src/github.com/cloudfoundry/cf-acceptance-tests/ cf-acceptance-tests/
