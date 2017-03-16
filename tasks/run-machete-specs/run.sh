b5897a4275be716324880ea977015128470d26b5
Dave Goddard <dave@goddard.id.au>
2017-03-16 10:23:34 -0400

    Convert machete job to using edge-shared
    - also cf-space-resource
    
    [#141817153]
    
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- tasks/run-machete-specs/run.sh
+++ tasks/run-machete-specs/run.sh
@@ -4,15 +4,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-DEPLOYMENT_NAME=$(cat cf-environments/name)
-
 pushd buildpacks-ci
   ./scripts/start-docker
 popd
 
-host="$DEPLOYMENT_NAME.$BOSH_LITE_DOMAIN_NAME"
+./cf-space/login
+
 pushd machete
-  ./scripts/cf_login_and_setup "$host"
   if [ ! -z "$RUBYGEM_MIRROR" ]; then
     bundle config mirror.https://rubygems.org "${RUBYGEM_MIRROR}"
   fi
