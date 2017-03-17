d60c6eb27091cf19f932c95ca967dd8952000875
Dave Goddard <dave@goddard.id.au>
2017-03-17 09:41:07 -0400

    UpdateNodeInRuby now supports node 4 and 6
    - Update both 4 and 6 major lines
    - Don't set default version
    - Don't update url mapping, it is now generic
    
    [#141167599]
    
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- lib/buildpack-dependency-updater/node.rb
+++ lib/buildpack-dependency-updater/node.rb
@@ -30,7 +30,7 @@ class BuildpackDependencyUpdater::Node < BuildpackDependencyUpdater
     original_dependencies = buildpack_manifest["dependencies"].clone
     new_dependencies = buildpack_manifest["dependencies"].clone
 
-    if buildpack == "nodejs" || (buildpack == "ruby" && major_version == "4") || (buildpack == 'dotnet-core' && major_version == '6')
+    if buildpack == "nodejs" || (buildpack == "ruby" && %w(4 6).include?(major_version)) || (buildpack == 'dotnet-core' && major_version == '6')
       new_dependencies = buildpack_manifest["dependencies"].delete_if { |dep| dep["name"] == dependency && dep["version"] == version_to_delete }
       dependency_hash = {
         "name" => dependency,
@@ -40,7 +40,6 @@ class BuildpackDependencyUpdater::Node < BuildpackDependencyUpdater
         "cf_stacks" => ["cflinuxfs2"]
       }
       buildpack_manifest["dependencies"] << dependency_hash
-      update_version_in_url_to_dependency_map if buildpack == "ruby"
     end
 
     @removed_versions = (original_dependencies - new_dependencies).map{|dep| dep['version']} unless new_dependencies == original_dependencies
@@ -49,24 +48,13 @@ class BuildpackDependencyUpdater::Node < BuildpackDependencyUpdater
   def perform_dependency_specific_changes
     major_version, _minor_version, _ = dependency_version.split(".")
 
-    # Make latest node 4.x.y version default node version for nodejs buildpack & ruby buildpack
+    # Make latest node 4.x.y version default node version for nodejs buildpack
     # Make latest node 6.x.y version default node version for dotnet-core buildpack
     update_default_versions = (buildpack == "nodejs" && major_version == "4") ||
-                              (buildpack == "ruby" && major_version == "4")   ||
                               (buildpack == 'dotnet-core' && major_version == '6')
 
     if update_default_versions
       perform_default_versions_update
     end
-  end
-
-  def update_version_in_url_to_dependency_map
-    buildpack_manifest["url_to_dependency_map"].delete_if { |dep| dep["name"] == dependency }
-    dependency_hash = {
-      "match" => "node",
-      "name" => dependency,
-      "version" => dependency_version
-    }
-    buildpack_manifest["url_to_dependency_map"] << dependency_hash
   end
 end
