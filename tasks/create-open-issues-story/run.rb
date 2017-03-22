2569e8a0f3c1c2239825bfb65f9cf961dcd85ca2
Dave Goddard <dave@goddard.id.au>
2017-03-22 08:36:15 -0400

    Remove Monday-morning PR and issues stories
    [#142065025]

--- tasks/create-open-issues-story/run.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-# encoding: utf-8
-
-buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
-require "#{buildpacks_ci_dir}/lib/open-github-story-creator"
-
-OpenGithubStoryCreator.create_issues_story
