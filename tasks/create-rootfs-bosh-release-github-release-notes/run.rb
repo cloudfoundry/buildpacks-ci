4f4aafef182837279da1f23a445db12ca0bb0aab
Dave Goddard <dave@goddard.id.au>
2017-04-03 16:02:22 -0400

    Rename the BOSH release cflinuxfs2-rootfs -> cflinuxfs2
    [#142964897]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- tasks/create-rootfs-bosh-release-github-release-notes/run.rb
+++ tasks/create-rootfs-bosh-release-github-release-notes/run.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 if ENV.fetch('ROOTFS_TYPE') == 'nc'
-  repo = 'pivotal-cf/stacks-nc'
+  repo = 'pivotal-cf/cflinusfs2-nc'
 else
   repo = 'cloudfoundry/cflinuxfs2'
 end
@@ -10,6 +10,6 @@ body_file = 'release-body/body'
 version = `cat version/number`
 github_url = "https://github.com/#{repo}/releases/tag/#{version}"
 
-message = "This release ships with #{repo.split('/').last} version #{version}. For more information, see this stack's [release notes](#{github_url})"
+message = "This release ships with #{repo.split('/').last} version #{version}. For more information, see the [release notes](#{github_url})"
 
 File.write(body_file, message)
