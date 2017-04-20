1493acf15ba5f95afc454ba68c5d12d9edb0714a
Sam Smith <sesmith177@gmail.com>
2017-04-20 11:03:34 -0400

    Suppress stdout during rspec test of swap-jwt-keys
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- spec/tasks/generate-cf-and-diego-manifests/swap-jwt-keys_spec.rb
+++ spec/tasks/generate-cf-and-diego-manifests/swap-jwt-keys_spec.rb
@@ -2,6 +2,7 @@
 require 'spec_helper.rb'
 require 'digest'
 require 'yaml'
+require 'open3'
 
 describe 'script for swapping out the default jwt keys in CF manifest' do
   let(:cf_release_dir)       { File.join(File.dirname(__FILE__), 'cf-release') }
@@ -9,14 +10,15 @@ describe 'script for swapping out the default jwt keys in CF manifest' do
   let(:swap_script_file)     { File.join(File.dirname(__FILE__), '..', '..','..', 'tasks', 'generate-cf-and-diego-manifests', 'swap-jwt-keys.rb') }
   let(:cf_manifest_file) { File.join(cf_release_dir, 'cf-with-jwt-signing-key.yml') }
 
-  before { FileUtils.cp(cf_manifest_file, cf_tmp_manifest_file) }
+  before do
+    FileUtils.cp(cf_manifest_file, cf_tmp_manifest_file)
+    output, status = Open3.capture2e(swap_script_file, cf_release_dir, cf_manifest_file)
+    raise output unless status.success?
+  end
 
   after { FileUtils.mv(cf_tmp_manifest_file, cf_manifest_file) }
 
-  subject { `#{swap_script_file} #{cf_release_dir} #{cf_manifest_file}` }
-
   it 'replaces the default jwt signing key with a generated SSH key' do
-    subject
     cf_manifest_contents = File.read(cf_manifest_file)
     cf_manifest_yaml = YAML.load(cf_manifest_contents)
     jwt = cf_manifest_yaml['properties']['uaa']['jwt']
