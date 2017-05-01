71fea0555f08922210b5cbf7fd86d18add35b0b3
Dave Goddard <dave@goddard.id.au>
2017-05-01 14:44:55 -0400

    PHP Notifications now include current PECL versions
    [#144601633]
    
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- tasks/check-for-latest-php-module-versions/run.rb
+++ tasks/check-for-latest-php-module-versions/run.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
+require 'yaml'
+
 buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 require "#{buildpacks_ci_dir}/lib/tracker-client"
 
@@ -15,28 +17,30 @@ labels = %w(maintenance)
 
 exit if name.empty?
 description += "\n\n" unless description.empty?
-description += "URL | Latest Version\n"
+description += "URL | Latest Version | PHP5 Current | PHP7 Current\n"
 description += "--- | ---\n"
 
+php5manifest = YAML.load(open('robots-repo/binary-builds/php-extensions.yml').read)
+php5 = Hash[*(php5manifest["extensions"].map {|e|[e["name"],e["version"]]}.flatten)]
+
+php7manifest = YAML.load(open('robots-repo/binary-builds/php7-extensions.yml').read)
+php7 = Hash[*(php7manifest["extensions"].map {|e|[e["name"],e["version"]]}.flatten)]
+
 Dir.glob('./*').each do |resource_dir|
+  next if resource_dir == './robots-repo'
   begin
     Dir.chdir(resource_dir) do
       url = File.read('url').strip
       version = File.read('version').strip
-      description += "#{url} | #{version}\n"
+      php5_current = php7_current = ''
+      if url =~ %r{https://pecl.php.net/package/(\S+)}
+        php5_current = php5[$1]
+        php7_current = php7[$1]
+      end
+      description += "#{url} | #{version} | #{php5_current} | #{php7_current}\n"
     end
   rescue
     puts "#{resource_dir} is not a new_version_resource"
-  end
-end
-Dir.glob('./*/tag').each do |tag_file|
-  begin
-    tag = File.read(tag_file)
-    version = File.read(tag_file.gsub(/tag$/,'version'))
-    source = tag_file.gsub(/\/tag$/,'').gsub(/^\.\//,'')
-    description += "#{source} | #{tag} -- #{version}\n"
-  rescue
-    description += "#{tag_file} | ERROR\n"
   end
 end
 
