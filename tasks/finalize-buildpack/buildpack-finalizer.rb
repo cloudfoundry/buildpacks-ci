4c1ba2352ecb9a5990347edc3e8999fceb1c81f8
Sam Smith <sesmith177@gmail.com>
2017-05-05 10:22:31 -0400

    Convert finalize-buildpack to use a class
    - so we can replace the specs that uses a concourse job with Ruby unit
    specs
    
    [#144882523]

---
+++ tasks/finalize-buildpack/buildpack-finalizer.rb
@@ -0,0 +1,68 @@
+# encoding: utf-8
+require 'digest'
+require 'fileutils'
+
+class BuildpackFinalizer
+
+  def initialize(artifact_dir, version, buildpack_repo_dir, cached_buildpack_dir)
+    @artifact_dir = artifact_dir
+    @version = version
+    @buildpack_repo_dir = buildpack_repo_dir
+    @cached_buildpack_dir = cached_buildpack_dir
+    @recent_changes_file = File.join(artifact_dir, 'RECENT_CHANGES')
+  end
+
+  def run
+    write_tag
+    write_changelog
+    write_dependencies
+    move_cached_buildpack
+  end
+
+  def write_tag
+    File.write(File.join(@artifact_dir, 'tag'), "v#{@version}")
+  end
+
+  def write_changelog
+    Dir.chdir(@buildpack_repo_dir) do
+      changes       = File.read('CHANGELOG')
+      recent_changes = changes.split(/^v[0-9\.]+.*?=+$/m)[1].strip
+
+      File.write(@recent_changes_file, "#{recent_changes}\n")
+    end
+  end
+
+  def write_dependencies
+    Dir.chdir(@buildpack_repo_dir) do
+      num_cores = `nproc`
+      system("BUNDLE_GEMFILE=cf.Gemfile bundle install --jobs=#{num_cores}")
+
+      File.write(@recent_changes_file, [
+        "\n\nPackaged binaries:\n",
+        `BUNDLE_GEMFILE=cf.Gemfile bundle exec buildpack-packager --list`,
+        "Default binary versions:\n",
+        `BUNDLE_GEMFILE=cf.Gemfile bundle exec buildpack-packager --defaults`
+      ].join("\n"), mode: 'a')
+    end
+  end
+
+  def move_cached_buildpack
+    Dir.chdir(@cached_buildpack_dir) do
+      Dir.glob('*.zip').map do |filename|
+        filename.match(/(.*)_buildpack-cached-v#{@version}\+.*.zip/) do |match|
+          _, language  = match.to_a
+          new_filename = "#{language}_buildpack-cached-v#{@version}.zip"
+          new_path     = File.join(@artifact_dir, new_filename)
+
+          FileUtils.mv(filename, new_path)
+
+          shasum = Digest::SHA256.file(new_path).hexdigest
+
+          # append SHA to RELEASE NOTES
+          File.write(@recent_changes_file, "  * SHA256: #{shasum}", mode: 'a')
+          File.write("#{new_path}.SHA256SUM.txt", "#{shasum}  #{new_filename}")
+        end
+      end
+    end
+  end
+end
