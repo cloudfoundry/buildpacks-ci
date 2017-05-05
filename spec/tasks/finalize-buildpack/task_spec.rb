8d5e0dd7e0cfde377693bbd3c142ff14cfb13485
Sam Smith <sesmith177@gmail.com>
2017-05-05 10:22:31 -0400

    Convert finalize-buildpack to use a class
    - so we can replace the specs that uses a concourse job with Ruby unit
    specs
    
    [#144882523]

--- spec/tasks/finalize-buildpack/task_spec.rb
+++ spec/tasks/finalize-buildpack/task_spec.rb
@@ -163,7 +163,7 @@ describe 'finalize-buildpack task' do
 
     it 'emits a SHA256.txt file' do
       output = run("cat /tmp/build/*/buildpack-artifacts/*.SHA256SUM.txt && echo '\t'")
-      expect(output).to include '6183bfc9d4c24cb123427b3b45d3b5ffa22d218adbe86cb48f204ce0a2c711fd  staticfile_buildpack-cached-v1.2.1.zip'
+      expect(output).to include '6183bfc9d4c24cb123427b3b45d3b5ffa22d218adbe86cb48f204ce0a2c711fd  staticfile_buildpack-cached-v1.2.2.zip'
     end
   end
 end
--- tasks/finalize-buildpack/run.rb
+++ tasks/finalize-buildpack/run.rb
@@ -1,43 +1,11 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-require 'digest'
-require 'fileutils'
+require_relative 'buildpack-finalizer'
 
-artifact_path = File.join(Dir.pwd, 'buildpack-artifacts')
+artifact_dir = File.join(Dir.pwd, 'buildpack-artifacts')
+version = File.read('buildpack/VERSION').strip
 
-tag = "v#{File.read('buildpack/VERSION')}"
-File.write(File.join(artifact_path, 'tag'), tag)
+buildpack_repo_dir = 'buildpack'
+cached_buildpack_dir = 'pivotal-buildpacks-cached'
 
-Dir.chdir('buildpack') do
-  num_cores = `nproc`
-  system("BUNDLE_GEMFILE=cf.Gemfile bundle install --jobs=#{num_cores}")
-
-  changes       = File.read('CHANGELOG')
-  recent_change = changes.split(/^v[0-9\.]+.*?=+$/m)[1].strip
-
-  File.write(File.join(artifact_path, 'RECENT_CHANGES'), [
-    recent_change,
-    "\n\nPackaged binaries:\n",
-    `BUNDLE_GEMFILE=cf.Gemfile bundle exec buildpack-packager --list`,
-    "Default binary versions:\n",
-    `BUNDLE_GEMFILE=cf.Gemfile bundle exec buildpack-packager --defaults`
-  ].join("\n"))
-end
-
-Dir.chdir('pivotal-buildpacks-cached') do
-  Dir.glob('*.zip').map do |filename|
-    filename.match(/(.*)_buildpack-cached-v(.*)\+.*.zip/) do |match|
-      _, language, version = match.to_a
-      new_filename = "#{language}_buildpack-cached-v#{version}.zip"
-      new_path     = File.join(artifact_path, new_filename)
-
-      FileUtils.mv(filename, new_path)
-
-      shasum = Digest::SHA256.file(new_path).hexdigest
-
-      # append SHA to RELEASE NOTES
-      File.write(File.join(artifact_path, 'RECENT_CHANGES'), "  * SHA256: #{shasum}", mode: 'a')
-      File.write("#{new_path}.SHA256SUM.txt", "#{shasum}  #{new_filename}")
-    end
-  end
-end
+BuildpackFinalizer.new(artifact_dir, version, buildpack_repo_dir, cached_buildpack_dir).run
