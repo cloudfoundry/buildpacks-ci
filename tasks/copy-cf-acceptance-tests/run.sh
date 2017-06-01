9482803f5ea759fb3815e82098aebef6a187da6b
Anna Thornton <athornton@pivotal.io>
2017-06-01 17:12:15 -0400

    Cherry pick commit to update ruby apps
    Signed-off-by: Keaty Gross <kgross@pivotal.io>
    Signed-off-by: Forest Eckhardt <forest.eckhardt@gmail.com>

--- tasks/copy-cf-acceptance-tests/run.sh
+++ tasks/copy-cf-acceptance-tests/run.sh
@@ -5,4 +5,9 @@ set -o nounset
 set -o pipefail
 
 echo "Extracting cf-acceptance-tests from cf-release"
+
+pushd cf-release/src/github.com/cloudfoundry/cf-acceptance-tests/
+  git cherry-pick 4731656e061ca14965a4f4fe47ee19bc6eaeeea2 --no-commit
+popd
+
 rsync -a cf-release/src/github.com/cloudfoundry/cf-acceptance-tests/ cf-acceptance-tests/
