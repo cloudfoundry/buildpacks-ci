759eed2d0245b695228de287e43d9469f5044981
Dave Goddard <dave@goddard.id.au>
2017-06-23 15:18:54 -0400

    Add job to update each buildpack on PWS
    Signed-off-by: Forest Eckhardt <forest.eckhardt@gmail.com>

---
+++ tasks/pws-update-buildpack/run.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+require 'open3'
+require 'fileutils'
+
+puts "Buildpack name: #{ENV['BUILDPACK_NAME']}\n"
+
+_, status = Open3.capture2e('cf', 'api', ENV['CF_API'])
+raise 'cf target failed' unless status.success?
+
+_, status = Open3.capture2e('cf','auth', ENV['USERNAME'], ENV['PASSWORD'])
+raise 'cf auth failed' unless status.success?
+
+puts "Original Buildpacks\n==================="
+system('cf', 'buildpacks')
+
+orig_filename = Dir.glob("pivotal-buildpacks-cached/#{ENV['BUILDPACK_NAME']}*.zip").first
+filename = orig_filename.gsub(/\+\d+\.zip$/, '.zip')
+
+FileUtils.mv(orig_filename, filename)
+
+puts filename
+
+_, status = Open3.capture2e('cf', 'update-buildpack', "#{ENV['BUILDPACK_NAME']}_buildpack", '-p', "#{filename}")
+raise 'cf update-buildpack failed' unless status.success?
