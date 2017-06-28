4d8dd42145d1f0faa54164a2796564af614ce659
Keaty Gross <kgross@pivotal.io>
2017-06-28 09:47:17 -0400

    Remove sec triage filter
    [#147876619]
    
    Signed-off-by: Forest Eckhardt <forest.eckhardt@gmail.com>

--- tasks/delete-unaffected-stories/run.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-require_relative './delete-unaffected-stories.rb'
-
-DeleteUnaffectedStories.new(
-  'tracker-filter-resource/data',
-  'cflinuxfs2/cflinuxfs2/cflinuxfs2_receipt',
-  'output/stories.json'
-).run
