b863e0e31f79b30458af13037d32fbad906d3129
Dave Goddard <dave@goddard.id.au>
2017-07-21 12:49:24 -0400

    Change to LOCAL_REPO_DIR before "git rev-parse HEAD"

--- tasks/set-status-success/run.rb
+++ tasks/set-status-success/run.rb
@@ -2,6 +2,7 @@
 require 'octokit'
 
 ## Required
+LOCAL_REPO_DIR = ENV.fetch('LOCAL_REPO_DIR') or raise 'Must supply env LOCAL_REPO_DIR'
 GITHUB_ACCESS_TOKEN = ENV.fetch('GITHUB_ACCESS_TOKEN') or raise 'Must supply env GITHUB_ACCESS_TOKEN'
 GITHUB_REPO = ENV.fetch('GITHUB_REPO') or raise 'Must supply env GITHUB_REPO'
 GITHUB_STATUS_CONTEXT = ENV.fetch('GITHUB_STATUS_CONTEXT') or raise 'Must supply env GITHUB_STATUS_CONTEXT'
@@ -14,7 +15,10 @@ Octokit.configure do |c|
   c.access_token = GITHUB_ACCESS_TOKEN
 end
 
-sha = `git rev-parse HEAD`.chomp
+sha = Dir.chdir(LOCAL_REPO_DIR) do
+  `git rev-parse HEAD`.chomp
+end
+
 Octokit.create_status(
   GITHUB_REPO,
   sha,
