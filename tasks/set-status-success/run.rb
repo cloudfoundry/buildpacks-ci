78911c7385f8e38b911c5789aad8ca6171ef45ae
Dave Goddard <dave@goddard.id.au>
2017-07-24 16:34:18 -0400

    SetGithubStatus uses fixed resource name ("repo")
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- tasks/set-status-success/run.rb
+++ tasks/set-status-success/run.rb
@@ -2,7 +2,6 @@
 require 'octokit'
 
 ## Required
-LOCAL_REPO_DIR = ENV.fetch('LOCAL_REPO_DIR') or raise 'Must supply env LOCAL_REPO_DIR'
 GITHUB_ACCESS_TOKEN = ENV.fetch('GITHUB_ACCESS_TOKEN') or raise 'Must supply env GITHUB_ACCESS_TOKEN'
 GITHUB_REPO = ENV.fetch('GITHUB_REPO') or raise 'Must supply env GITHUB_REPO'
 GITHUB_STATUS_CONTEXT = ENV.fetch('GITHUB_STATUS_CONTEXT') or raise 'Must supply env GITHUB_STATUS_CONTEXT'
@@ -15,7 +14,7 @@ Octokit.configure do |c|
   c.access_token = GITHUB_ACCESS_TOKEN
 end
 
-sha = Dir.chdir(LOCAL_REPO_DIR) do
+sha = Dir.chdir('repo') do
   `git rev-parse HEAD`.chomp
 end
 
