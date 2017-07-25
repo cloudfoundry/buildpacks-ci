a2df54702add523af096c6d2cb95f9d4468a3632
Anna Thornton <athornton@pivotal.io>
2017-07-25 11:07:19 -0400

    Remove affected labels when marking davos stories with fixed labels
    [#149486435]
    
    Signed-off-by: Forest Eckhardt <forest.eckhardt@gmail.com>

--- spec/tasks/finalize-security-notices/finalize-security-notices_spec.rb
+++ spec/tasks/finalize-security-notices/finalize-security-notices_spec.rb
@@ -14,16 +14,16 @@ describe FinalizeSecurityNoticeStories do
 
   before(:each) do
     allow(tracker_client).to receive(:search_with_filters).with(anything).and_return(affected_stories)
-    allow(tracker_client).to receive(:add_label_to_story).with('487', 'fixed-999.999')
-    allow(tracker_client).to receive(:add_label_to_story).with('987', 'fixed-999.999')
+    allow(tracker_client).to receive(:overwrite_label_on_story).with(affected_stories.first, 'fixed-999.999')
+    allow(tracker_client).to receive(:overwrite_label_on_story).with(affected_stories.last, 'fixed-999.999')
     allow(tracker_client).to receive(:change_story_state).with('487', 'finished')
     allow(tracker_client).to receive(:change_story_state).with('987', 'finished')
   end
 
   it "finishes any stories tagged 'affected-<version of rootfs>' in the 'started' state and tags them with 'fixed-<version of latest rootfs>" do
     expect(tracker_client).to receive(:search_with_filters).with(label: "affected-*", state: "started")
-    expect(tracker_client).to receive(:add_label_to_story).with(story: affected_stories.first, label: "fixed-999.999")
-    expect(tracker_client).to receive(:add_label_to_story).with(story: affected_stories.last, label: "fixed-999.999")
+    expect(tracker_client).to receive(:overwrite_label_on_story).with(story: affected_stories.first, existing_label_regex: /affected-*/, new_label: "fixed-999.999")
+    expect(tracker_client).to receive(:overwrite_label_on_story).with(story: affected_stories.last, existing_label_regex: /affected-*/, new_label: "fixed-999.999")
     expect(tracker_client).to receive(:change_story_state).with(story_id: "487", current_state: "delivered")
     expect(tracker_client).to receive(:change_story_state).with(story_id: "987", current_state: "delivered")
 
