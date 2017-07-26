f35b321b16555f63bf3c16db1ade31edb5e99074
Dave Goddard <dave@goddard.id.au>
2017-07-26 08:24:40 -0400

    Handle hwc-offline bosh release [#149497611]

--- lib/cf-release-common.rb
+++ lib/cf-release-common.rb
@@ -7,6 +7,8 @@ end
 
 def find_buildpack_key(blobs, language)
   case language
+  when 'hwc-offline'
+    blobs.keys.detect { |key| key =~ /hwc_buildpack-cached/ }
   when /offline/
     blobs.keys.detect { |key| key =~ /java-buildpack-offline/ }
   when /java/
