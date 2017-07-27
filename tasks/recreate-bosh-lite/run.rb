02688e23edddab3af4a9f2d8a56dd8c5dee8f5a8
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2017-07-27 09:49:03 -0400

    Revert "Update BoshLiteManager for targeting with BOSH 2 CLI"
    This reverts commit 3747155d93ef0713ac55d915d84203c53eb34a8f.

--- tasks/recreate-bosh-lite/run.rb
+++ tasks/recreate-bosh-lite/run.rb
@@ -3,6 +3,7 @@
 require_relative '../../lib/bosh-lite-manager.rb'
 require 'ostruct'
 
+iaas = ENV.fetch('IAAS')
 deployment_id = ENV.fetch('DEPLOYMENT_NAME')
 domain_name = ENV.fetch('BOSH_LITE_DOMAIN_NAME')
 bosh_lite_user = ENV.fetch('BOSH_USER')
@@ -11,14 +12,19 @@ bosh_private_key = ENV.fetch('BOSH_PRIVATE_KEY')
 
 bosh_lite_url = "https://#{deployment_id}.#{domain_name}"
 
-bosh_lite_deployment_name = ENV.fetch("GCP_BOSH_LITE_NAME")
-bosh_director_user = ENV.fetch("GCP_BOSH_DIRECTOR_USER")
-bosh_director_password = ENV.fetch("GCP_BOSH_DIRECTOR_PASSWORD")
-bosh_director_target = "10.0.0.6"
-deployments_location = 'buildpacks-ci'
-
-BOSH_DIRECTOR_CA_CERT_PATH="/tmp/director_ca_cert".freeze
-File.write(BOSH_DIRECTOR_CA_CERT_PATH, ENV.fetch("GCP_BOSH_DIRECTOR_CA_CERT"))
+if iaas == 'azure' || iaas == 'gcp'
+  bosh_lite_deployment_name = ENV.fetch("#{iaas.upcase}_BOSH_LITE_NAME")
+  bosh_director_user = ENV.fetch("#{iaas.upcase}_BOSH_DIRECTOR_USER")
+  bosh_director_password = ENV.fetch("#{iaas.upcase}_BOSH_DIRECTOR_PASSWORD")
+  bosh_director_target = "10.0.0.6"
+  deployments_location = 'buildpacks-ci'
+elsif iaas == 'aws'
+  bosh_lite_deployment_name = bosh_director_user = bosh_director_password = bosh_director_target = nil
+  deployments_location = 'deployments-buildpacks-artifacts'
+else
+  puts "Please specify IAAS=(aws|azure|gcp)"
+  exit 1
+end
 
 exit 1 unless system "rsync -a deployments-buildpacks/ deployments-buildpacks-artifacts"
 
@@ -33,7 +39,7 @@ credentials_struct = OpenStruct.new({
   'gcp_bosh_lite_postgres_password' => ENV.fetch('BOSH_LITE_POSTGRES_PASSWORD')
 })
 
-manager = BoshLiteManager.new(iaas: 'gcp',
+manager = BoshLiteManager.new(iaas: iaas,
                                deployment_dir: deployment_dir,
                                deployment_id: deployment_id,
                                bosh_lite_user: bosh_lite_user,
@@ -44,7 +50,6 @@ manager = BoshLiteManager.new(iaas: 'gcp',
                                bosh_director_password: bosh_director_password,
                                bosh_director_target: bosh_director_target,
                                bosh_private_key: bosh_private_key,
-                               bosh_director_ca_cert_path: BOSH_DIRECTOR_CA_CERT_PATH,
                                credentials_struct: credentials_struct
                               )
 
