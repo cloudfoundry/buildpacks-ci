888b76aa2f245765a8e3b2c902e32906bcb38ac5
Sam Coward <scoward@pivotal.io>
2017-07-27 14:46:57 -0400

    Add task for running BAL specs on windows
    We ensure tar is available on the PATH because the windows team removes
    it in the concourse-windows-release in order to work around some issues
    
    [#148103225]
    
    Signed-off-by: Forest Eckhardt <forest.eckhardt@gmail.com>

---
+++ tasks/run-bal-specs/run.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$env:CREDENTIAL_FILTER_WHITELIST="SystemDrive,SERVICE_ID,NUMBER_OF_PROCESSORS,PROCESSOR_LEVEL,WINSW_SERVICE_ID,__PIPE_SERVICE_NAME"
+
+$env:GOPATH="$PWD/garden-runc-release"
+$env:PATH=$env:GOPATH + "/bin;C:/go/bin;C:/var/vcap/bosh/bin;" + $env:PATH
+
+$buildDir=$env:GOPATH + "/src/code.cloudfoundry.org/buildpackapplifecycle"
+md -Force $buildDir
+
+echo "Moving buildpackapplifecycle onto the gopath..."
+cp bal-develop/* $buildDir -recurse
+
+push-location $buildDir
+
+  go get -t ./...
+  go get github.com/onsi/ginkgo/ginkgo
+
+  go get github.com/pivotal-cf-experimental/concourse-filter
+  push-location ../../github.com/pivotal-cf-experimental/concourse-filter
+    go build
+  pop-location
+
+  $(& ginkgo -r; $ExitCode="$LastExitCode") | concourse-filter
+
+pop-location
+
+Exit $ExitCode
