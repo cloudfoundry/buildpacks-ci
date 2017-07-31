7b7291a34d18497cff1ed1b3c03314e88d1e6385
Keaty Gross <kgross@pivotal.io>
2017-07-31 14:25:36 -0400

    Install ginkgo and mockgen from vendored version
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- tasks/update-libbuildpack/run.sh
+++ tasks/update-libbuildpack/run.sh
@@ -20,34 +20,40 @@ else
 fi
 
 pushd buildpack
-	pushd "$update_dir"
-		go get github.com/FiloSottile/gvt
-		go get github.com/golang/mock/gomock
-		go get github.com/golang/mock/mockgen
-		go get github.com/onsi/ginkgo/ginkgo
-		go get github.com/onsi/gomega
+  pushd "$update_dir"
+    go get github.com/FiloSottile/gvt
 
-		gvt update github.com/cloudfoundry/libbuildpack
-		go generate || true
-		[ -d hooks ] && (cd hooks && (go generate || true))
-		[ -d supply ] && (cd supply && (go generate || true))
-		[ -d finalize ] && (cd finalize && (go generate || true))
-		ginkgo -r
-	popd
+    pushd vendor/github.com/golang/mock/mockgen
+      go install
+    popd
+    pushd vendor/github.com/onsi/ginkgo/ginkgo
+      go install
+    popd
 
-	git add "$update_dir"
+    gvt update github.com/cloudfoundry/libbuildpack
 
-	set +e
-		git diff --cached --exit-code
-		no_changes=$?
-	set -e
+    go generate || true
+    [ -d compile ] && (cd compile && (go generate || true))
+    [ -d hooks ] && (cd hooks && (go generate || true))
+    [ -d supply ] && (cd supply && (go generate || true))
+    [ -d finalize ] && (cd finalize && (go generate || true))
 
-	if [ $no_changes -ne 0 ]
-	then
-		git commit -m "Update libbuildpack"
-	else
-		echo "libbuildpack is up to date"
-	fi
+    ginkgo -r
+  popd
+
+  git add "$update_dir"
+
+  set +e
+    git diff --cached --exit-code
+    no_changes=$?
+  set -e
+
+  if [ $no_changes -ne 0 ]
+  then
+    git commit -m "Update libbuildpack"
+  else
+    echo "libbuildpack is up to date"
+  fi
 popd
 
 rsync -a buildpack/ buildpack-artifacts
