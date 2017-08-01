ef57b2486cd9fde0b642d3e4bd9d4140d4d6ebb9
Dave Goddard <dave@goddard.id.au>
2017-08-01 08:09:56 -0400

    Use ginkgo for cutlass buildpacks on edge and lts
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

---
+++ tasks/run-buildpack-lts-ginkgo-specs/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+set -o errexit
+set -o nounset
+set -o pipefail
+
+DEPLOYMENT_NAME=$(cat cf-environments/name)
+cf login -a "api.$DEPLOYMENT_NAME.$BOSH_LITE_DOMAIN_NAME" -u admin -p "$CF_PASSWORD" --skip-ssl-validation
+cf create-org integration || true
+cf create-space integration -o integration || true
+cf target -o integration -s integration
+
+./buildpacks-ci/scripts/start-docker
+
+cd buildpack
+
+export GOPATH=$PWD
+export GOBIN=$PWD/.bin
+export PATH=$GOBIN:$PATH
+
+./scripts/unit.sh
+
+CACHED=true  ./scripts/integration.sh
+CACHED=false ./scripts/integration.sh
