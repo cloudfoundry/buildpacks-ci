2ee618ba80585690fa614784ea36ce602d7ab49c
Forest Eckhardt <forest.eckhardt@gmail.com>
2017-08-08 11:31:43 -0400

    Set date to 7 days ago
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- tasks/run-shared-brats/run.sh
+++ tasks/run-shared-brats/run.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+./cf-space/login
 SPACE=$(cat cf-space/name)
 
 pushd brats
@@ -11,9 +12,6 @@ pushd brats
     bundle config mirror.https://rubygems.org "${RUBYGEM_MIRROR}"
   fi
   bundle install
-  if [ -n "$CI_CF_PASSWORD" ]; then
-    cf login -a "api.buildpacks-shared.cf-app.com" -u "$CI_CF_USERNAME" -p "$CI_CF_PASSWORD" -o pivotal -s "$SPACE" --skip-ssl-validation || true
-  fi
 
   if [ -z "${STACK-}" ]; then
     bundle exec rspec cf_spec/integration --tag language:"${LANGUAGE}" --tag buildpack_branch:"${BUILDPACK_BRANCH}"
