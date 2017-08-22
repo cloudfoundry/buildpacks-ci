dd29e68fc52692376e5b6a735caedcf8ce74ef53
Sam Coward <scoward@pivotal.io>
2017-08-22 10:28:15 -0400

    Update cf-release pipeline to build cutlass buildpacks with new packager
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

---
+++ tasks/build-offline-buildpack-cutlass/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+tag=$(cat blob/tag)
+GITHUB_ORG=${GITHUB_ORG:-cloudfoundry}
+git clone "https://github.com/$GITHUB_ORG/$LANGUAGE-buildpack.git" source
+
+pushd source
+  git checkout "$tag"
+  git submodule update --init --recursive
+
+  (cd src/$LANGUAGE/vendor/github.com/cloudfoundry/libbuildpack/packager/buildpack-packager && go install)
+
+  buildpack-packager --cached
+popd
+
+# shellcheck disable=SC2035
+mv source/*_buildpack-cached*.zip buildpack-zip/
