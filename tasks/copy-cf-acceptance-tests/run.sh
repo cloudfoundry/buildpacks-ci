bc3a3cfbe1c73712875230911c732c3ac06843b1
Dave Goddard <dave@goddard.id.au>
2017-08-24 10:23:03 -0400

    Switch branch before extraction
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- tasks/copy-cf-acceptance-tests/run.sh
+++ tasks/copy-cf-acceptance-tests/run.sh
@@ -4,10 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "Extracting cf-acceptance-tests from cf-release"
-rsync -a cf-release/src/github.com/cloudfoundry/cf-acceptance-tests/ cf-acceptance-tests/
 if [ ! -z "$BRANCH" ]; then
   echo Checking out branch "$BRANCH"
-  cd cf-acceptance-tests
-  git checkout "$BRANCH"
+  pushd cf-release/src/github.com/cloudfoundry/cf-acceptance-tests
+    git checkout "$BRANCH"
+  popd
 fi
+
+echo "Extracting cf-acceptance-tests from cf-release"
+rsync -a cf-release/src/github.com/cloudfoundry/cf-acceptance-tests/ cf-acceptance-tests/
