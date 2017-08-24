bede2f2d7379f876f9e5c6d8f4f385879f0065d5
Dave Goddard <dave@goddard.id.au>
2017-08-24 09:39:29 -0400

    Allow changing branch when copying cf-acceptance-tests
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- tasks/copy-cf-acceptance-tests/run.sh
+++ tasks/copy-cf-acceptance-tests/run.sh
@@ -6,3 +6,8 @@ set -o pipefail
 
 echo "Extracting cf-acceptance-tests from cf-release"
 rsync -a cf-release/src/github.com/cloudfoundry/cf-acceptance-tests/ cf-acceptance-tests/
+if [ ! -z "$BRANCH" ]; then
+  echo Checking out branch "$BRANCH"
+  cd cf-acceptance-tests
+  git checkout "$BRANCH"
+fi
