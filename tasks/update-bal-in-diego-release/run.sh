d37d81e64064670d444bff4b38ddb99cb9644fca
Dave Goddard <dave@goddard.id.au>
2017-08-30 10:33:51 -0400

    Update buildpacklifecycle in diego-release
    After running CATs
    
    [#149860083]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

---
+++ tasks/update-bal-in-diego-release/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+set -x
+
+pushd bal-develop
+  BAL_DEV_SHA=$(git rev-parse HEAD)
+popd
+
+rsync -a diego-release updated-diego-release
+
+pushd updated-diego-release/src/code.cloudfoundry.org/buildpacklifecycle
+  git checkout $BAL_DEV_SHA
+  git add .
+popd
+
+git commit -m 'Update buildpacklifecycle'
