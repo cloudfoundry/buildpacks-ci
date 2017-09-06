50e9b9ec6686e162130b597e88e54595d282ff43
Dave Goddard <dave@goddard.id.au>
2017-09-06 17:02:15 -0400

    Absolutize paths
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- tasks/finalize-buildpack/run.rb
+++ tasks/finalize-buildpack/run.rb
@@ -8,8 +8,8 @@ version = File.read('buildpack/VERSION').strip
 buildpack_repo_dir = 'buildpack'
 uncached_buildpack_dir = 'pivotal-buildpacks'
 
-ENV['GOPATH']=buildpack_repo_dir
-ENV['GOBIN']="#{buildpack_repo_dir}/.bin"
-ENV['PATH']="#{buildpack_repo_dir}/.bin:#{ENV['PATH']}"
+ENV['GOPATH']=File.expand_path(buildpack_repo_dir)
+ENV['GOBIN']="#{ENV['GOPATH']}/.bin"
+ENV['PATH']="#{ENV['GOPATH']}/.bin:#{ENV['PATH']}"
 
 BuildpackFinalizer.new(artifact_dir, version, buildpack_repo_dir, uncached_buildpack_dir).run
