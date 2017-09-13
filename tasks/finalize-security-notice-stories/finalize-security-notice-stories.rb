838bc3faeca118f78e828e6262c2e5306c70c081
Dave Goddard <dave@goddard.id.au>
2017-09-13 11:08:05 -0400

    Output feedback on FinalizeSecurityNoticeStories in CI

--- tasks/finalize-security-notice-stories/finalize-security-notice-stories.rb
+++ tasks/finalize-security-notice-stories/finalize-security-notice-stories.rb
@@ -13,6 +13,7 @@ class FinalizeSecurityNoticeStories
   def run
     fixed_affected_stories = @tracker_client.search_with_filters(label: 'affected', state: 'started')
     fixed_affected_stories.each do |story|
+      puts "Changing label on #{story["id"]} from affected to fixed-#{@new_stack_version}"
       @tracker_client.overwrite_label_on_story(story: story, existing_label_regex: /affected/, new_label: "fixed-#{@new_stack_version}")
       @tracker_client.change_story_state(story_id: story["id"], current_state: 'delivered')
     end
