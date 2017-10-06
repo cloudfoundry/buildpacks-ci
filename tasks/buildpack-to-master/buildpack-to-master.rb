237ed54f58715f8521bcfb71f9f740a16caea54b
Dave Goddard <dave@goddard.id.au>
2017-10-06 11:31:54 -0400

    Revert "Temporary fix for apt-buildpack"
    This reverts commits 10b3b9850ace279001e2adac13e9082968cbfda9 and 83887c1229c9dfa8479d85c46db3519910772b0b

--- tasks/buildpack-to-master/buildpack-to-master.rb
+++ tasks/buildpack-to-master/buildpack-to-master.rb
@@ -59,7 +59,6 @@ class BuildpackToMaster
   end
 
   def no_other_changes?
-    return true if @github_repo =~ /apt-buildpack/
     files = GitClient.last_commit_files('repo').split("\n").sort
     puts "Files changes in commit are #{files.inspect}."
     return files == ["CHANGELOG", "VERSION"]
