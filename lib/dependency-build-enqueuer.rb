c6e2f39c3887a02977a5c79bc2cfa509c80e3aa5
Keaty Gross <kgross@pivotal.io>
2017-11-01 14:10:42 -0400

    Avoid shelling out to download and compute SHA256
    This will also fail correctly when download_url 404s
    
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- lib/dependency-build-enqueuer.rb
+++ lib/dependency-build-enqueuer.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require 'octokit'
 require 'yaml'
+require 'open-uri'
 require_relative 'git-client'
 
 class DependencyBuildEnqueuer
@@ -118,7 +119,6 @@ class DependencyBuildEnqueuer
 
   def self.shasum_256_verification(download_url)
     # only get the sha value and not the filename
-    shasum256 = `curl -sL #{download_url} | shasum -a 256 | cut -d " " -f 1`
-    ["sha256", shasum256.strip]
+    ["sha256", open(download_url) { |url| Digest::SHA256.hexdigest url.read } ]
   end
 end
