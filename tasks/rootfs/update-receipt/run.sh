b974cf0f911d657f8098681b1931c20a705e1593
Dave Goddard <dave@goddard.id.au>
2018-02-01 16:02:06 -0500

    fix for shellcheck
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- tasks/rootfs/update-receipt/run.sh
+++ tasks/rootfs/update-receipt/run.sh
@@ -4,11 +4,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-cp receipt-s3/${ROOTFS}_receipt-* rootfs/${ROOTFS}/${ROOTFS}_receipt
+cp receipt-s3/"${ROOTFS}"_receipt-* "rootfs/${ROOTFS}/${ROOTFS}_receipt"
 
 pushd rootfs
     version=$(cat ../version/number)
-    git add ${ROOTFS}/${ROOTFS}_receipt
+    git add "${ROOTFS}/${ROOTFS}_receipt"
 
     set +e
       git diff --cached --exit-code
