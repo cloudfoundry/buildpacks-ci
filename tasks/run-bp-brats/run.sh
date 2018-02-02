6da1b1b1dd36038e77102f5affd647e1b99ada19
Dave Goddard <dave@goddard.id.au>
2018-02-02 09:54:58 -0400

    Run pip install for php before ginkgo unit tests

--- tasks/run-bp-brats/run.sh
+++ tasks/run-bp-brats/run.sh
@@ -10,5 +10,12 @@ export GOPATH=$PWD
 export GOBIN=$PWD/.bin
 export PATH=$GOBIN:$PATH
 
+# for the PHP buildpack
+if [ -e run_tests.sh ]; then
+  export TMPDIR
+  TMPDIR=$(mktemp -d)
+  pip install -r requirements.txt
+fi
+
 ./scripts/unit.sh
 ./scripts/brats.sh
