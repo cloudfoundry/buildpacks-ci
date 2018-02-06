46845002292cf0dc0b118a1221ef4c73e4ccab17
Dave Goddard <dave@goddard.id.au>
2018-02-06 09:01:06 -0400

    Add "pip install" for PHP buildpack on update libbuildpack

--- tasks/update-libbuildpack/run.sh
+++ tasks/update-libbuildpack/run.sh
@@ -17,6 +17,12 @@ else
   update_dir="src/$LANGUAGE"
 fi
 
+# for the PHP buildpack
+if [ -e run_tests.sh ]; then
+  export TMPDIR=$(mktemp -d)
+  pip install -r requirements.txt
+fi
+
 pushd buildpack
   pushd "$update_dir"
     if [ -d vendor/github.com/golang/mock/mockgen ]; then
