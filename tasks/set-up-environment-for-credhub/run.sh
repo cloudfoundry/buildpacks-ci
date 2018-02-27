410295f31ce5b719ef20f846be8148b710380b6f
Andrew Meyer <ameyer@pivotal.io>
2018-02-27 12:19:15 -0500

    BAL pipeline fixes and updates
    - Upgrade BBL/cf-deployment-concourse-tasks to 5.x
    - Supply credhub user credential in CATs config
    - Allow access to UAA for CATs service broker app
    - Kill any ssh tunnel potentially set up by eval-ing bbl 5's print-env
    - Delete deployment via bosh-deployment resource
    - Pipeline now bbl-up/bbl-destroys BOSH
    
    [#155588007]
    
    Signed-off-by: Dave Goddard <dave@goddard.id.au>
    Signed-off-by: Andrew Meyer <ameyer@pivotal.io>
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- tasks/set-up-environment-for-credhub/run.sh
+++ tasks/set-up-environment-for-credhub/run.sh
@@ -12,12 +12,23 @@ cf auth "$CI_CF_USERNAME" "$CI_CF_PASSWORD"
 
 cf set-running-environment-variable-group '{"CREDHUB_API": "https://credhub.service.cf.internal:8844/"}'
 
-cd bbl-state
-eval "$(bbl print-env)"
+wget --quiet https://github.com/cloudfoundry/bosh-bootloader/releases/download/v5.11.5/bbl-v5.11.5_linux_x86-64
+chmod 755 bbl-v5.11.5_linux_x86-64
 
-bosh2 instances -d cf --json | jq '.Tables[0].Rows | map(select(.instance | startswith("credhub\/") )) | map({"protocol":"tcp", "ports":"8844", "description": "Allow credhub traffic", "destination": .ips})' > security_group.json
+pushd "bbl-state/$ENV_NAME"
+  eval "$(../../bbl-v5.11.5_linux_x86-64 print-env)"
+  bosh2 instances -d cf --json | jq '.Tables[0].Rows | map(select(.instance | startswith("credhub\/") )) | map({"protocol":"tcp", "ports":"8844", "description": "Allow credhub traffic", "destination": .ips})' > credhub_security_group.json
+  bosh2 instances -d cf --json | jq '.Tables[0].Rows | map(select(.instance | startswith("uaa\/") )) | map({"protocol":"tcp", "ports":"8443", "description": "Allow UAA traffic", "destination": .ips})' > uaa_security_group.json
+popd
 
-cf create-security-group credhub security_group.json
+# CATs Credhub tests need to talk to credhub to create and interpolate credentials
+cf create-security-group credhub "bbl-state/$ENV_NAME/credhub_security_group.json"
 cf bind-running-security-group credhub
 cf bind-staging-security-group credhub
 
+# CATs Credhub tests deploy a service broker app which needs to talk to UAA
+cf create-security-group uaa "bbl-state/$ENV_NAME/uaa_security_group.json"
+cf bind-running-security-group uaa
+cf bind-staging-security-group uaa
+
+pkill -f ssh
