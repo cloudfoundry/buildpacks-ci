0a89a837e10e337f8f996f9906a87b31082ce2f7
Andrew Meyer <ameyer@pivotal.io>
2018-02-27 12:19:15 -0500

    BAL pipeline fixes and updates
    - Upgrade BBL/cf-deployment-concourse-tasks to 5.x
    - Supply credhub user credential in CATs config
    - Allow access to UAA for CATs service broker app
    - Kill any ssh tunnel potentially set up by eval-ing bbl 5's print-env
    - Delete deployment via bosh-deployment resource
    - Pipeline now bbl-up/bbl-destroys BOSH
    
    [#155588007]
    
    Signed-off-by: Dave Goddard <dave@goddard.id.au>
    Signed-off-by: Andrew Meyer <ameyer@pivotal.io>
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- tasks/destroy-cf-deployment/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-
-set -o errexit
-set -o nounset
-set -o pipefail
-
-bosh2 -n delete-deployment -d "${DEPLOYMENT_NAME}"
--- tasks/write-cats-config/run.rb
+++ tasks/write-cats-config/run.rb
@@ -7,6 +7,7 @@ admin_password = ENV.fetch('CI_CF_PASSWORD')
 apps_domain = ENV.fetch('APPS_DOMAIN')
 diego_docker_on = ENV.fetch('DIEGO_DOCKER_ON')
 credhub_mode = ENV.fetch('CREDHUB_MODE')
+credhub_secret = ENV.fetch('CREDHUB_CLIENT_SECRET')
 
 cats_config = {
   "admin_password" => admin_password,
@@ -14,6 +15,7 @@ cats_config = {
   "api" => "api.#{apps_domain}",
   "apps_domain" => apps_domain,
   "credhub_mode" => credhub_mode,
+  "credhub_secret" => credhub_secret,
   "async_service_operation_timeout" => 1200,
   "backend" => "diego",
   "cf_push_timeout" => 600,
