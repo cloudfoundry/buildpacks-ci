73431776897e16b113b9be1474235242517039da
Andrew Meyer <ameyer@pivotal.io>
2018-03-01 16:51:14 -0500

    Use BBL 5.x in task to run rootfs smoke test
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- tasks/run-rootfs-smoke-test/run.sh
+++ tasks/run-rootfs-smoke-test/run.sh
@@ -6,8 +6,12 @@ set -o pipefail
 
 set -x
 
+wget --quiet https://github.com/cloudfoundry/bosh-bootloader/releases/download/v5.11.5/bbl-v5.11.5_linux_x86-64
+chmod 755 bbl-v5.11.5_linux_x86-64
+
 pushd "bbl-state/$ENV_NAME"
-  eval "$(bbl print-env)"
+  eval "$(../../bbl-v5.11.5_linux_x86-64 print-env)"
 popd
 
 bosh2 -d rootfs-smoke-test run-errand cflinuxfs2-smoke-test
+pkill -f ssh
