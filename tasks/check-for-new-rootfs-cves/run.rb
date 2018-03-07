fe4bd794946930fedfcffac7db0626c27fb08e3e
Dave Goddard <dave@goddard.id.au>
2018-03-07 13:04:49 -0400

    Remove CVEEmailPreparerAndGithubIssueNotifier
    [#155778738]
    
    Signed-off-by: Jackson Feeny <jacksonfeeny@gmail.com>

--- tasks/check-for-new-rootfs-cves/run.rb
+++ tasks/check-for-new-rootfs-cves/run.rb
@@ -6,12 +6,11 @@ buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..
 require "#{buildpacks_ci_dir}/lib/rootfs-cve-notifier"
 require "#{buildpacks_ci_dir}/lib/notifiers/cve-tracker-notifier"
 require "#{buildpacks_ci_dir}/lib/notifiers/cve-slack-notifier"
-require "#{buildpacks_ci_dir}/lib/notifiers/cve-email-preparer-and-github-issue-notifier"
 
 stacks_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'cflinuxfs2'))
 
 if ENV.fetch('STACK') == 'cflinuxfs2'
-  notifiers = [CVEEmailPreparerAndGithubIssueNotifier, CVESlackNotifier, CVETrackerNotifier]
+  notifiers = [CVESlackNotifier, CVETrackerNotifier]
   cves_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'output-new-cves', 'new-cve-notifications'))
 else
   raise "Unsupported stack: #{ENV.fetch('STACK')}"
