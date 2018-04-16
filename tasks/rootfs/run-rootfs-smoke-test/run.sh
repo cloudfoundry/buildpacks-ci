6f368a4677ff3751733e6475ab7c8949b33d1480
Tyler Phelan <tphelan@pivotal.io>
2018-04-16 12:08:34 -0400

    Dont reveal bbl credentials
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- tasks/rootfs/run-rootfs-smoke-test/run.sh
+++ tasks/rootfs/run-rootfs-smoke-test/run.sh
@@ -7,7 +7,10 @@ set -o pipefail
 set -x
 
 pushd "bbl-state/$ENV_NAME"
+  set +x
   eval "$(bbl print-env)"
+  set -x
+  trap "pkill -f ssh" EXIT
 popd
 
 bosh2 -d rootfs-smoke-test run-errand "${STACK}-smoke-test"
