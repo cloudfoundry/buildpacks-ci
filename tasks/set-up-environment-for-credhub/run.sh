1af4a0ae36bc40cdf8dbb70e34cf2b6811c08265
Tyler Phelan <tphelan@pivotal.io>
2018-04-16 11:41:06 -0400

    Deploy BAL env with windows2016 cf stack
    - test windows cells via CATs,
    - remove WATs
    
    [#156172179]
    
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- tasks/set-up-environment-for-credhub/run.sh
+++ tasks/set-up-environment-for-credhub/run.sh
@@ -13,7 +13,10 @@ cf auth "$CI_CF_USERNAME" "$CI_CF_PASSWORD"
 cf set-running-environment-variable-group '{"CREDHUB_API": "https://credhub.service.cf.internal:8844/"}'
 
 pushd "bbl-state/$ENV_NAME"
+  set +o xtrace
   eval "$(bbl print-env)"
+  set -o xtrace
+  trap "pkill -f ssh" EXIT
   bosh2 instances -d cf --json | jq '.Tables[0].Rows | map(select(.instance | startswith("credhub\/") )) | map({"protocol":"tcp", "ports":"8844", "description": "Allow credhub traffic", "destination": .ips})' > credhub_security_group.json
   bosh2 instances -d cf --json | jq '.Tables[0].Rows | map(select(.instance | startswith("uaa\/") )) | map({"protocol":"tcp", "ports":"8443", "description": "Allow UAA traffic", "destination": .ips})' > uaa_security_group.json
 popd
@@ -27,5 +30,3 @@ cf bind-staging-security-group credhub
 cf create-security-group uaa "bbl-state/$ENV_NAME/uaa_security_group.json"
 cf bind-running-security-group uaa
 cf bind-staging-security-group uaa
-
-pkill -f ssh
