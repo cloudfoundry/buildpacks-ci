b12f9e09d36896d224476565932cd7d025c3e38c
Leah Hanson <lhanson@pivotal.io>
2018-04-17 10:28:07 -0400

    wip; make-rootfs/test-rootfs use STACK param
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- tasks/make-rootfs/run.sh
+++ tasks/make-rootfs/run.sh
@@ -6,21 +6,19 @@ set -o pipefail
 
 set -x
 
-SUFFIX="${STACKS_SUFFIX-}"
-
 buildpacks-ci/scripts/start-docker
 
-pushd cflinuxfs2
+pushd "$STACK"
   make
 
-  versioned_stack_filename="../cflinuxfs2-artifacts/cflinuxfs2$SUFFIX-$(cat ../version/number).tar.gz"
-  mv cflinuxfs2.tar.gz "$versioned_stack_filename"
+  versioned_stack_filename="../$STACK-artifacts/$STACK-$(cat ../version/number).tar.gz"
+  mv "$STACK.tar.gz" "$versioned_stack_filename"
 
-  versioned_receipt_filename="../receipt-artifacts/cflinuxfs2_receipt$SUFFIX-$(cat ../version/number)"
+  versioned_receipt_filename="../receipt-artifacts/${STACK}_receipt-$(cat ../version/number)"
   echo "Rootfs SHASUM: $(sha1sum "$versioned_stack_filename" | awk '{print $1}')" > "$versioned_receipt_filename"
   echo "" >> "$versioned_receipt_filename"
-  cat cflinuxfs2/cflinuxfs2_dpkg_l.out >> "$versioned_receipt_filename"
+  cat "$STACK/${STACK}_dpkg_l.out" >> "$versioned_receipt_filename"
 
   which git
-  TERM=xterm-color git --no-pager diff cflinuxfs2/cflinuxfs2_receipt "$versioned_receipt_filename" || true
+  TERM=xterm-color git --no-pager diff "$STACK/${STACK}_receipt" "$versioned_receipt_filename" || true
 popd
