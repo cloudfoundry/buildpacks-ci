6ed570915cc3822e74f2825ee8b8de757b7c04db
Sam Coward <scoward@pivotal.io>
2018-04-17 15:58:13 -0400

    Add stack to relases if not there
    Signed-off-by: Leah Hanson <lhanson@pivotal.io>

--- tasks/overwrite-rootfs-release/run.sh
+++ tasks/overwrite-rootfs-release/run.sh
@@ -27,7 +27,7 @@ popd
 cat <<EOF > ${release_dir}/use-dev-release-opsfile.yml
 ---
 - type: replace
-  path: /releases/name=$STACK
+  path: /releases/name=$STACK?
   value:
     name: $STACK
     version: ${version}
