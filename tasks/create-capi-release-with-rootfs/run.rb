6ed0bb65a465c6e5f09ad3c32eeda53cb0c2ccbf
Sam Coward <scoward@pivotal.io>
2018-04-17 14:34:43 -0400

    wip; fix paths, remove silly debug
    Signed-off-by: Leah Hanson <lhanson@pivotal.io>

--- tasks/create-capi-release-with-rootfs/run.rb
+++ tasks/create-capi-release-with-rootfs/run.rb
@@ -1,15 +1,13 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-puts 'whats going on'
 require 'yaml'
-puts 'i dunno'
-stack = ENV.fetch('STACK', "whats this in here?")
+stack = ENV.fetch('STACK')
 
 puts "Creating BOSH release capi with #{stack}"
 version = "212.0.#{Time.now.strftime('%s')}"
 
 %w[cloud_controller_clock cloud_controller_ng cloud_controller_worker nsync stager].each do |job|
-  specfile = "capi-release/jobs/#{job}/spec"
+  specfile = "../capi-release/jobs/#{job}/spec"
   spec = YAML.safe_load(File.read(specfile))
   if spec['properties']['cc.diego.lifecycle_bundles']['default'].keys.grep(/#{stack}/).none?
     spec['properties']['cc.diego.lifecycle_bundles']['default']["buildpack/#{stack}"] = 'buildpack_app_lifecycle/buildpack_app_lifecycle.tgz'
@@ -17,11 +15,11 @@ version = "212.0.#{Time.now.strftime('%s')}"
   File.write(specfile, YAML.dump(spec))
 end
 
-FileUtils.cp_r 'capi-release', 'capi-release-artifacts'
+FileUtils.cp_r '../capi-release', '../capi-release-artifacts'
 
 puts "Running 'bosh create release' in capi-release"
 
-Dir.chdir('capi-release-artifacts') do
+Dir.chdir('../capi-release-artifacts') do
   puts `bosh2 create-release --force --tarball "dev_releases/capi/capi-$version.tgz" --name capi --version "#{version}"`
 
   File.write('use-dev-release-opsfile.yml', "---
