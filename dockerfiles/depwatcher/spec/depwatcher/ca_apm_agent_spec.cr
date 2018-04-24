56ebd494761d08cdab90767ed810d0e53ad8be53
Leah Hanson <lhanson@pivotal.io>
2018-04-24 15:06:21 -0400

    Add CA APM PHP Agent depwatcher.
    - ran unit tests
    - copied from httpd and rlang depwatchers
    
    [#156633749]
    
    Signed-off-by: Jackson Feeny <jacksonfeeny@gmail.com>

---
+++ dockerfiles/depwatcher/spec/depwatcher/ca_apm_agent_spec.cr
@@ -0,0 +1,25 @@
+require "spec2"
+require "./httpclient_mock"
+require "../../src/depwatcher/ca_apm_agent"
+
+Spec2.describe Depwatcher::CaApmAgent do
+  let(client) { HTTPClientMock.new }
+  subject { described_class.new.tap { |s| s.client = client } }
+  before do
+    client.stub_get("https://ca.bintray.com/apm-agents/", File.read(__DIR__+"/../fixtures/apm-agents.html"))
+  end
+
+  describe "#check" do
+    it "returns real releases sorted" do
+      expect(subject.check.map(&.ref)).to eq ["10.6.0", "10.7.0"]
+    end
+  end
+
+  describe "#in" do
+    it "returns real releases sorted" do
+      obj = subject.in("10.6.0")
+      expect(obj.ref).to eq "10.6.0"
+      expect(obj.url).to eq "https://ca.bintray.com/apm-agents/CA-APM-PHPAgent-10.6.0_linux.tar.gz"
+    end
+  end
+end
