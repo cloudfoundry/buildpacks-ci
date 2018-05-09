eb9235c23aa32279385fb32b0862aa096be4e0b5
Tyler Phelan <tphelan@pivotal.io>
2018-05-09 17:59:43 -0400

    Use CAAPM as dependency name in binary-builder-new
    [#157443324]
    
    Signed-off-by: Jackson Feeny <jacksonfeeny@gmail.com>

--- tasks/build-binary-new/build.rb
+++ tasks/build-binary-new/build.rb
@@ -54,9 +54,17 @@ when 'pipenv'
     sha256: sha,
     url: "https://buildpacks.cloudfoundry.org/dependencies/#{name}/#{filename}"
   })
-when 'appdynamics', 'ca_apm_php_agent'
+when 'CAAPM', 'appdynamics'
+  res = open(url).read
+  sha = Digest::SHA256.hexdigest(res)
+  if data.dig('version', 'md5_digest') && Digest::MD5.hexdigest(res) != data.dig('version', 'md5_digest')
+    raise "MD5 digest does not match version digest"
+  elsif data.dig('version', 'sha256') && sha != data.dig('version', 'sha256')
+    raise "SHA256 digest does not match version digest"
+  end
+
   out_data.merge!({
-    sha256: data.dig('version', 'sha256'),
+    sha256: sha,
     url: url
   })
 when 'setuptools', 'rubygems', 'yarn', 'pip'
