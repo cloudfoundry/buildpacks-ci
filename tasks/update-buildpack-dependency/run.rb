6abca98c8d92442f46cf072de7aa1611bb0bc827
Leah Hanson <lhanson@pivotal.io>
2018-05-09 10:54:35 -0400

    Write extra file when updating appdynamics in php.
    Prevents a build breakage when app dynamics is updated by updating the version in the install script as well as in the manifest.
    
    [#157405810]
    
    Signed-off-by: Jackson Feeny <jacksonfeeny@gmail.com>

--- tasks/update-buildpack-dependency/run.rb
+++ tasks/update-buildpack-dependency/run.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'json'
+require 'regexp'
 require 'yaml'
 require_relative './dependencies'
 
@@ -33,6 +34,19 @@ if added.length == 0 && !rebuilt
   exit 0
 end
 
+path_to_extensions = 'extensions/appdynamics/extension.py'
+write_extensions = ''
+if !rebuilt && name == 'appdynamics' && manifest['language'] == 'php'
+  if removed.length == 1 && added.length == 1
+    text = File.read('buildpack/' + path_to_extensions)
+    write_extensions = text.gsub(/#{Regexp.quote(removed.first)}/, added.first)
+  else
+   puts 'Expected to have one added version and one removed version for appdynamics in the PHP buildpack.'
+   puts 'Got added (#{added}) and removed (#{removed}).'
+   exit 1
+  end
+end
+
 commit_message = "Add #{name} #{version}"
 if rebuilt
   commit_message = "Rebuild #{name} #{version}"
@@ -46,7 +60,12 @@ Dir.chdir('artifacts') do
   GitClient.set_global_config('user.name', 'CF Buildpacks Team CI Server')
 
   File.write('manifest.yml', manifest.to_yaml)
-
   GitClient.add_file('manifest.yml')
+
+  if write_extensions != ''
+    File.write(path_to_extensions, write_extensions)
+    GitClient.add_file(path_to_extensions)
+  end
+
   GitClient.safe_commit("#{commit_message} [##{story_id}]")
 end
