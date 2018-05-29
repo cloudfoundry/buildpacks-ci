3fb6304b95ed9014fc291cdc6f44a9c5a910b837
Jackson Feeny <jacksonfeeny@gmail.com>
2018-05-29 13:56:13 -0400

    Add dotnet framework depwatcher
    [#157791342]
    
    Signed-off-by: Tyler Phelan <tphelan@pivotal.io>

---
+++ dockerfiles/depwatcher/spec/depwatcher/dotnet_framework_spec.cr
@@ -0,0 +1,24 @@
+require "spec2"
+require "./httpclient_mock"
+require "../../src/depwatcher/dotnet_framework.cr"
+
+Spec2.describe Depwatcher::DotnetFramework do
+  let(client) {HTTPClientMock.new}
+  subject {described_class.new.tap {|s| s.client = client}}
+  before do
+    client.stub_get("https://api.github.com/repos/cloudfoundry/public-buildpacks-ci-robots/contents/binary-builds-new/dotnet-framework", File.read(__DIR__ + "/../fixtures/dotnet_framework.json"))
+  end
+
+  describe "#check" do
+    it "returns real releases sorted" do
+      expect(subject.check().map(&.ref)).to eq ["1.0.11", "1.1.8", "2.1.0-rc1-26423-06"]
+    end
+  end
+
+  describe "#in" do
+    it "returns real releases sorted" do
+      obj = subject.in("1.0.11")
+      expect(obj.ref).to eq "1.0.11"
+    end
+  end
+end
