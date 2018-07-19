d74fdee673b71aa4fdda3ae7964c92a034c211ce
Tyler Phelan <tphelan@pivotal.io>
2018-07-19 12:30:09 -0400

    Update make-rootfs task to create receipt correctly
    [#157725021]
    
    Signed-off-by: Danny Joyce <djoyce@pivotal.io>

--- tasks/make-rootfs/run.sh
+++ tasks/make-rootfs/run.sh
@@ -9,15 +9,17 @@ set -x
 buildpacks-ci/scripts/start-docker
 
 pushd rootfs
+  old_receipt_copy="receipt.${STACK}.x86_64.copy"
+  mv "receipt.${STACK}.x86_64" "$old_receipt_copy"
+
   make --always-make NAME="$STACK"
 
   versioned_stack_filename="../rootfs-artifacts/$STACK-$(cat ../version/number).tar.gz"
   mv "$STACK.x86_64.tar.gz" "$versioned_stack_filename"
 
   versioned_receipt_filename="../receipt-artifacts/receipt.${STACK}.x86_64-$(cat ../version/number)"
-  echo "Rootfs SHASUM: $(sha1sum "$versioned_stack_filename" | awk '{print $1}')" > "$versioned_receipt_filename"
-  echo "" >> "$versioned_receipt_filename"
+  mv "receipt.${STACK}.x86_64" "$versioned_receipt_filename"
 
   which git
-  TERM=xterm-color git --no-pager diff "receipt.${STACK}.x86_64" "$versioned_receipt_filename" || true
+  TERM=xterm-color git --no-pager diff "$old_receipt_copy" "$versioned_receipt_filename" || true
 popd
