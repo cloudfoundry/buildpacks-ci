19271aa37b58f6394a27d36aae0443a297c03f41
Tyler Phelan <tphelan@pivotal.io>
2018-07-19 17:02:41 -0400

    Add STACK to rootfs github release notes.
    Signed-off-by: Danny Joyce <djoyce@pivotal.io>

--- tasks/create-rootfs-bosh-release-github-release-notes/run.rb
+++ tasks/create-rootfs-bosh-release-github-release-notes/run.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 
-repo = 'cloudfoundry/cflinuxfs2'
+stack = ENV['STACK']
+repo = "cloudfoundry/#{stack}"
 
 body_file = 'release-body/body'
 version = `cat version/number`
 github_url = "https://github.com/#{repo}/releases/tag/#{version}"
 
-message = "This release ships with #{repo.split('/').last} version #{version}. For more information, see the [release notes](#{github_url})"
+message = "This release ships with #{stack} version #{version}. For more information, see the [release notes](#{github_url})"
 
 File.write(body_file, message)
