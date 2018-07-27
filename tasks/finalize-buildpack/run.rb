375d365ecc73b0b628df6266c2e02846db943f5b
Tyler Phelan <tphelan@pivotal.io>
2018-07-27 14:59:25 -0400

    Update finalizer to work with multiple stacks
    [#144940817]

--- tasks/finalize-buildpack/run.rb
+++ tasks/finalize-buildpack/run.rb
@@ -6,10 +6,10 @@ artifact_dir = File.join(Dir.pwd, 'buildpack-artifacts')
 version = File.read('buildpack/VERSION').strip
 
 buildpack_repo_dir = 'buildpack'
-uncached_buildpack_dir = 'pivotal-buildpacks'
+uncached_buildpack_dirs = Dir.glob('pivotal-buildpacks*')
 
 ENV['GOPATH']=File.expand_path(buildpack_repo_dir)
 ENV['GOBIN']="#{ENV['GOPATH']}/.bin"
 ENV['PATH']="#{ENV['GOBIN']}:#{ENV['PATH']}"
 
-BuildpackFinalizer.new(artifact_dir, version, buildpack_repo_dir, uncached_buildpack_dir).run
+BuildpackFinalizer.new(artifact_dir, version, buildpack_repo_dir, uncached_buildpack_dirs).run
