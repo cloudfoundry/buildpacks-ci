80fb9dfe1db4b901b099e646a67b9484714b9db0
Leah Hanson <lhanson@pivotal.io>
2018-07-31 10:43:28 -0400

    Make cf-release actually build dev releases based on github buildpacks.
    - Create uncached buildpack for each stack
    - Put new buildpack blobs into bosh release before building dev release
    
    [#159222878]
    
    Signed-off-by: Jackson Feeny <jacksonfeeny@gmail.com>

--- tasks/create-bump-buildpacks-opsfile/run.rb
+++ tasks/create-bump-buildpacks-opsfile/run.rb
@@ -2,13 +2,47 @@
 # encoding: utf-8
 
 require 'fileutils'
+require 'open3'
 require 'yaml'
 
 version = "0.#{Time.now.to_i}"
 
 replacements = []
-Dir.glob("*-buildpack-github-release").each do |github_release|
-  release_name = github_release.gsub("-github-release", "")
+Dir.glob('*-buildpack-github-release').each do |github_release|
+  release_name = github_release.gsub('-github-release', '')
+  language = release_name.gsub('-buildpack', '')
+
+  stacks = %w[cflinuxfs2 cflinuxfs3]
+  stacks << %w[windows2012R2 windows2016] if release_name == 'binary-buildpack'
+
+  ## Build new buildpack from master for each stack
+  stacks.each do |stack|
+    system(%(buildpack-packager build --uncached --stack=#{stack}`)) || raise("cannot package buildpack #{release_name} #{stack}")
+  end
+
+  ## Bump blobs in bosh release
+  Dir.chdir("#{release_name}-bosh-release") do
+    ## Clean out existing blobs
+    system(%(rm -rf blobs) || raise("can't remove blobs"))
+    if File.exists('config/blobs.yml')
+      File.open('config/blobs.yml', 'w') { |file| file.write("---\n{}") }
+    end
+    cmd = "bosh2 blobs | grep -- '-buildpack/.*buildpack' | awk '{print $1}'"
+    Open3.popen3(cmd) do |_, stdout, _, _|
+      stdout.lines.each do |line|
+        system(%(bosh2 remove-blob #{line}))
+      end
+    end
+
+    ## Add new blobs for new buildpacks
+    Dir.glob("../#{github_release}/#{language}_buildpack-*.zip") do |blob|
+      system(%(bosh2 -n add-blob #{blob} #{release_name}/#{File.basename(blob)})) || raise("cannot add blob #{blob} to #{release_name}")
+    end
+
+    # Create release and copy to built-buildpacks-artifacts
+    system(%(bosh2 create-release --force --tarball dev_releases/#{release_name}/#{release_name}-#{version}.tgz --name #{release_name} --version #{version})) || raise("cannot create #{release_name} #{version}")
+    system(%(cp dev_releases/*/*.tgz ../built-buildpacks-artifacts/))
+  end
 
   release_replacement = {
     "path" => "/releases/name=#{release_name}",
@@ -18,11 +52,6 @@ Dir.glob("*-buildpack-github-release").each do |github_release|
       "version" => version
     }
   }
-  Dir.chdir("#{release_name}-bosh-release") do
-    system(%(bosh2 --parallel 10 sync-blobs && bosh2 create-release --force --tarball dev_releases/#{release_name}/#{release_name}-#{version}.tgz --name #{release_name} --version #{version})) || raise("cannot create #{release_name} #{version}")
-    system(%(cp dev_releases/*/*.tgz ../built-buildpacks-artifacts/))
-  end
-
   replacements << release_replacement
 end
 
@@ -38,4 +67,3 @@ replacements << {
 }
 
 File.open("bump-buildpacks-opsfile/opsfile.yml", 'w') {|f| f.write replacements.to_yaml }
-
