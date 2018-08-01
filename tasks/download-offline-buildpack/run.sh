d0e0c21d5de1daf9047c387edc3c2472a3114c88
Leah Hanson <lhanson@pivotal.io>
2018-08-01 17:31:40 -0400

    Build stack associated java buildpack bosh releases
    [#144940817]
    
    Signed-off-by: Tyler Phelan <tphelan@pivotal.io>

--- tasks/download-offline-buildpack/run.sh
+++ tasks/download-offline-buildpack/run.sh
@@ -7,11 +7,16 @@ set -o pipefail
 ls -l pivnet-production
 
 pushd pivnet-production
-    echo "stack: cflinuxfs2" > manifest.yml
     old_name="$(echo ./*buildpack-offline*.zip)"
-    zip "$old_name" manifest.yml
-    new_name="${old_name//-offline/-offline-$CF_STACK}"
-    mv "$old_name" "$new_name"
+    # shellcheck disable=SC2128
+    for stack in $CF_STACKS; do
+      echo "stack: $stack" > manifest.yml
+      new_name="${old_name//-offline/-offline-$stack}"
+      cp "$old_name" "$new_name"
+      zip "$new_name" manifest.yml
+      rm manifest.yml
+    done
+    rm $old_name
 popd
 
 # shellcheck disable=SC2035
