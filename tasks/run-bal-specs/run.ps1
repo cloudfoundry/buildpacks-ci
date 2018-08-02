088307057ed74c5121e29aa4934e12efc7dd9c03
Leah Hanson <lhanson@pivotal.io>
2018-08-02 16:04:25 -0400

    Make unit specs run for bal on Windows.
    - Install chocolatey package manager, in order to install git.
    - Don't install concourse-filter because it wouldn't chain with ginkgo
    - Use windows paths to run ginkgo because it isn't ending up on the path correctly.
    
    [#159240864]

--- tasks/run-bal-specs/run.ps1
+++ tasks/run-bal-specs/run.ps1
@@ -3,44 +3,32 @@ $env:GOPATH="C:\go-" + (-join ((48..57) + (97..122) | Get-Random -Count 6 | % {[
 trap { $host.SetShouldExit(1); cmd.exe /c rd /q /s $env:GOPATH }
 
 $env:CREDENTIAL_FILTER_WHITELIST="SystemDrive,SystemRoot,SERVICE_ID,NUMBER_OF_PROCESSORS,PROCESSOR_LEVEL,WINSW_SERVICE_ID,__PIPE_SERVICE_NAME,GOPATH,USERPROFILE"
-
 $env:PATH=$env:GOPATH + "/bin;C:/go/bin;C:/var/vcap/bosh/bin;" + $env:PATH
-
 $buildDir=$env:GOPATH + "/src/code.cloudfoundry.org/buildpackapplifecycle"
 md -Force $buildDir
-
-echo "Moving buildpackapplifecycle onto the gopath..."
 cp bal-develop/* $buildDir -recurse
 
-# install git
-echo "#### Attempting to install git"
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
-$urlPath = "https://github.com/git-for-windows/git/releases/download/v2.18.0.windows.1/Git-2.18.0-64-bit.exe"
-$urlSplit = $urlPath.split('/')
-$filename = (Resolve-Path .\).ToString() + '\' + $urlSplit[$urlSplit.length - 1]
-$webclient = New-Object "System.Net.WebClient"
-$webclient.DownloadFile($urlPath, $filename)
-invoke-item $filename
-echo "#### Maybe installed git"
+echo "Installing chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+echo "Installing git"
+choco install -y git --force
 
 push-location $buildDir
-
+ # New-Item -ItemType directory -Path C:\Windows\system32\config\systemprofile\AppData\Local\Temp
   go get -t ./...
   go get github.com/onsi/ginkgo/ginkgo
-
-  go get github.com/pivotal-cf-experimental/concourse-filter
-  push-location ../../github.com/pivotal-cf-experimental/concourse-filter
+  push-location ../../github.com/onsi/ginkgo
     go build
   pop-location
 
-  $(& ginkgo -r; $ExitCode="$LastExitCode") | concourse-filter
-
+  echo "Running tests..."
+  $(& "$env:GOPATH\bin\ginkgo.exe" -r -race; $ExitCode="$LastExitCode")
   if ($ExitCode) {
     echo "Running tests for windows2012R2 tag..."
     $env:TAR_URL="https://s3.amazonaws.com/bosh-windows-dependencies/tar-1503683828.exe"
-    $(& ginkgo -tags windows2012R2 -r; $ExitCode="$LastExitCode") | concourse-filter
+    $(& "$env:GOPATH\bin\ginkgo.exe" -tags windows2012R2 -r; $ExitCode="$LastExitCode")
   }
-
 pop-location
 
 cmd.exe /c rd /q /s $env:GOPATH
