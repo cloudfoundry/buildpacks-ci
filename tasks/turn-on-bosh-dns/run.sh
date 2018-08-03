33bb0804945b13bcdb00fab1bd99b6187d3eb75f
Leah Hanson <lhanson@pivotal.io>
2018-08-03 15:24:26 -0400

    Add debugging for enabling bosh dns
    Signed-off-by: Andrew Meyer <ameyer@pivotal.io>

--- tasks/turn-on-bosh-dns/run.sh
+++ tasks/turn-on-bosh-dns/run.sh
@@ -3,7 +3,10 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -x
 
+echo "Before sourcing..."
 source cf-deployment-concourse-tasks/shared-functions
-
+echo "After sourcing..."
 bosh_update_dns_runtime_config
+echo "After config update..."
