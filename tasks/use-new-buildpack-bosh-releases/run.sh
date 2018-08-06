9571a36a22b15f3fadbc31d1090b93e53cf4ad3b
Andrew Meyer <ameyer@pivotal.io>
2018-08-06 16:15:14 -0400

    Use latest buildpack releases in cflinuxfs3 [#145881065]
    Signed-off-by: Tyler Phelan <tphelan@pivotal.io>

---
+++ tasks/use-new-buildpack-bosh-releases/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+version=$(date +%s)
+
+for language in binary dotnet-core go java nodejs php python ruby staticfile; do
+  pushd "$language-buildpack-release"
+    bosh2 create-release --tarball "../built-buildpacks-artifacts/$language-$version.tgz" --name "$language" --version "$version"
+  popd
+
+  cat <<EOF >> buildpacks-opsfile/use-latest-buildpack-releases.yml
+- path: /releases/name=$language-buildpack/version?
+  type: replace
+  value: $version
+
+EOF
+done
+
+
