c21e09aff6ef67fdb20e07f4ef231152866ea076
Leah Hanson <lhanson@pivotal.io>
2018-08-06 10:18:57 -0400

    Update write-pivnet-metadata to allow stacks.
    - finalize-buildpack expects to get one or more stack-specific inputs.
    - write-pivnet-metadata should at least expect the resource to be named wit -stack0
    
    [#159529464]

--- tasks/write-buildpack-pivnet-metadata/run.rb
+++ tasks/write-buildpack-pivnet-metadata/run.rb
@@ -12,7 +12,7 @@ recent_changes_filename = File.join(root_dir, 'buildpack-artifacts', 'RECENT_CHA
 
 buildpack_files = ''
 
-Dir.chdir('pivotal-buildpacks-cached') do
+Dir.chdir('pivotal-buildpacks-stack0') do
   buildpack_files = Dir["#{buildpack}_buildpack-cached*-v*.zip"]
 end
 
