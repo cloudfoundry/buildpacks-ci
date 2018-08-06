aa28e28cbb7264a13572037f5dfa7283db5c36b6
Andrew Meyer <ameyer@pivotal.io>
2018-08-06 17:31:54 -0400

    Use private bosh releases in cflinuxfs3 pipeline
    - Online releases are not available
    
    [#145881065]
    
    Signed-off-by: Tyler Phelan <tphelan@pivotal.io>

--- tasks/use-new-buildpack-bosh-releases/run.sh
+++ tasks/use-new-buildpack-bosh-releases/run.sh
@@ -8,6 +8,17 @@ version=$(date +%s)
 
 for language in binary dotnet-core go java nodejs php python ruby staticfile; do
   pushd "$language-buildpack-release"
+    #TODO: We do not need to do this when we are using online releases
+    cat > config/private.yml <<EOF
+---
+blobstore:
+  provider: s3
+  options:
+    access_key_id: $ACCESS_KEY_ID
+    bucket_name: pivotal-offline-buildpacks
+    secret_access_key: $SECRET_ACCESS_KEY
+    credentials_source: static
+EOF
     bosh2 create-release --tarball "../built-buildpacks-artifacts/$language-$version.tgz" --name "$language" --version "$version"
   popd
 
@@ -18,5 +29,4 @@ for language in binary dotnet-core go java nodejs php python ruby staticfile; do
 
 EOF
 done
-
 
