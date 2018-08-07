2a10abbe97845379b35d05702ec08ad3f71886fb
Jackson Feeny <jacksonfeeny@gmail.com>
2018-08-07 16:01:14 -0400

    Set bosh env manually instead of using shared-functions
    - We can set this back when we update to newer
    cf-deployment-concourse-tasks
    
    [#159240864]
    
    Signed-off-by: Tyler Phelan <tphelan@pivotal.io>

--- tasks/turn-on-bosh-dns/run.sh
+++ tasks/turn-on-bosh-dns/run.sh
@@ -2,9 +2,12 @@
 set -eux
 set -o pipefail
 
-source cf-deployment-concourse-tasks/shared-functions
-
-setup_bosh_env_vars
 trap "pkill -f ssh" EXIT
+
+set +x
+pushd "bbl-state/${BBL_STATE_DIR}"
+  eval "$(bbl print-env)"
+popd
+set -x
 
 bosh -n update-runtime-config bosh-deployment/runtime-configs/dns.yml --name dns
