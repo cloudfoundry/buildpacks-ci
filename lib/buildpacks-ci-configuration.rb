0248ffdf5f94f63d8f3b992d6d22e2ab00ca1ed1
Ben Fickes <bfickes@pivotal.io>
2018-08-14 10:47:08 -0400

    Create bosh releases for each buildpack that target cflinuxfs2 exclusivley
    [#159578149]
    
    Signed-off-by: Danny Joyce <djoyce@pivotal.io>

--- lib/buildpacks-ci-configuration.rb
+++ lib/buildpacks-ci-configuration.rb
@@ -15,6 +15,10 @@ class BuildpacksCIConfiguration
     ENV.fetch('LPASS_BOSH_RELEASE_PRIVATE_KEYS_FILE', 'Shared-Buildpacks/buildpack-bosh-release-repos-private-keys.yml')
   end
 
+  def bosh_release_private_keys_filename_lts
+    ENV.fetch('LPASS_BOSH_RELEASE_PRIVATE_KEYS_FILE_LTS', 'Shared-Buildpacks/buildpack-bosh-release-repos-private-keys-lts.yml')
+  end
+
   def git_repos_private_keys_filename
     ENV.fetch('LPASS_GIT_REPOS_PRIVATE_KEYS_FILE', 'Shared-Buildpacks/git-repos-private-keys.yml')
   end
