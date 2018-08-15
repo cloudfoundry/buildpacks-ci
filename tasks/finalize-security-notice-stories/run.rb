c17ce063ab902128e0451ba23813f4556c805add
Tyler Phelan <tphelan@pivotal.io>
2018-08-15 14:38:33 -0400

    Update finalize security stories for new story format
    - Security triage changed the format of the rootfs CVE stories
    
    [#145881065]

--- tasks/finalize-security-notice-stories/run.rb
+++ tasks/finalize-security-notice-stories/run.rb
@@ -8,10 +8,8 @@ new_stack_version = File.read('version/number').strip.gsub(/-rc.*/, '')
 token = ENV.fetch('TRACKER_API_TOKEN')
 project_id = ENV.fetch('TRACKER_PROJECT_ID')
 requester_id = ENV.fetch('TRACKER_REQUESTER_ID')
-davos_token = ENV.fetch('DAVOS_TOKEN')
 stack = ENV.fetch('STACK')
 
 tracker_client = TrackerClient.new(token, project_id, requester_id.to_i)
-davos_client = DavosClient.new(davos_token)
 
-FinalizeSecurityNoticeStories.new(tracker_client, new_stack_version, davos_client, stack).run
+FinalizeSecurityNoticeStories.new(tracker_client, new_stack_version, stack).run
