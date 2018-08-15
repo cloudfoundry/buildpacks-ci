fd24cb834eb1d51845546b75f4bd1cb97b8e7494
Tyler Phelan <tphelan@pivotal.io>
2018-08-15 14:38:33 -0400

    Update finalize security stories for new story format
    - Security triage changed the format of the rootfs CVE stories
    
    [#145881065]

--- spec/tasks/finalize-security-notices/finalize-security-notices_spec.rb
+++ spec/tasks/finalize-security-notices/finalize-security-notices_spec.rb
@@ -4,14 +4,13 @@ require_relative '../../../tasks/finalize-security-notice-stories/finalize-secur
 
 describe FinalizeSecurityNoticeStories do
   let(:tracker_client) {double "tracker_client"}
-  let(:davos_client) {double "davos"}
   let(:new_stack_version) { 999.999 }
   let(:affected_stories) { [
-    { "id"=>"487", "kind"=>"story", "label"=>"affected", "current_state"=>"started", "description"=>"(https://davos.cfapps.io/product_stories/4876)" },
-    { "id"=>"987", "kind"=>"story", "label"=>"affected", "current_state"=>"started", "description"=>"(https://davos.cfapps.io/product_stories/9876)" },
+    { "id"=>"487", "kind"=>"story", "label"=>"affected", "current_state"=>"started", "description"=>"packages" },
+    { "id"=>"987", "kind"=>"story", "label"=>"affected", "current_state"=>"started", "description"=>"packages" },
   ] }
 
-  subject { FinalizeSecurityNoticeStories.new(tracker_client, new_stack_version, davos_client, 'cflinuxfs2') }
+  subject { FinalizeSecurityNoticeStories.new(tracker_client, new_stack_version, 'some-stack') }
 
   before(:each) do
     allow(tracker_client).to receive(:search_with_filters).with(anything).and_return(affected_stories)
@@ -19,18 +18,14 @@ describe FinalizeSecurityNoticeStories do
     allow(tracker_client).to receive(:overwrite_label_on_story).with(affected_stories.last, 'fixed-999.999')
     allow(tracker_client).to receive(:change_story_state).with('487', 'finished')
     allow(tracker_client).to receive(:change_story_state).with('987', 'finished')
-    allow(davos_client).to receive(:change).with(anything, anything)
   end
 
-  it "finishes any stories tagged 'affected-<version of rootfs>' in the 'started' state and tags them with 'fixed-<version of latest rootfs>" do
-    expect(tracker_client).to receive(:search_with_filters).with(label: %w(affected cflinuxfs2), state: 'started')
+  it "delivers any stories tagged 'affected' and '<some-stack>' in the 'started' state and tags them with 'fixed-<version of latest rootfs>" do
+    expect(tracker_client).to receive(:search_with_filters).with(label: %w(affected some-stack), state: 'started')
     expect(tracker_client).to receive(:overwrite_label_on_story).with(story: affected_stories.first, existing_label_regex: /affected/, new_label: 'fixed-999.999')
     expect(tracker_client).to receive(:overwrite_label_on_story).with(story: affected_stories.last, existing_label_regex: /affected/, new_label: 'fixed-999.999')
     expect(tracker_client).to receive(:change_story_state).with(story_id: '487', current_state: 'delivered')
     expect(tracker_client).to receive(:change_story_state).with(story_id: '987', current_state: 'delivered')
-
-    expect(davos_client).to receive(:change).with('4876', status: 'fixed', affected_versions: '', fixed_versions: '999.999')
-    expect(davos_client).to receive(:change).with('9876', status: 'fixed', affected_versions: '', fixed_versions: '999.999')
 
     subject.run
   end
--- tasks/finalize-security-notice-stories/finalize-security-notice-stories.rb
+++ tasks/finalize-security-notice-stories/finalize-security-notice-stories.rb
@@ -5,10 +5,9 @@ require 'tracker_api'
 require 'yaml'
 
 class FinalizeSecurityNoticeStories
-  def initialize(tracker_client, new_stack_version, davos_client, stack)
+  def initialize(tracker_client, new_stack_version, stack)
     @tracker_client = tracker_client
     @new_stack_version = new_stack_version
-    @davos_client = davos_client
     @stack = stack
   end
 
@@ -18,12 +17,6 @@ class FinalizeSecurityNoticeStories
       puts "Changing label on #{story['id']} from affected to fixed-#{@new_stack_version}"
       @tracker_client.overwrite_label_on_story(story: story, existing_label_regex: /affected/, new_label: "fixed-#{@new_stack_version}")
       @tracker_client.change_story_state(story_id: story['id'], current_state: 'delivered')
-      @davos_client.change(davos_id(story), status: 'fixed', affected_versions: '', fixed_versions: @new_stack_version.to_s)
     end
-  end
-
-  def davos_id(story)
-    m = story['description'].match(%r{\(https://davos.cfapps.io/product_stories/(\d+)\)})
-    return m[1] if m&.length == 2
   end
 end
