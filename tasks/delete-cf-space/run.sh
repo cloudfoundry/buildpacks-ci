a471a6b507364c270cf67edd6367af0f1acf9cc5
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2018-08-20 14:05:56 -0400

    Add no-stack namespace to cf_space in delete-space task
    - The sample-apps pipeline should test with default stack so it
      makes sense to not have to namespace the task

--- tasks/delete-cf-space/run.sh
+++ tasks/delete-cf-space/run.sh
@@ -4,6 +4,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-"./cf-space-$CF_STACK/login"
-SPACE="$(cat cf-space-"$CF_STACK"/name)"
+cf_flag=""
+
+if [ -n "$CF_STACK" ]; then
+    cf_flag="-$CF_STACK"
+fi
+
+"./cf-space$cf_flag/login"
+
+set -x
+
+SPACE=$(cat cf-space"$cf_flag"/name)
 cf delete-space -f "$SPACE" || true
