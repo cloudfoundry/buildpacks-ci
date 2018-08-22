f1ec079b4af2568923eb866c4f2efed6c5713e2c
Danny Joyce <djoyce@pivotal.io>
2018-08-22 16:17:21 -0400

    Added test coverage for dependencies that use binary builder.
    [#158946396]
    
    Co-authored-by: Ben Fickes <bfickes@pivotal.io>

--- spec/tasks/build-binary-new/builder_spec.rb
+++ spec/tasks/build-binary-new/builder_spec.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 require 'tmpdir'
 require 'fileutils'
 require_relative '../../../tasks/build-binary-new/builder'
@@ -9,38 +7,70 @@ require_relative '../../../tasks/build-binary-new/build_output'
 require_relative '../../../tasks/build-binary-new/artifact_output'
 require_relative '../../../tasks/build-binary-new/binary_builder_wrapper'
 
+TableTestInput = Struct.new(:dep, :version) do
+end
+
+TableTestOutput = Struct.new(:old_file_path, :prefix, :extension) do
+end
+
 describe 'Builder' do
-  context 'when building python' do
-    subject { Builder.new }
+  subject { Builder.new }
+
+  let(:build_input) { double(BuildInput) }
+  let(:build_output) { double(BuildOutput) }
+  let(:artifact_output) { double(ArtifactOutput) }
+
+  context 'when using the old binary-builder' do
     let(:binary_builder) { double(BinaryBuilderWrapper) }
-    let(:source_input) { SourceInput.new('python', 'https://fake.com', '2.7.14', 'fake-md5', '') }
-    let(:build_input) { double(BuildInput) }
-    let(:build_output) { double(BuildOutput) }
-    let(:artifact_output) { double(ArtifactOutput) }
 
-    it 'returns metadata' do
-      allow(binary_builder).to receive(:base_dir)
-        .and_return '/fake-binary-builder'
-      expect(binary_builder).to receive(:build)
-        .with(source_input)
+    {
+      TableTestInput.new('bundler', '2.7.14') => TableTestOutput.new('/fake-binary-builder/bundler-2.7.14.tgz', 'bundler-2.7.14-cflinuxfs2', 'tgz'),
+      TableTestInput.new('python', '2.7.14')  => TableTestOutput.new('/fake-binary-builder/python-2.7.14-linux-x64.tgz', 'python-2.7.14-linux-x64-cflinuxfs2', 'tgz'),
+      TableTestInput.new('hwc', '2.7.14')     => TableTestOutput.new('/fake-binary-builder/hwc-2.7.14-windows-amd64.zip', 'hwc-2.7.14-windows-amd64', 'zip'),
+      TableTestInput.new('dep', '2.7.14')     => TableTestOutput.new('/fake-binary-builder/dep-v2.7.14-linux-x64.tgz', 'dep-v2.7.14-linux-x64-cflinuxfs2', 'tgz'),
+      TableTestInput.new('glide', '2.7.14')   => TableTestOutput.new('/fake-binary-builder/glide-v2.7.14-linux-x64.tgz', 'glide-v2.7.14-linux-x64-cflinuxfs2', 'tgz'),
+      TableTestInput.new('godep', '2.7.14')   => TableTestOutput.new('/fake-binary-builder/godep-v2.7.14-linux-x64.tgz', 'godep-v2.7.14-linux-x64-cflinuxfs2', 'tgz'),
+      TableTestInput.new('go', '2.7.14')      => TableTestOutput.new('/fake-binary-builder/go2.7.14.linux-amd64.tar.gz', 'go2.7.14.linux-amd64-cflinuxfs2', 'tar.gz'),
+      TableTestInput.new('node', '2.7.14')    => TableTestOutput.new('/fake-binary-builder/node-2.7.14-linux-x64.tgz', 'node-2.7.14-linux-x64-cflinuxfs2', 'tgz'),
+      TableTestInput.new('httpd', '2.7.14')   => TableTestOutput.new('/fake-binary-builder/httpd-2.7.14-linux-x64.tgz', 'httpd-2.7.14-linux-x64-cflinuxfs2', 'tgz'),
+      TableTestInput.new('ruby', '2.7.14')    => TableTestOutput.new('/fake-binary-builder/ruby-2.7.14-linux-x64.tgz', 'ruby-2.7.14-linux-x64-cflinuxfs2', 'tgz'),
+      TableTestInput.new('jruby', '9.2.0')    => TableTestOutput.new('/fake-binary-builder/jruby-9.2.0_ruby-2.5-linux-x64.tgz', 'jruby-9.2.0_ruby-2.5-linux-x64-cflinuxfs2', 'tgz'),
+      TableTestInput.new('php', '7.0.0')      => TableTestOutput.new('/fake-binary-builder/php7-7.0.0-linux-x64.tgz', 'php7-7.0.0-linux-x64-cflinuxfs2', 'tgz'),
+    }.each do |input, output|
+      describe "to build #{input.dep}" do
+        let(:source_input) { SourceInput.new(input.dep, 'https://fake.com', input.version, 'fake-md5', '') }
 
-      expect(build_input).to receive(:tracker_story_id).and_return 'fake-story-id'
-      expect(build_input).to receive(:copy_to_build_output)
+        before do
+          allow(binary_builder).to receive(:base_dir).and_return '/fake-binary-builder'
 
-      expect(build_output).to receive(:git_add_and_commit)
-        .with({
-          :tracker_story_id => 'fake-story-id',
-          :version          => '2.7.14',
-          :source           => { :url => 'https://fake.com', :md5 => 'fake-md5', :sha256 => '' },
-          :sha256           => 'fake-sha256',
-          :url              => 'fake-url'
-        })
+          if input.dep != 'php'
+            expect(binary_builder).to receive(:build).with source_input
+          else
+            expect(binary_builder).to receive(:build).with(source_input, anything)
+          end
 
-      expect(artifact_output).to receive(:move_dependency)
-        .with('python', '/fake-binary-builder/python-2.7.14-linux-x64.tgz', 'python-2.7.14-linux-x64-cflinuxfs2', 'tgz')
-        .and_return(sha256: 'fake-sha256', url: 'fake-url')
+          expect(build_input).to receive(:tracker_story_id).and_return 'fake-story-id'
+          expect(build_input).to receive(:copy_to_build_output)
 
-      subject.execute(binary_builder, 'cflinuxfs2', source_input, build_input, build_output, artifact_output)
+          expect(build_output).to receive(:git_add_and_commit)
+            .with(
+              tracker_story_id: 'fake-story-id',
+              version:          input.version,
+              source:           { url: 'https://fake.com', md5: 'fake-md5', sha256: '' },
+              sha256:           'fake-sha256',
+              url:              'fake-url'
+            )
+        end
+
+        it 'should build correctly' do
+          dep_name = input.dep == 'php' ? 'php7' : input.dep
+          expect(artifact_output).to receive(:move_dependency)
+            .with(dep_name, output.old_file_path, output.prefix, output.extension)
+            .and_return(sha256: 'fake-sha256', url: 'fake-url')
+
+          subject.execute(binary_builder, 'cflinuxfs2', source_input, build_input, build_output, artifact_output)
+        end
+      end
     end
   end
 end
