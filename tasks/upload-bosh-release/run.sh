bf667c6ba43a8711ee16bb16f0064a96e6b953f7
Andrew Meyer <ameyer@pivotal.io>
2018-08-23 12:21:01 -0400

    Adding more output to upload-bosh-release task.
    [#159909406]
    
    Signed-off-by: Leah Hanson <lhanson@pivotal.io>

--- tasks/upload-bosh-release/run.sh
+++ tasks/upload-bosh-release/run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -l
+echo "Uploading bosh release..."
 set -ex -o pipefail
 
-# Target the bosh director
+
+echo "Targeting bosh director..."
 pushd env-repo/$BOSH_ENV
   eval "$(bbl print-env)"
 popd
 
-# Upload any matching releases
+echo "Uploading any matching releases..."
 pushd bosh-release
   bosh upload-release $RELEASES_GLOB
 popd
+
+echo "All done."
