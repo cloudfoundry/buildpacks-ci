688a7ea630c159b8648c3691f18262937342dc84
Andrew Meyer <ameyer@pivotal.io>
2018-08-23 13:47:29 -0400

    Update bosh release upload script
    Make it more like other examples
    [#159909406]
    
    Signed-off-by: Leah Hanson <lhanson@pivotal.io>

--- tasks/upload-bosh-release/run.sh
+++ tasks/upload-bosh-release/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -l
-echo "Uploading bosh release..."
-set -ex -o pipefail
-
+set -xeuo pipefail
 
 echo "Targeting bosh director..."
 pushd "env-repo/$BOSH_ENV"
+  set +x
   eval "$(bbl print-env)"
+  set -x
+  trap "pkill -f ssh" EXIT
 popd
 
 echo "Uploading any matching releases..."
