d3750e2739a631ad66bc8b22ef5e979a65a98b3d
Tyler Phelan <tphelan@pivotal.io>
2018-08-29 12:07:22 -0400

    Remove unused brats task/specs
    Signed-off-by: An Yu <anyu@pivotal.io>

--- spec/tasks/run-brats/brats/cf_spec/integration/go_spec.rb
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-describe 'go_spec test', concourse_test: true, language: 'go' do
-  it 'setups the deployment environment' do
-    puts 'This spec has ran'
-    expect(1).to equal 1
-  end
-end
