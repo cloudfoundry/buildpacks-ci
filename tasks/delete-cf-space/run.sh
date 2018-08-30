dbfdae871e9052321c682af07cea8935f66ae10f
Ben Fickes <bfickes@pivotal.io>
2018-08-30 10:33:11 -0400

    Remove stack logic from delete-cf-space task
    - We use input-mapping now
    
    [#159909406]
    
    Co-authored-by: Tyler Phelan <tphelan@pivotal.io>

--- tasks/delete-cf-space/run.sh
+++ tasks/delete-cf-space/run.sh
@@ -4,15 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-cf_flag=""
-
-if [ -n "$CF_STACK" ]; then
-    cf_flag="-$CF_STACK"
-fi
-
-"./cf-space$cf_flag/login"
+"./cf-space/login"
 
 set -x
 
-SPACE=$(cat cf-space"$cf_flag"/name)
+SPACE=$(cat cf-space/name)
 cf delete-space -f "$SPACE" || true
