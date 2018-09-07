f1427e4f381304f570ba2d5f0bac04a760aac1a6
Tyler Phelan <tphelan@pivotal.io>
2018-09-07 17:51:28 -0400

    Fix cats tests
    - Use cf-deployment-concourse-tasks run-cats task
    
    Co-authored-by: An Yu <anyu@pivotal.io>

--- tasks/add-flake-attempts-to-cats/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o nounset
-set -o pipefail
-
-rsync -vaz cats-concourse-task-inp/ cats-concourse-task/
-
-sed -i s/keepGoing/-flakeAttempts=3/ cats-concourse-task/task
