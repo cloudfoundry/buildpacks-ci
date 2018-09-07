00d5e7053cef93c70d76c8cf7576298bd99a9386
Danny Joyce <djoyce@pivotal.io>
2018-09-07 14:05:38 -0400

    Update sample app smoke test to include a stack
    [#160301603]
    
    Co-authored-by: Daniel Thornton <dthornton@pivotal.io>

--- tasks/sample-app-smoke-test/run.rb
+++ tasks/sample-app-smoke-test/run.rb
@@ -7,23 +7,23 @@ require 'fileutils'
 require 'digest'
 
 def main
-  app_name = ENV.fetch('APPLICATION_NAME')
-  buildpack_url = ENV.fetch('BUILDPACK_URL')
-  request_path = ENV.fetch('REQUEST_PATH')
+  app_name         = ENV.fetch('APPLICATION_NAME')
+  buildpack_url    = ENV.fetch('BUILDPACK_URL')
+  request_path     = ENV.fetch('REQUEST_PATH')
   database_to_bind = ENV.fetch('DATABASE_TO_BIND')
-  request_type = ENV.fetch('REQUEST_TYPE')
+  request_type     = ENV.fetch('REQUEST_TYPE')
 
-  system "./cf-space/login"
+  system './cf-space/login'
 
   bind_database(database_to_bind)
 
   push_app(buildpack_url, app_name)
 
-  host = get_app_host(app_name, File.read("./cf-space/name").strip)
+  host = get_app_host(app_name, File.read('./cf-space/name').strip)
 
   response = get_app_response(host, request_path, request_type)
 
-  if response.code == "200"
+  if response.code == '200'
     puts 'Got HTTP response 200. App push successful'
   else
     puts "Got HTTP response #{response.code}. App push unsuccessful"
@@ -34,11 +34,11 @@ end
 def push_app(buildpack_url, app_name)
   Dir.chdir('sample-app') do
     if app_name == 'spring-music'
-      java_jdk_dir = '/opt/java'
-      java_jdk_tar_file = File.join(java_jdk_dir, 'openjdk-8-jdk.tar.gz')
-      java_jdk_bin_dir = File.join(java_jdk_dir, 'bin')
-      java_jdk_sha256 = '1315567082b55b3e1a62156d36c6f8adad152c32ab4a9eed7e72c1b24c381f9e'
-      java_buildpack_java_sdk = "https://java-buildpack.cloudfoundry.org/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_131.tar.gz"
+      java_jdk_dir            = '/opt/java'
+      java_jdk_tar_file       = File.join(java_jdk_dir, 'openjdk-8-jdk.tar.gz')
+      java_jdk_bin_dir        = File.join(java_jdk_dir, 'bin')
+      java_jdk_sha256         = '1315567082b55b3e1a62156d36c6f8adad152c32ab4a9eed7e72c1b24c381f9e'
+      java_buildpack_java_sdk = 'https://java-buildpack.cloudfoundry.org/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_131.tar.gz'
 
       FileUtils.mkdir_p(java_jdk_dir)
       raise "Downloading openjdk-8-jdk failed." unless system("wget #{java_buildpack_java_sdk} -O #{java_jdk_tar_file}")
@@ -52,29 +52,29 @@ def push_app(buildpack_url, app_name)
       raise "Untarring openjdk-8-jdk failed." unless system("tar xvf #{java_jdk_tar_file} -C #{java_jdk_dir}")
 
       ENV['JAVA_HOME'] = java_jdk_dir
-      ENV['PATH'] = "#{ENV['PATH']}:#{java_jdk_bin_dir}"
+      ENV['PATH']      = "#{ENV['PATH']}:#{java_jdk_bin_dir}"
 
       system "./gradlew assemble"
       FileUtils.mv("./build/libs/sample-app-1.0.jar", "./build/libs/spring-music-1.0.jar")
     end
 
-    system "cf push #{app_name} -b #{buildpack_url} -t 180 --random-route"
+    system "cf push #{app_name} -b #{buildpack_url} -t 180 --random-route -s #{ENV.fetch('STACK', 'cflinuxfs2')}"
   end
 end
 
 def get_app_host(app_name, space_name)
-  spaces = JSON.parse(`cf curl '/v2/spaces' -X GET -H 'Content-Type: application/x-www-form-urlencoded' -d 'q=name:#{space_name}'`)
+  spaces     = JSON.parse(`cf curl '/v2/spaces' -X GET -H 'Content-Type: application/x-www-form-urlencoded' -d 'q=name:#{space_name}'`)
   space_guid = spaces['resources'].first['metadata']['guid']
 
-  apps = JSON.parse(`cf curl '/v2/apps' -X GET -H 'Content-Type: application/x-www-form-urlencoded' -d 'q=name:#{app_name}&q=space_guid:#{space_guid}'`)
+  apps       = JSON.parse(`cf curl '/v2/apps' -X GET -H 'Content-Type: application/x-www-form-urlencoded' -d 'q=name:#{app_name}&q=space_guid:#{space_guid}'`)
   routes_url = apps['resources'].first['entity']['routes_url']
 
-  routes = JSON.parse(`cf curl #{routes_url}`)
-  name = routes['resources'].first['entity']['host']
+  routes     = JSON.parse(`cf curl #{routes_url}`)
+  name       = routes['resources'].first['entity']['host']
   domain_url = routes['resources'].first['entity']['domain_url']
 
   domains = JSON.parse(`cf curl #{domain_url}`)
-  domain = domains['entity']['name']
+  domain  = domains['entity']['name']
 
   "#{name}.#{domain}"
 end
@@ -84,20 +84,23 @@ def get_app_response(host, path, type)
 
   Net::HTTP.start(request_uri.host, request_uri.port, :use_ssl => true) do |http|
     req = case type
-            when 'DELETE' then Net::HTTP::Delete.new(request_uri)
-            when 'GET' then Net::HTTP::Get.new(request_uri)
-            else raise "Invalid request type #{type}"
-          end
+    when 'DELETE' then
+      Net::HTTP::Delete.new(request_uri)
+    when 'GET' then
+      Net::HTTP::Get.new(request_uri)
+    else
+      raise "Invalid request type #{type}"
+    end
     http.request(req)
   end
 end
 
 def bind_database(database)
   case database
-    when 'mysql'
-      system "cf create-service cleardb spark mysql"
-    when 'pgsql'
-      system "cf create-service elephantsql turtle pgsql"
+  when 'mysql'
+    system "cf create-service cleardb spark mysql"
+  when 'pgsql'
+    system "cf create-service elephantsql turtle pgsql"
   end
 end
 
