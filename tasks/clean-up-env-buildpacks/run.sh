7dd185bb922a9abae118e101f63cbea27bded9ba
Tyler Phelan <tphelan@pivotal.io>
2018-09-10 11:48:58 -0400

    handle when grep finds no matches [#160356865]
    Co-authored-by: Daniel Thornton <dthornton@pivotal.io>

--- tasks/clean-up-env-buildpacks/run.sh
+++ tasks/clean-up-env-buildpacks/run.sh
@@ -11,14 +11,17 @@ cf api "$target" --skip-ssl-validation || (sleep 4 && cf api "$target" --skip-ss
 cf auth admin "$cf_password" || (sleep 4 && cf auth admin "$cf_password")
 set -x
 
-cf buildpacks | tail -n +4 | cut -d ' ' -f1 | grep -v "hwc_buildpack\|apt_buildpack\|binary_buildpack\|credhub_buildpack\|dotnet_core_buildpack\|go_buildpack\|nginx_buildpack\|nodejs_buildpack\|php_buildpack\|python_buildpack\|r_buildpack\|ruby_buildpack\|staticfile_buildpack"
-cf buildpacks | tail -n +4 | grep -v "cflinuxfs2\|cflinuxfs3\|windows2012R2\|windows2016" | cut -d ' ' -f1
+custom_buildpacks=$(cf buildpacks | tail -n +4 | cut -d ' ' -f1 | grep -v "hwc_buildpack\|apt_buildpack\|binary_buildpack\|credhub_buildpack\|dotnet_core_buildpack\|go_buildpack\|nginx_buildpack\|nodejs_buildpack\|php_buildpack\|python_buildpack\|r_buildpack\|ruby_buildpack\|staticfile_buildpack\|java_buildpack" || true)
+null_buildpacks=$(cf buildpacks | tail -n +4 | grep -v "cflinuxfs2\|cflinuxfs3\|windows2012R2\|windows2016" | cut -d ' ' -f1 || true)
+
+for bp in $custom_buildpacks $null_buildpacks; do
+  cf delete-buildpack "$bp" -f
+done
 
 for windows_stack in windows2012R2 windows2016; do
-  cf buildpacks | tail -n +4 | grep "hwc_buildpack" | grep $windows_stack
+  hwc_buildpacks=$(cf buildpacks | tail -n +4 | grep "hwc_buildpack" | grep $windows_stack) || true
 
-  exit_status="$?"
-  if [[ "$exit_status" -eq "1" ]]; then
+  if [[ "$hwc_buildpacks" == "" ]]; then
     pushd hwc-buildpack
       export GOPATH="$PWD"
       export GOBIN=$PWD/.bin
@@ -28,6 +31,7 @@ for windows_stack in windows2012R2 windows2016; do
 
       buildpack-packager build -stack "$windows_stack"
       cf create-buildpack hwc_buildpack hwc_buildpack-*.zip 999
+      rm hwc_buildpack-*.zip
     popd
   fi
 done
