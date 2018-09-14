e4594b47318ea3b2d93cbe587e3f0124fa7213e8
Danny Joyce <djoyce@pivotal.io>
2018-09-14 13:45:22 -0400

    Run V3 specs on the develop branch with docker
    [#160290732]
    
    Co-authored-by: Ben Fickes <bfickes@pivotal.io>
    Co-authored-by: Andrew Meyer <ameyer@pivotal.io>

--- tasks/run-bp-v3-brats/run.sh
+++ tasks/run-bp-v3-brats/run.sh
@@ -8,4 +8,10 @@ export GOPATH=$PWD
 export GOBIN=$PWD/.bin
 export PATH=$GOBIN:$PATH
 
+echo "Start Docker"
+../buildpacks-ci/scripts/start-docker > /dev/null
+
+echo "Pulling build image"
+docker pull cfbuildpacks/bpv3:build
+
 ./scripts/v3-brats.sh
