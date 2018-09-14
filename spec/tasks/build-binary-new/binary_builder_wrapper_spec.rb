4a37df1838de1fddcbcf6bf8d68a8e9a947ef941
Daniel Thornton <dthornton@pivotal.io>
2018-09-14 15:39:55 -0400

    Ensure php is built with correct name
    - There was a bug where php was being built with a name different from the name expected when
      moving the php tarball.
    - Added test coverage for this
    
    [#160478492]
    
    Co-authored-by: An Yu <anyu@pivotal.io>

--- spec/tasks/build-binary-new/binary_builder_wrapper_spec.rb
+++ spec/tasks/build-binary-new/binary_builder_wrapper_spec.rb
@@ -3,12 +3,14 @@ require_relative '../../../tasks/build-binary-new/binary_builder_wrapper'
 require_relative '../../../tasks/build-binary-new/source_input'
 
 describe 'BinaryBuilderWrapper' do
-  let(:runner) { double('Runner') }
-  let(:source_input) { SourceInput.new('fake-name', 'fake-url', 'version', nil, '123456') }
   subject { BinaryBuilderWrapper.new(runner, Dir.mktmpdir) }
 
+  let(:runner) { double('Runner') }
+  let(:source_input) { SourceInput.new('fake-name', 'fake-url', 'version', nil, '123456') }
+
+
   context 'with an extension file' do
-    it 'should run the old binary builder' do
+    it 'runs the old binary builder' do
       expect(runner).to receive(:run)
         .with('./bin/binary-builder', '--name=fake-name', '--version=version', '--sha256=123456', 'fake-ext-file.yml')
       subject.build(source_input, 'fake-ext-file.yml')
@@ -16,9 +18,49 @@ describe 'BinaryBuilderWrapper' do
   end
 
   context 'without an extension file' do
-    it 'should run the old binary builder' do
+    it 'runs the old binary builder' do
       expect(runner).to receive(:run)
         .with('./bin/binary-builder', '--name=fake-name', '--version=version', '--sha256=123456')
+      subject.build(source_input)
+    end
+  end
+
+  context 'with node' do
+    let(:source_input) { SourceInput.new('node', 'fake-url', 'version', nil, '123456') }
+
+    it 'uses node as the name it runs with' do
+      expect(runner).to receive(:run)
+        .with('./bin/binary-builder', '--name=node', '--version=version', '--sha256=123456')
+      subject.build(source_input)
+    end
+  end
+
+  context 'with php5' do
+    let(:source_input) { SourceInput.new('php', 'fake-url', '5.3.15', nil, '123456') }
+
+    it 'uses php as the name it runs with' do
+      expect(runner).to receive(:run)
+        .with('./bin/binary-builder', '--name=php', '--version=5.3.15', '--sha256=123456')
+      subject.build(source_input)
+    end
+  end
+
+  context 'with php7.1.X' do
+    let(:source_input) { SourceInput.new('php', 'fake-url', '7.1.21', nil, '123456') }
+
+    it 'uses php7 as the name it runs with' do
+      expect(runner).to receive(:run)
+        .with('./bin/binary-builder', '--name=php7', '--version=7.1.21', '--sha256=123456')
+      subject.build(source_input)
+    end
+  end
+
+  context 'with php7.2.X' do
+    let(:source_input) { SourceInput.new('php', 'fake-url', '7.2.10', nil, '123456') }
+
+    it 'uses php7 as the name it runs with' do
+      expect(runner).to receive(:run)
+        .with('./bin/binary-builder', '--name=php7', '--version=7.2.10', '--sha256=123456')
       subject.build(source_input)
     end
   end
