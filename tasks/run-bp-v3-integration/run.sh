131eb030c63e32452e2aa06a9eb19697a6dc2f16
Danny Joyce <djoyce@pivotal.io>
2018-09-24 12:31:09 -0400

    Add v3 integration job
    - Rename v3 brats to v3 acceptance
    
    [#160290872]
    
    Co-authored-by: An Yu <anyu@pivotal.io>

---
+++ tasks/run-bp-v3-integration/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+set -o errexit
+set -o pipefail
+
+cd buildpack
+
+export GOPATH=$PWD
+export GOBIN=$PWD/.bin
+export PATH=$GOBIN:$PATH
+
+echo "Start Docker"
+../buildpacks-ci/scripts/start-docker > /dev/null
+
+./scripts/v3-integration.sh
