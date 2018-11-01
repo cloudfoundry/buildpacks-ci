c66b4e0735da769180ddad2c9c64d60eedbf79d4
Tyler Phelan <tphelan@pivotal.io>
2018-11-01 15:25:44 -0400

    Start docker for php unit tests
    - Needed to run compile script
    
    Co-authored-by: An Yu <anyu@pivotal.io>

--- tasks/run-buildpack-unit-specs/run.sh
+++ tasks/run-buildpack-unit-specs/run.sh
@@ -5,6 +5,11 @@ set -o pipefail
 
 cd buildpack
 
+if [[ ${DOCKER_START} == "true" ]]; then
+  echo "Start Docker"
+  ../buildpacks-ci/scripts/start-docker >/dev/null
+fi
+
 # for the PHP buildpack
 if [ -e run_tests.sh ]; then
   TMPDIR="$(mktemp -d)"
