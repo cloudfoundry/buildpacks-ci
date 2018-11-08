88d4ff6078be4bc2819f21bb3d65dbe46cac0b57
Daniel Thornton <dthornton@pivotal.io>
2018-11-08 17:02:15 -0500

    update php unit tests to run on cflinux docker container
    - this should prevent linking errors due to missing .so libraries between php and underlying cfbuildpacks/ci docker image
    [#161706086]
    
    Co-authored-by: Guillermo Kardolus <gkardolus@pivotal.io>

--- tasks/run-buildpack-unit-specs/run.sh
+++ tasks/run-buildpack-unit-specs/run.sh
@@ -10,11 +10,4 @@ if [[ ${DOCKER_START} == "true" ]]; then
   ../buildpacks-ci/scripts/start-docker >/dev/null
 fi
 
-# for the PHP buildpack
-if [ -e run_tests.sh ]; then
-  TMPDIR="$(mktemp -d)"
-  export TMPDIR
-  pip install -r requirements.txt
-fi
-
 ./scripts/unit.sh
