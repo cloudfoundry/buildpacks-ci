3b59f88693fe58c1de9f081e6737d87492ebdfe8
Danny Joyce <djoyce@pivotal.io>
2018-11-14 12:16:48 -0500

    Add task to create lock metadata for creating environment locks
    [#161755427]
    
    Co-authored-by: Tyler Phelan <tphelan@pivotal.io>

---
+++ tasks/write-env-locks/run.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+env = Env.fetch('ENVIRONMENT')
+locks = Env.fetch('NUMBER_OF_LOCKS').to_i - 1
+output_dir = 'environment-locks'
+
+for i in 0..locks do
+  Dir.mkdir(File.join(output_dir, env + i.to_s))
+  File.write(File.join(output_dir, env + i.to_s, 'name'), env + i.to_s)
+  File.write(File.join(output_dir, env + i.to_s, 'metadata'), '')
+end
