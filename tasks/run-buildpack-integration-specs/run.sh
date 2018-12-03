30b8c24a64e11c6ea3bd6cd9d3b02f6e4b643271
Tyler Phelan <tphelan@pivotal.io>
2018-12-03 17:00:38 -0500

    Pass buildpack zips into master specs
    - Now we are testing the actual files we push to Pivnet and PWS
    
    [#162382903]
    
    Co-authored-by: Zander Mackie <amackie@pivotal.io>

--- tasks/run-buildpack-integration-specs/run.sh
+++ tasks/run-buildpack-integration-specs/run.sh
@@ -5,6 +5,15 @@ set -o pipefail
 
 "./cf-space/login"
 
+ if [[ -d "uncached-buildpack" ]]; then
+   export UNCACHED_BUILDPACK_FILE=$(realpath $(find ./uncached-buildpack -name "*.zip"))
+ fi
+
+if [[ -d "cached-buildpack" ]]; then
+  export CACHED_BUILDPACK_FILE=$(realpath $(find ./cached-buildpack -name "*.zip"))
+  echo "BUILDPACK FILE!!!!!!!!!: $CACHED_BUILDPACK_FILE"
+fi
+
 cd buildpack
 
 if [[ -z ${SKIP_DOCKER_START:-} ]]; then
