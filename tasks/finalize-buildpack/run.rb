42434e5d83595dfc63c2eb61a4f30107a81ac0f6
An Yu <anyu@pivotal.io>
2018-12-10 16:58:15 -0500

    Update to be compatible with go modules
    Co-authored-by: Tyler Phelan <tphelan@pivotal.io>

--- tasks/finalize-buildpack/run.rb
+++ tasks/finalize-buildpack/run.rb
@@ -8,8 +8,7 @@ version = File.read('buildpack/VERSION').strip
 buildpack_repo_dir = 'buildpack'
 uncached_buildpack_dirs = Dir.glob('pivotal-buildpacks*')
 
-ENV['GOPATH']=File.expand_path(buildpack_repo_dir)
-ENV['GOBIN']="#{ENV['GOPATH']}/.bin"
+ENV['GOBIN']="#{File.expand_path(buildpack_repo_dir)}/.bin"
 ENV['PATH']="#{ENV['GOBIN']}:#{ENV['PATH']}"
 
 BuildpackFinalizer.new(artifact_dir, version, buildpack_repo_dir, uncached_buildpack_dirs).run
