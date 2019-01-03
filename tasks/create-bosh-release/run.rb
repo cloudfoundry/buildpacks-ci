9ba81bb142cb40045077e57126b45a0ab9cfbba0
Tyler Phelan <tphelan@pivotal.io>
2019-01-03 17:42:31 -0500

    Fail create-bosh-release job if bosh release tag already exists

--- tasks/create-bosh-release/run.rb
+++ tasks/create-bosh-release/run.rb
@@ -18,8 +18,15 @@ secret_access_key = ENV.fetch('SECRET_ACCESS_KEY', false)
 language = ENV.fetch('LANGUAGE')
 release_name = ENV.fetch('RELEASE_NAME')
 
-
 Dir.chdir(ENV.fetch('RELEASE_DIR')) do
+  release_tags = `git tag`.split("\n")
+
+  if release_tags.include?(version)
+    puts "BOSH release version #{version} already exists"
+    puts "exiting"
+    exit 1
+  end
+
   updater = BuildpackBOSHReleaseUpdater.new(
     version,
     access_key_id,
