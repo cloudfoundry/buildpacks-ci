4b7c15a2970dc8d048cb14ed43783af33f75a47a
Zander Mackie <amackie@pivotal.io>
2019-02-08 15:23:04 -0500

    Fix shellcheck and yml
    [#163793805]
    
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

--- tasks/renew-lts-env/run.sh
+++ tasks/renew-lts-env/run.sh
@@ -5,4 +5,4 @@ set -o nounset
 set -o pipefail
 set -x
 
-curl -v https://environments.toolsmiths.cf-app.com/gcp_engineering_environments/${ENV_ID}/renew
+curl -v "https://environments.toolsmiths.cf-app.com/gcp_engineering_environments/${ENV_ID}/renew"
