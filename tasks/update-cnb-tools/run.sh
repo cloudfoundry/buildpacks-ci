ea39fd3b06c800f7ba5fe84758a18cd357131084
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2019-02-19 17:51:35 -0500

    create update cnb tools task

---
+++ tasks/update-cnb-tools/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+set -x
+
+pushd buildpack
+  go get github.com/cloudfoundry/cnb-tools
+  go mod vendor
+  ./scripts/unit.sh
+  git add .
+
+  set +e
+    git diff --cached --exit-code
+    no_changes=$?
+  set -e
+
+  if [ $no_changes -ne 0 ]
+  then
+    git commit -m "Update CNB tools"
+  else
+    echo "CNB tools are up to date"
+  fi
+popd
+
+rsync -a buildpack/ buildpack-artifacts
