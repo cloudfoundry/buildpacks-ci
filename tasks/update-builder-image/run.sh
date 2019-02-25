628b11ddf727eeb1d3d22e5303efe36d95bd3f9b
Daniel Thornton <dthornton@pivotal.io>
2019-02-25 11:27:36 -0500

    Quote buildpack urls. User version file for pack create-builder
    Co-authored-by: Zander Mackie <amackie@pivotal.io>

--- tasks/update-builder-image/run.sh
+++ tasks/update-builder-image/run.sh
@@ -30,9 +30,9 @@ pushd yarn-cnb
     YARN=$(./scripts/package.sh | grep -e "/tmp/yarn-cnb_*" | awk -F' ' '{print $4}')
 popd
 
-erb packaged_nodejs=$NODE packaged_npm=$NPM packaged_yarn=$YARN ./buildpacks-ci/tasks/update-builder-image/builder-template.toml.erb > final.toml
+erb packaged_nodejs="$NODE" packaged_npm="$NPM" packaged_yarn="$YARN" ./buildpacks-ci/tasks/update-builder-image/builder-template.toml.erb > final.toml
 
-builder_ver="0.0.0"
+builder_ver=cflinuxfs3-v"$(cat ./version/cflinuxfs3)"
 
 ./pack add-stack org.cloudfoundry.stacks.cflinuxfs3 -b cfbuildpacks/cflinuxfs3-cnb-experimental:build -r cfbuildpacks/cflinuxfs3-cnb-experimental:run
 ./buildpacks-ci/scripts/start-docker >/dev/null
