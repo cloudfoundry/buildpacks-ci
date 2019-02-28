68105edc49fb88b79c3d197ef2c900d0854ee72e
Zander Mackie <amackie@pivotal.io>
2019-02-28 12:12:40 -0500

    Clean up update builder script
    [#163709831]
    
    Co-authored-by: Guillermo Kardolus <gkardolus@pivotal.io>

--- tasks/update-builder-image/run.sh
+++ tasks/update-builder-image/run.sh
@@ -4,15 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-
-# build the pack binary from the repo.
-# should our builder use urls? or do we add in the buildpack files here?
-# save builder-toml file we use to ci-robots
-# build a builder image
-# get old builder version from dockerhub
-# curl 'https://registry.hub.docker.com/v2/repositories/cfbuildpacks/ci/tags/'|jq '."results"[]["name"]'
-# push it to dockerhub
-
 pushd nodejs-cnb
     ./scripts/install_tools.sh
     mv .bin/pack ./..
@@ -36,9 +27,7 @@ erb packaged_nodejs="$NODE" packaged_npm="$NPM" packaged_yarn="$YARN" ./buildpac
 ./buildpacks-ci/scripts/start-docker >/dev/null
 
 IMG_NAME="cloudfoundry/cnb"
-./pack create-builder "$IMG_NAME" --builder-config ./final.toml --stack org.cloudfoundry.stacks.cflinuxfs3 #--publish
+./pack create-builder "$IMG_NAME" --builder-config ./final.toml --stack org.cloudfoundry.stacks.cflinuxfs3
 
 docker save "$IMG_NAME" -o ./docker-artifacts/builder.tgz
-#get docker file we want to push up to
-#move output of line 39 to line 40
 
