69a579b5cb3d4c5819f7806362311e978bf200a9
Guillermo Kardolus <gkardolus@pivotal.io>
2019-03-05 17:54:07 -0500

    Package the lifecycle methods
    - Append shasum to the name
    - Upload files to S3 bucket
    
    [#163987847]
    
    Co-authored-by: Chhavi Kankaria <ckankaria@pivotal.io>

--- tasks/update-v3-buildpack-manifest/run.sh
+++ tasks/update-v3-buildpack-manifest/run.sh
@@ -3,9 +3,18 @@
 mkdir -p binaries
 tar -xzf lifecycle-tar/lifecycle-binaries-latest.tgz --directory binaries
 
+bins=(analyzer builder detector exporter launcher)
+
 pushd binaries
-  shasum -a 256 analyzer builder detector exporter launcher > shasum.txt
+  touch shasum.txt
+  for i in ${bins[@]}; do
+      shasum -a 256 ${i} >> shasum.txt
+      tar czf v3-${i} ${i}
+      tarsha=$(shasum -a 256 v3-${i} | cut -c1-8)
+      filename=v3-${i}-$tarsha.tgz
+      mv v3-${i} $filename
+      aws s3 cp $filename s3://lifecycle-binaries
+  done
 popd
 
-version=$(cat binaries/VERSION)
-tar cvf binaries-$version.tgz binaries
+aws s3 cp binaries/shasum.txt s3://lifecycle-binaries
