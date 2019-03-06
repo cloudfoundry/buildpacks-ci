0cd6d7826a2c6a230193ea5b8b54bf535bdb77b4
Guillermo Kardolus <gkardolus@pivotal.io>
2019-03-06 10:00:42 -0500

    Double quote variables to prevent globbing [#163987847]

--- tasks/update-v3-buildpack-manifest/run.sh
+++ tasks/update-v3-buildpack-manifest/run.sh
@@ -7,13 +7,13 @@ bins=(analyzer builder detector exporter launcher)
 
 pushd binaries
   touch shasum.txt
-  for i in ${bins[@]}; do
-      shasum -a 256 ${i} >> shasum.txt
-      tar czf v3-${i} ${i}
-      tarsha=$(shasum -a 256 v3-${i} | cut -c1-8)
-      filename=v3-${i}-$tarsha.tgz
-      mv v3-${i} $filename
-      aws s3 cp $filename s3://lifecycle-binaries
+  for i in "${bins[@]}"; do
+      shasum -a 256 "${i}" >> shasum.txt
+      tar czf v3-"${i}" "${i}"
+      tarsha=$(shasum -a 256 v3-"${i}" | cut -c1-8)
+      filename=v3-"${i}"-"$tarsha".tgz
+      mv v3-"${i}" "$filename"
+      aws s3 cp "$filename" s3://lifecycle-binaries
   done
 popd
 
