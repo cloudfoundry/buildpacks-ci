27698d54e66f7ab524389f5221e03d8077785ef5
Don Nguyen <dnguyen@pivotal.io>
2019-03-19 17:54:24 -0400

    Create write-stack-auditor-pivnet-metadata task [#164697842]
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

---
+++ tasks/write-stack-auditor-pivnet-metadata/run.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+
+# class BuildpackPivnetMetadataWriter
+#
+#   attr_reader :buildpack, :output_dir, :buildpack_dir, :cached_buildpack_filenames, :recent_changes_filename, :lts_product
+#
+#   def initialize(buildpack, output_dir, buildpack_dir, cached_buildpack_filenames, recent_changes_filename, lts_product)
+#     @buildpack = buildpack
+#     @output_dir = output_dir
+#     @buildpack_dir = buildpack_dir
+#     @cached_buildpack_filenames = cached_buildpack_filenames
+#     @recent_changes_filename = recent_changes_filename
+#     @lts_product = lts_product
+#   end
+
+# def get_version
+root_dir = Dir.pwd
+$product_dir = File.join(root_dir, "plugin-release")
+metadata_dir = root_dir
+
+def get_version
+  Dir.chdir($product_dir) do
+    File.read('version').strip
+  end
+end
+
+def display_name(os)
+  "Stack Auditor #{os}"
+end
+
+def description
+  File.read(File.join($product_dir, "body"))
+end
+
+metadata_file_name = "stack-auditor.yml"
+metadata_yml = File.join(Dir.pwd, metadata_file_name)
+version = get_version
+
+metadata = {}
+metadata['release'] = {
+    'version' => version,
+    'release_type' => "Minor Release",
+    'eula_slug' => "pivotal_software_eula",
+    'release_notes_url' => "https://github.com/cloudfoundry/stack-auditor/releases/tag/v#{version}",
+}
+# metadata['release']['eccn'] = eccn
+# metadata['release']['license_exception'] = license_exception
+
+artifacts = [ "darwin", "linux", "windows" ]
+metadata['product_files'] = []
+artifacts.each do |os|
+  extension = os == "windows" ? "zip" : "tgz"
+  filename = "#{$product_dir}/stack-auditor-#{version}-#{os}.#{extension}"
+  metadata['product_files'].push({
+                                     'file' => filename,
+                                     'upload_as' => display_name(os),
+                                     'description' => description
+                                 })
+end
+
+puts "Writing metadata to #{metadata_yml}"
+puts metadata.to_yaml
+puts "\n\n"
+
+File.write(metadata_yml, metadata.to_yaml)
+
+
