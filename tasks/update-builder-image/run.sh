bae7073fd22afdc82905bbfe13f6d210cc9c6ca7
Zander Mackie <amackie@pivotal.io>
2019-03-25 14:49:31 -0400

    Support new pack stack api
    [#164646384]

--- tasks/update-builder-image/run.sh
+++ tasks/update-builder-image/run.sh
@@ -10,7 +10,7 @@ cnbs=(nodejs npm yarn) # Do not hardcode
 erbCmd="erb "
 
 for i in "${cnbs[@]}"; do
-  buildpack="${i}-cnb"
+  buildpack="${i}-cnb-release "
   pushd "$buildpack"
     path=$(./scripts/package.sh | grep -e "packaged into" | awk -F' ' '{print $4}')
   popd
@@ -20,9 +20,29 @@ done
 erbCmd+="./buildpacks-ci/tasks/update-builder-image/builder-template.toml.erb > final.toml"
 eval "$erbCmd"
 
-./pack add-stack org.cloudfoundry.stacks.cflinuxfs3 -b cfbuildpacks/cflinuxfs3-cnb-experimental:build -r cfbuildpacks/cflinuxfs3-cnb-experimental:run
+if [ "${MASTER}" = "true" ] # Support master release of pack
+then
+
+stackConfig=$(cat <<-END
+[stack]
+id = "org.cloudfoundry.stacks.cflinuxfs3"
+build-image = "cfbuildpacks/cflinuxfs3-cnb-experimental:build"
+run-image = "cfbuildpacks/cflinuxfs3-cnb-experimental:run"
+END
+)
+  erbCmd+="stack_config=$stackConfig"
+  STACK_COMMAND=""
+
+else
+
+  erbCmd+="stack_config='"
+  STACK_COMMAND=" --stack org.cloudfoundry.stacks.cflinuxfs3"
+  ./pack add-stack org.cloudfoundry.stacks.cflinuxfs3 -b cfbuildpacks/cflinuxfs3-cnb-experimental:build -r cfbuildpacks/cflinuxfs3-cnb-experimental:run
+
+fi
+
 ./buildpacks-ci/scripts/start-docker >/dev/null
 
 img_name="cloudfoundry/cnb"
-./pack create-builder "$img_name" --builder-config ./final.toml --stack org.cloudfoundry.stacks.cflinuxfs3
+./pack create-builder "$img_name" --builder-config ./final.toml --stack org.cloudfoundry.stacks.cflinuxfs3 "${STACK_COMMAND}"
 docker save "$img_name" -o ./docker-artifacts/builder.tgz
