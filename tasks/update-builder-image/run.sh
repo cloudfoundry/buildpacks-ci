18efd938b2d7f8bdf8b5c642c4fdb461520bf6fb
Zander Mackie <amackie@pivotal.io>
2019-03-25 15:38:39 -0400

    Use release paths for builder.toml
    [#164646384]

--- tasks/update-builder-image/run.sh
+++ tasks/update-builder-image/run.sh
@@ -11,14 +11,11 @@ erbCmd="erb "
 
 for i in "${cnbs[@]}"; do
   buildpack="${i}-cnb-release"
-  pushd "$buildpack"
-    path=$(./scripts/package.sh | grep -e "packaged into" | awk -F' ' '{print $4}')
-  popd
+  path=$(find "$buildpack" -name "*.tgz")
   erbCmd+="packaged_${i}=$path "
 done
 
 erbCmd+="./buildpacks-ci/tasks/update-builder-image/builder-template.toml.erb > final.toml"
-eval "$erbCmd"
 
 if [ "${MASTER}" = "true" ] # Support master release of pack
 then
@@ -36,13 +33,15 @@ END
 else
 
   erbCmd+="stack_config='"
-  STACK_COMMAND=" --stack org.cloudfoundry.stacks.cflinuxfs3"
+  STACK_COMMAND="--stack org.cloudfoundry.stacks.cflinuxfs3"
   ./pack add-stack org.cloudfoundry.stacks.cflinuxfs3 -b cfbuildpacks/cflinuxfs3-cnb-experimental:build -r cfbuildpacks/cflinuxfs3-cnb-experimental:run
 
 fi
 
+eval "$erbCmd"
+
 ./buildpacks-ci/scripts/start-docker >/dev/null
 
 img_name="cloudfoundry/cnb"
-./pack create-builder "$img_name" --builder-config ./final.toml --stack org.cloudfoundry.stacks.cflinuxfs3 "${STACK_COMMAND}"
+./pack create-builder "$img_name" --builder-config ./final.toml "${STACK_COMMAND}"
 docker save "$img_name" -o ./docker-artifacts/builder.tgz
