26b0c6a5dbfa62f185fafa5363a6f6c6912cdb01
Tyler Phelan <tphelan@pivotal.io>
2019-03-27 17:19:44 -0400

    Remove dependency-cache from java bps
    - Tag as cloudfoundry/cnb:<stack>-<version>
    
    [#164646384]
    
    Co-authored-by: Don Nguyen <dnguyen@pivotal.io>

--- tasks/create-builder/run.rb
+++ tasks/create-builder/run.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
 
 require 'tomlrb' # One gem to read (supports v0.4.0)
-require 'toml' #One to write
+require 'toml'   # One to write
 
 version = File.read(File.join("version", "version")).strip()
-builder_image = ENV.fetch("BUILDER_IMAGE")
+builder_repo = ENV.fetch("BUILDER_REPO")
 build_image = ENV.fetch("BUILD_IMAGE")
 run_image = ENV.fetch("RUN_IMAGE")
-stack = ENV.fetch("STACK")
+cnb_stack = ENV.fetch("STACK")
+stack = cnb_stack.split('.').last
 builder_config_file = "builder.toml"
 
 Dir.chdir "pack" do
@@ -16,19 +17,26 @@ end
 
 buildpacks = Dir.glob("sources/*/").map do |dir|
   id = Tomlrb.load_file(File.join(dir, "buildpack.toml"))['buildpack']['id']
-  bp_file = ""
+  bp_location = ""
   Dir.chdir dir do
-    if Dir.exist?("ci") # TODO: This exists because we package Buildpacks differently
+    if Dir.exist?("ci") # This exists because we package Buildpacks differently
       system File.join("ci", "package.sh") or exit 1
-      bp_file = File.join(dir, Dir.glob(File.join("artifactory", "*", "*", "*", "*", "*", "*.tgz")).first)
+      bp_tar = Dir.glob(File.join("artifactory", "*", "*", "*", "*", "*", "*.tgz")).first
+      # Remove dependency-cache because we can only package as a cached buildpack for now
+      output = 'built-buildpack'
+      Dir.mkdir(output)
+      system "tar", "xvf", bp_tar, "-C", output or exit 1
+      system "rm", "-rf", File.join(output, "dependency-cache")
+      bp_location = File.join(dir, output)
     else
       system File.join("scripts", "package.sh") or exit 1
-      bp_file = File.join(dir, Dir.glob("*.tgz").first)
+      bp_location = File.join(dir, Dir.glob("*.tgz").first)
     end
   end
   {
       "id": id,
-      "uri": bp_file
+      "uri": bp_location,
+      "latest": true,
   }
 end
 
@@ -38,7 +46,7 @@ config_hash = {
     "buildpacks": buildpacks,
     "groups": groups,
     "stack": {
-        "id": stack,
+        "id": cnb_stack,
         "build-image": build_image,
         "run-image": run_image
     }
@@ -48,5 +56,5 @@ builder_config = TOML::Generator.new(config_hash).body
 File.write(builder_config_file, builder_config)
 
 system "buildpacks-ci/scripts/start-docker" or exit 1
-system "./pack/pack", "create-builder", "#{builder_image}:#{version}", "--builder-config", "#{builder_config_file}" or exit 1
-system "docker", "save", "#{builder_image}", "-o", "builder-image/builder.tgz" or exit 1
+system "./pack/pack", "create-builder", "#{builder_repo}:#{stack}-v#{version}", "--builder-config", "#{builder_config_file}" or exit 1
+system "docker", "save", "#{builder_repo}:#{stack}-v#{version}", "-o", "builder-image/builder.tgz" or exit 1
