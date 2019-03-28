1aef5961bc278b236ee730036a0e0f6321bce379
Tyler Phelan <tphelan@pivotal.io>
2019-03-28 16:58:58 -0400

    Add test task for builder images
    [#164646384]
    
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

---
+++ tasks/test-builder/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+./buildpacks-ci/scripts/start-docker
+
+docker load -i builder-image/builder.tgz
+
+pushd pack
+    go build -mod=vendor -o pack ./cmd/pack/main.go
+popd
+
+./pack/pack build node-app --builder "$BUILDER_REPO:$STACK" -p pack/acceptance/testdata/node_app
