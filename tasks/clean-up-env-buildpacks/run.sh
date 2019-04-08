aaadc36235cc0576bebbfa404c0ea1d567c69f1a
Tyler Phelan <tphelan@pivotal.io>
2019-04-08 10:29:27 -0400

    Install nginx and r buildpacks for cflinufs2 for edge envs
    - These buildpacks don't get installed with cf-deployment but we run tests for them
    
    Co-authored-by: Chhavi Kankaria <ckankaria@pivotal.io>

--- tasks/clean-up-env-buildpacks/run.sh
+++ tasks/clean-up-env-buildpacks/run.sh
@@ -17,9 +17,7 @@ cf api "$target" --skip-ssl-validation || (sleep 4 && cf api "$target" --skip-ss
 cf auth admin "$cf_password" || (sleep 4 && cf auth admin "$cf_password")
 set -x
 
-# We don't have the hwc-buildpack in this list because cf-deployment installs it as nil-stack and we want to
-# delete it here and replace it later.
-custom_buildpacks=$(cf buildpacks | tail -n +4 | awk '{ print $1,$6 }' | grep -v 'apt_buildpack\|binary_buildpack\|credhub_buildpack\|dotnet_core_buildpack\|go_buildpack\|nginx_buildpack\|nodejs_buildpack\|php_buildpack\|python_buildpack\|r_buildpack\|ruby_buildpack\|staticfile_buildpack\|java_buildpack' | grep 'cflinuxfs2\|cflinuxfs3\|windows' || true)
+custom_buildpacks=$(cf buildpacks | tail -n +4 | awk '{ print $1,$6 }' | grep -v 'apt_buildpack\|binary_buildpack\|credhub_buildpack\|dotnet_core_buildpack\|go_buildpack\|nginx_buildpack\|nodejs_buildpack\|php_buildpack\|python_buildpack\|r_buildpack\|ruby_buildpack\|staticfile_buildpack\|java_buildpack\|hwc_buildpack' | grep 'cflinuxfs2\|cflinuxfs3\|windows' || true)
 
 echo "$custom_buildpacks" | while read -r bp_and_stack; do
   bp=$(echo $bp_and_stack | cut -d ' ' -f1)
@@ -33,27 +31,25 @@ for bp in $null_buildpacks; do
   cf delete-buildpack "$bp" -f -s ""
 done
 
-if [ "$INSTALL_STACK_ASSOC_HWC_BPS" = true ] ; then
-  for windows_stack in windows2012R2 windows2016 windows; do
-    pushd hwc-buildpack
+if [ "$INSTALL_ADDITIONAL" = true ] ; then
+    pushd r-buildpack
         export GOBIN=$PWD/.bin
         export PATH=$GOBIN:$PATH
        (go install github.com/cloudfoundry/libbuildpack/packager/buildpack-packager)
 
-        buildpack-packager build -stack "$windows_stack"
-        cf create-buildpack hwc_buildpack hwc_buildpack-*.zip 999
-        rm hwc_buildpack-*.zip
+        buildpack-packager build -stack "cflinuxfs2"
+        cf create-buildpack r_buildpack r_buildpack-*.zip 999
+        rm r_buildpack-*.zip
     popd
 
-    pushd binary-buildpack
+    pushd nginx-buildpack
         export GOBIN=$PWD/.bin
         export PATH=$GOBIN:$PATH
        (go install github.com/cloudfoundry/libbuildpack/packager/buildpack-packager)
 
-        buildpack-packager build -stack "$windows_stack"
-        cf update-buildpack binary_buildpack -p binary_buildpack-*.zip -s "$windows_stack" || cf create-buildpack binary_buildpack binary_buildpack-*.zip 999 || true
-        rm binary_buildpack-*.zip
+        buildpack-packager build -stack "cflinuxfs2"
+        cf create-buildpack nginx_buildpack nginx_buildpack-*.zip 999
+        rm nginx_buildpack-*.zip
     popd
-  done
 fi
 
