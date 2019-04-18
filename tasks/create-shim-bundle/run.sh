e6057fa69ad0097fac0fc435f5c5475335921f57
Tilly Taylor <mataylor@pivotal.io>
2019-04-18 11:57:32 -0400

    Cut sha to 8 characters
    - Use generic versions bucket so we dont have to keep making more buckets
    
    [#164880949]
    
    Co-authored-by: Tyler Phelan <tphelan@pivotal.io>

--- tasks/create-shim-bundle/run.sh
+++ tasks/create-shim-bundle/run.sh
@@ -15,4 +15,4 @@ tar czf archive/shim-bundle.tgz detect supply finalize release
 
 sha=$(sha256sum archive/shim-bundle.tgz | cut -d ' ' -f 1)
 
-mv archive/shim-bundle.tgz "archive/shim-bundle-$version-$sha.tgz"
+mv archive/shim-bundle.tgz "archive/shim-bundle-$version-${sha::8}.tgz"
