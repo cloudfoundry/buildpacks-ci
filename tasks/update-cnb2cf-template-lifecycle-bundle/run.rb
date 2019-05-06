6d9f20436e9a72f6de8dcd8ab52431e029a83f08
Tyler Phelan <tphelan@pivotal.io>
2019-05-06 09:55:14 -0400

    Fix path correctly
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

--- tasks/update-cnb2cf-template-lifecycle-bundle/run.rb
+++ tasks/update-cnb2cf-template-lifecycle-bundle/run.rb
@@ -7,50 +7,50 @@ require_relative "#{buildpacks_ci_dir}/lib/git-client"
 
 ALL_STACKS = ['cflinuxfs2', 'cflinuxfs3']
 
-repo = 'cnb2cf'
-name = 'lifecycle'
-manifest_path = "#{repo}/template/manifest.yml"
-manifest = YAML.load_file(manifest_path)
-version = File.read('s3/version').strip()
-build = JSON.parse(File.read("builds/binary-builds-new/lifecycle/#{version}-any-stack.json"))
+repo          = 'cnb2cf'
+name          = 'lifecycle'
+manifest_path = 'template/manifest.yml'
+manifest      = YAML.load_file(File.join(repo, manifest_path))
+version       = File.read('s3/version').strip()
+build         = JSON.parse(File.read("builds/binary-builds-new/lifecycle/#{version}-any-stack.json"))
 
 
-added = []
+added   = []
 removed = []
 rebuilt = []
 
 total_stacks = []
 
-source_url = build['source']['url']
+source_url    = build['source']['url']
 source_sha256 = build['source']['sha256']
 
 dep = {
-  'name' => name,
-  'version' => version,
-  'uri' => build['url'],
-  'sha256' => build['sha256'],
-  'cf_stacks' => ALL_STACKS,
-  'source' => source_url,
+  'name'          => name,
+  'version'       => version,
+  'uri'           => build['url'],
+  'sha256'        => build['sha256'],
+  'cf_stacks'     => ALL_STACKS,
+  'source'        => source_url,
   'source_sha256' => source_sha256
 }
 
 old_versions = manifest.fetch('dependencies', [])
-               .select { |d| d['name'] == name }
-               .map {|d| d['version']}
+  .select { |d| d['name'] == name }
+  .map { |d| d['version'] }
 
 manifest['dependencies'] = Dependencies.new(
-    dep,
-    'major',
-    'remove_all',
-    manifest['dependencies'],
-    []
+  dep,
+  'major',
+  'remove_all',
+  manifest['dependencies'],
+  []
 ).switch
 
 new_versions = manifest.fetch('dependencies', [])
-               .select { |d| d['name'] == name }
-               .map { |d| d['version'] }
+  .select { |d| d['name'] == name }
+  .map { |d| d['version'] }
 
-added += (new_versions - old_versions).uniq.sort
+added   += (new_versions - old_versions).uniq.sort
 removed += (old_versions - new_versions).uniq.sort
 rebuilt += [old_versions.include?(version)]
 
