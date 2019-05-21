0f70fb3f2dfe794343f7ff8154ddb0e983d9a723
Daniel Thornton <dthornton@pivotal.io>
2019-05-21 13:53:03 -0400

    Add summary in same job as creating release
    - Don't need github anymore since we are pulling in the existing github release
    
    [#164881372]
    
    Co-authored-by: Tyler Phelan <tphelan@pivotal.io>

--- tasks/ship-v3-buildpack/run.rb
+++ tasks/ship-v3-buildpack/run.rb
@@ -3,44 +3,34 @@ require 'octokit'
 require 'toml'
 require 'fileutils'
 
-Octokit.auto_paginate = true
-Octokit.configure do |c|
-  c.login    = ENV.fetch('GITHUB_USERNAME')
-  c.password = ENV.fetch('GITHUB_PASSWORD')
-  token = ENV.fetch("GIT_TOKEN","")
-  c.access_token = token if token != ""
-end
 language = ENV['LANGUAGE']
-
-last_version = Gem::Version.new("0.0.0")
-
-releases = Octokit.releases("cloudfoundry/#{language}-cnb").map(&:name)
-if releases.size > 0
-  last_version = Gem::Version.new(releases.first.gsub('v',''))
+packager_path = File.absolute_path(File.join('buildpack', '.bin', 'packager'))
+release_body_file = File.absolute_path(File.join('release-artifacts', 'body'))
+last_version = Gem::Version.new(File.read(File.join('buildpack-github-release', 'version')).strip.tr('v',''))
+next_version = Gem::Version.new(TOML.load_file('buildpack/buildpack.toml')['buildpack']['version'])
+if next_version <= last_version
+  raise "#{next_version.to_s} does not come after the current release #{last_version.to_s}"
 end
 
-next_version = Gem::Version.new(TOML.load_file('buildpack/buildpack.toml')['buildpack']['version'])
+Dir.chdir('packager/packager') do
+  `go build -o #{packager_path}`
+end
 
-if next_version > last_version
-  File.write('release-artifacts/name', "v#{next_version.to_s}")
-  File.write('release-artifacts/tag', "v#{next_version.to_s}")
-  changes = File.read('buildpack/CHANGELOG')
-  recent_changes = changes.split(/^v[0-9\.]+.*?=+$/m)[1]
+File.write('release-artifacts/name', "v#{next_version.to_s}")
+File.write('release-artifacts/tag', "v#{next_version.to_s}")
+changes = File.read('buildpack/CHANGELOG')
+recent_changes = changes.split(/^v[0-9\.]+.*?=+$/m)[1]
 
-  if recent_changes != nil 
-    recent_changes = recent_changes.strip
-  else 
-    recent_changes = ""
-  end
-  
-  File.write('release-artifacts/body', "#{recent_changes}\n")
-
-  target = File.join(Dir.pwd, "release-artifacts", "#{language}-cnb-#{next_version.to_s}")
-  Dir.chdir('buildpack') do
-    `scripts/install_tools.sh`
-    `.bin/packager -archive -uncached #{target}`
-  end
-
+if recent_changes != nil
+  recent_changes = recent_changes.strip
 else
-  raise "#{next_version.to_s} does not come after the current release #{last_version.to_s}"
+  recent_changes = ""
+end
+
+File.write(release_body_file, "#{recent_changes}\n")
+
+target = File.join(Dir.pwd, "release-artifacts", "#{language}-cnb-#{next_version.to_s}")
+Dir.chdir('buildpack') do
+  `#{packager_path} -archive -uncached #{target}`
+  File.write(release_body_file, `#{packager_path} -summary`, mode: 'a')
 end
