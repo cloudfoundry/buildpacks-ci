b43cfad10277274f24329a228fe1faceede44d9b
Chhavi Kankaria <ckankaria@pivotal.io>
2019-06-06 17:49:48 -0400

    Add double quotes to a variable

--- tasks/get-cf-creds/run.sh
+++ tasks/get-cf-creds/run.sh
@@ -9,4 +9,5 @@ pushd "bbl-state/$ENV_NAME"
   eval "$(bbl print-env)"
   set -x
 popd
-  credhub get -n /bosh-$ENV_NAME/cf/cf_admin_password -j | jq -r .value > cf-admin-password/password
+  credhub get -n /bosh-"$ENV_NAME"/cf/cf_admin_password -j | jq -r .value > cf-admin-password/password
+
