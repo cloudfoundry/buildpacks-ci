476160ac25cd39316f85fc8759238b14209abc54
Chhavi Kankaria <ckankaria@pivotal.io>
2019-06-06 17:41:33 -0400

    Add tasks to get cf-release env's password from credhub and pass it along

--- tasks/get-cf-creds/run.sh
+++ tasks/get-cf-creds/run.sh
@@ -5,14 +5,8 @@ set -o nounset
 set -o pipefail
 
 pushd "bbl-state/$ENV_NAME"
-set +x
-eval "$(bbl print-env)"
-set -x
-trap "pkill -f ssh" EXIT
+  set +x
+  eval "$(bbl print-env)"
+  set -x
 popd
-
-
-#bbl_env, err = Open3.capture3("bbl --state-dir #{env_home} print-env")
-#31     err_handle(err)
-#➤  32     cf_pass, err, status = Open3.capture3("eval \"#{bbl_env}\" && credhub get -n /bosh-#{env_name}/cf/                       cf_admin_password -j | jq -r .value")
-#33     err_handle(err)
+  credhub get -n /bosh-$ENV_NAME/cf/cf_admin_password -j | jq -r .value > cf-admin-password/password
