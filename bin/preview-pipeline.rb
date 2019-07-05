ee3d66dfb9c5c84e6b73d424b3c180c0b95a44bf
Daniel Thornton <dthornton@pivotal.io>
2019-07-05 14:39:22 -0400

    add preview-pipeline script

---
+++ bin/preview-pipeline.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require_relative '../lib/buildpacks-ci-configuration'
+require 'yaml'
+
+# USAGE: just pass any prefix string of the pipeline you want eg ./bin/preview-pipeline dependency-buil
+# will output the dependency-builds pipeline
+
+def preview_pipelines(selector)
+  buildpacks_configuration = BuildpacksCIConfiguration.new
+  organization = buildpacks_configuration.organization
+  run_php_oracle_tests = buildpacks_configuration.run_oracle_php_tests?
+
+  Dir['pipelines/*.yml'].each do |filename|
+    if File.basename(filename).start_with?(selector)
+      outstring = `erb organization=#{organization} run_oracle_php_tests=#{run_php_oracle_tests} #{filename}`
+      puts outstring.gsub(/\nS+/,"\n")
+    end
+  end
+end
+
+
+preview_pipelines(ARGV.first)
