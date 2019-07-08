0254567164e5cf27bc642b1017072b2af859ae92
Zander Mackie <amackie@pivotal.io>
2019-07-08 17:48:05 -0400

    Add node-engine-cnb auto-release
    - Template auto-release pipeline
    - Print buildpack location and suppress pack output
    [#166793497]
    
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

--- tasks/run-packaged-v3-bp-integration/run.sh
+++ tasks/run-packaged-v3-bp-integration/run.sh
@@ -2,18 +2,15 @@
 set -o errexit
 set -o pipefail
 
-#PACK_VERSION="$(cat pack/version)"
-#export PACK_VERSION
-
 if [[ -d "release-artifacts" ]]; then
     BP_PACKAGED_PATH="$(realpath "$(find release-artifacts -name "*.tgz")")"
     export BP_PACKAGED_PATH
-    echo "${BP_PACKAGED_PATH}"
+    echo "Buildpack is at: ${BP_PACKAGED_PATH}"
 fi
 
 PACK_PATH="$(realpath "$(find pack -name "*linux.tgz")")"
 mkdir -p buildpack/.bin
-tar xvf "${PACK_PATH}" -C buildpack/.bin
+tar xvf "${PACK_PATH}" -C buildpack/.bin > /dev/null
 
 cd buildpack
 
