a91aef8447fd1d9abfb740703b74ada45c783e31
Don Nguyen <dnguyen@pivotal.io>
2019-08-05 17:29:11 -0400

    Add build-cnb-buildpack task to construct shimmed cf buildpacks from cnbs
    - Add BUILDPACK_FILE param to run-scripts-integration
    [#167052988]
    
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

---
+++ tasks/build-cnb-buildpack/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+set -x
+
+pushd cnb2cf
+  ./scripts/build.sh
+popd
+
+version=$(cat version/version)
+pushd repo
+  mkdir .bin
+  mv ../cnb2cf/build/cnb2cf .bin/cnb2cf
+  ./scripts/package.sh -v "${version}"
+#  TODO: Configure cnb2cf to output to desired dir
+  mv "nodejs_buildpack-v${version}.zip" ../release-candidate
+popd
