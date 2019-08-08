ebdc9c05d45271dfba2ff34b18cc81b5b1e70495
Don Nguyen <dnguyen@pivotal.io>
2019-08-08 17:43:57 -0400

    Fix smoke-tests for CNB builders
    Co-authored-by: Guillermo Kardolus <gkardolus@pivotal.io>

--- tasks/test-builder/run.sh
+++ tasks/test-builder/run.sh
@@ -17,12 +17,12 @@ popd
 
 echo "Building test apps..."
 for app_path in cnb-builder/fixtures/*; do
-    ./pack/pack build "$(basename "$app_path")" --builder "$REPO:$STACK" -p "$app_path"
+    ./pack/pack build "$(basename "$app_path")" --builder "$REPO:$STACK" -p "$app_path" --no-pull
 done
 
 if [ "$STACK" == "cflinuxfs3" ]; then
     for app_path in cnb-builder/fs3-fixtures/*; do
-	./pack/pack build "$(basename "$app_path")" --builder "$REPO:$STACK" -p "$app_path"
+	./pack/pack build "$(basename "$app_path")" --builder "$REPO:$STACK" -p "$app_path" --no-pull
     done
 fi
 
