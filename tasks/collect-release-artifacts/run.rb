5f9560a096398fca8f7e93b276fcb30a1c6a844f
Forest Eckhardt <feckhardt@pivotal.io>
2019-08-08 11:44:31 -0400

    Create collect-release-artifacts task for shimmed buildpack release
    [#167052988]
    
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

---
+++ tasks/collect-release-artifacts/run.rb
@@ -0,0 +1,113 @@
+require 'octokit'
+require 'yaml'
+require 'rubygems'
+require 'zip'
+require 'digest'
+
+repo = ENV.fetch('REPO')
+stack = ENV.fetch('STACK', "cflinuxfs3")
+GITHUB_ACCESS_TOKEN = ENV.fetch('GITHUB_ACCESS_TOKEN')
+
+Octokit.configure do |c|
+  c.access_token = GITHUB_ACCESS_TOKEN
+end
+
+#read manifest of previous version here by unzipping buildpack-github-release
+def open_manifest_from_zip(path)
+  manifest = ""
+  Zip::File.open(path) do |zip_file|
+    entry = zip_file.glob('*/manifest.yml').first
+    if entry.file?
+      manifest = YAML.load(entry.get_input_stream.read)
+    end
+  end
+  manifest
+end
+
+def reduce_manifest(manifest)
+  deps = manifest.fetch('dependencies').reduce({}) do |accumulator, dep|
+    accumulator[dep['name']] = ['version']
+    accumulator
+  end
+  deps.reject!{|key| key == "lifecycle"}
+end
+
+def cnb_name(name)
+  cnb_name = name.split('.').last
+  if name.start_with? "org.cloudfoundry"
+    "#{cnb_name}-cnb"
+  elsif name.start_with? "io.pivotal"
+    "p-#{cnb_name}-cnb"
+  else
+    raise "unknown cnb path"
+  end
+end
+
+def get_url(name)
+  if name.start_with? "org.cloudfoundry"
+    "cloudfoundry/#{cnb_name(name)}"
+  elsif name.start_with? "io.pivotal"
+    "pivotal-cf/#{cnb_name(name)}"
+  else
+    raise "unknown cnb path"
+  end
+end
+
+def find_version_diffs(old_deps, new_deps)
+  cnb_version_map = {}
+  new_deps.each do |dep, version|
+    if old_deps.include? dep
+      old_version = old_deps[dep]
+
+      cnb_tags = Octokit.tags(get_url(dep)).collect{|tag| tag.name}
+      # Get the releases in between the last and the current, inclusive of the current release
+      diff_version = cnb_tags[cnb_tags.index("v#{version}"),cnb_tags.index("v#{old_version}") - 1]
+      cnb_version_map[dep] = diff_version
+    end
+  end
+
+  cnb_version_map
+end
+
+def compile_release_notes(cnbs)
+  release_notes = ""
+
+  cnbs.each do |cnb, versions|
+    release_notes << "\n# #{cnb_name(cnb)} \n"
+    # Assumes there's no weird stuff
+    releases = Octokit.releases(get_url(cnb)).select{|release| versions.include? release.name}
+    releases.each do |release|
+      trimmed_release_notes = release.body.split('Supported stacks:')[0].strip!
+      release_notes << "## #{release.name}\n#{trimmed_release_notes}\n"
+    end
+  end
+
+  release_notes
+end
+
+old_manifest = open_manifest_from_zip(File.absolute_path(File.join('buildpack-github-release', 'source.zip')))
+old_manifest_deps = reduce_manifest(old_manifest)
+
+rc_path = Dir.glob(File.join("release-candidate", "*-v*.zip")).first
+absolute_rc_path = File.absolute_path(rc_path)
+current_manifest = open_manifest_from_zip(absolute_rc_path)
+current_manifest_deps = reduce_manifest(current_manifest)
+
+cnbs = find_version_diffs(old_manifest_deps, current_manifest_deps)
+
+release_notes = compile_release_notes(cnbs)
+version = File.read(File.join("version", "version")).strip()
+tag = "v#{version}"
+
+# get shasum of rc file
+rc_shasum = Digest::SHA256.file(absolute_rc_path).hexdigest
+
+# write version, release notes, shasum to output dir
+# Move and rename rc to buildpack path
+output_dir = File.absolute_path("buildpack-artifacts")
+bp_name = "#{repo}-#{stack}-#{tag}.zip"
+sha_file_name = "#{bp_name}.SHA256SUM.txt"
+File.write(File.join(output_dir, "tag"), tag)
+File.write(File.join(output_dir, "release_notes"), release_notes)
+File.rename(absolute_rc_path, File.join(output_dir, bp_name))
+File.write(File.join(output_dir, sha_file_name), rc_shasum)
