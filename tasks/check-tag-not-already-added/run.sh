74279f5dfcbaf0c9d1e08069990c5e1d07c27cd6
Forest Eckhardt <feckhardt@pivotal.io>
2019-08-08 12:31:03 -0400

    Remove VERSION param, add optional version input to check-tag job
    [#167052988]
    
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

--- tasks/check-tag-not-already-added/run.sh
+++ tasks/check-tag-not-already-added/run.sh
@@ -4,6 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+VERSION=""
+if [[ -d "version" ]]; then
+    VERSION=$(cat version/version)
+else
+    VERSION=$(cat buildpack/VERSION)
+fi
+
 cd buildpack
-TEST_VERSION=${VERSION:-$(cat VERSION)}
-git tag "v${TEST_VERSION}"
+git tag "v${VERSION}"
