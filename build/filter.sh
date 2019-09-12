888086a499752be489238158dd64b4b6e7df82d9
Mark DeLillo <mdelillo@pivotal.io>
2019-09-12 16:01:18 -0400

    Overwrite /bin/bash with shim to filter concourse output
    [#168353012]

--- build/filter.sh
+++ build/filter.sh
@@ -83,5 +83,3 @@ USER"
 if [ ! -z "${TEST_CREDENTIAL_FILTER_WHITELIST:-}" ]; then
   export CREDENTIAL_FILTER_WHITELIST="$CREDENTIAL_FILTER_WHITELIST,$TEST_CREDENTIAL_FILTER_WHITELIST"
 fi
-
-exec > >(concourse-filter) 2> >(concourse-filter -stderr)
