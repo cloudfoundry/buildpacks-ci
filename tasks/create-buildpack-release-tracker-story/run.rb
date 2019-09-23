816a6c88e619d4a842f7bfb1ebafe14b42d973e1
Chhavi Kankaria <ckankaria@pivotal.io>
2019-09-23 10:16:35 -0400

    Look in both backlogs to create buildpack release story
    * Put dep bump and version line stories into releng backlog
    
    [#168513268]

--- tasks/create-buildpack-release-tracker-story/run.rb
+++ tasks/create-buildpack-release-tracker-story/run.rb
@@ -5,6 +5,7 @@ require_relative '../../lib/buildpack-release-story-creator'
 buildpack_name = ENV.fetch('BUILDPACK_NAME')
 previous_buildpack_version = File.read('buildpack/VERSION')
 tracker_project_id = ENV.fetch('TRACKER_PROJECT_ID')
+releng_tracker_project_id = ENV.fetch('RELENG_TRACKER_PROJECT_ID')
 tracker_requester_id = ENV.fetch('TRACKER_REQUESTER_ID').to_i
 tracker_api_token = ENV.fetch('TRACKER_API_TOKEN')
 
@@ -12,6 +13,7 @@ buildpack_release_story_creator = BuildpackReleaseStoryCreator.new(
     buildpack_name:             buildpack_name,
     previous_buildpack_version: previous_buildpack_version,
     tracker_project_id:         tracker_project_id,
+    releng_tracker_project_id:  releng_tracker_project_id,
     tracker_requester_id:       tracker_requester_id,
     tracker_api_token:          tracker_api_token
 )
