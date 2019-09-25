dafc9bbf7fa422526022b59488e293b4c53a0a23
Ryan Moran <rmoran@pivotal.io>
2019-09-25 11:46:06 -0400

    Fix ownership of GOCACHE when running unprivileged
    [#168510955]

--- tasks/run-scripts-unit/run.sh
+++ tasks/run-scripts-unit/run.sh
@@ -9,6 +9,10 @@ export GOBIN=$PWD/.bin
 export PATH=$GOBIN:$PATH
 
 if [[ "${RUN_UNPRIVILEGED}" == "true" ]]; then
+  export GOCACHE=/home/testuser/.cache/go-build
+
+  mkdir -p "${GOCACHE}"
+  chown -R testuser:testuser /home/testuser
   chpst -u testuser:testuser ./scripts/unit.sh
 else
   ./scripts/unit.sh
