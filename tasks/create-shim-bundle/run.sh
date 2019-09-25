c6878e52dafcc7d8d12f49bca85b89cac7c193c6
Ryan Moran <rmoran@pivotal.io>
2019-09-25 11:46:06 -0400

    Fix ownership of GOCACHE when running unprivileged
    [#168510955]

--- tasks/create-shim-bundle/run.sh
+++ tasks/create-shim-bundle/run.sh
@@ -2,14 +2,27 @@
 
 set -euo pipefail
 
-version=$(cat version/version)
+readonly SHIM_DIR="${PWD}/shim"
+readonly ARCHIVE_DIR="${PWD}/archive"
+readonly VERSION_DIR="${PWD}/version"
 
-pushd shim
-    ./scripts/build.sh
-popd
+function main() {
+    local version sha
+    version="$(cat "${VERSION_DIR}/version")"
 
-tar czf archive/shim-bundle.tgz detect supply finalize release
+    pushd "${SHIM_DIR}" > /dev/null || return
+        ./scripts/build.sh
+    popd > /dev/null || return
 
-sha=$(sha256sum archive/shim-bundle.tgz | cut -d ' ' -f 1)
+    pushd "${SHIM_DIR}/template/bin" > /dev/null || return
+        tar czf \
+            "${ARCHIVE_DIR}/shim-bundle.tgz" \
+            detect supply finalize release
+    popd > /dev/null || return
 
-mv archive/shim-bundle.tgz "archive/shim-bundle-$version-${sha::8}.tgz"
+    sha="$(sha256sum "${ARCHIVE_DIR}/shim-bundle.tgz" | cut -d ' ' -f 1)"
+
+    mv "${ARCHIVE_DIR}/shim-bundle.tgz" "${ARCHIVE_DIR}/shim-bundle-${version}-${sha::8}.tgz"
+}
+
+main
