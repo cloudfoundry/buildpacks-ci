63126bddc6a7ab5d7411c87e96245d715d3cad12
Malini Valliath <mvalliath@pivotal.io>
2019-10-22 14:15:36 -0400

    Add cnb-release-candidate resource
    - Update usage of candidate resource to cnb resource
    - Comment out existing resource for now
    
    [#169270242]

--- tasks/promote-candidate-to-rc/run.sh
+++ tasks/promote-candidate-to-rc/run.sh
@@ -3,4 +3,4 @@
 set -exo pipefail
 
 #mv candidate/candidate.zip "release-candidate/nodejs_buildpack-v$(cat version/version).zip"
-mv candidate/metacnb-candidate.tgz "release-candidate/${LANGUAGE}_cnb-v$(cat version/version).tgz"
+mv candidate/metacnb-candidate.tgz "cnb-release-candidate/${LANGUAGE}_cnb-v$(cat version/version).tgz"
