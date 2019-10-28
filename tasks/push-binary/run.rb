4609b4d85be4fc0b8255e142ebee3ca7a61e3afd
Chhavi Kankaria <ckankaria@pivotal.io>
2019-10-28 16:20:08 -0400

    Delete unused tasks from the CI repo [#159770301]

--- tasks/push-binary/run.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-# encoding: utf-8
-
-require_relative 's3-dependency-uploader'
-
-S3DependencyUploader.new(ENV.fetch('DEPENDENCY'), ENV.fetch('BUCKET_NAME'), 'binary-builder-artifacts').run
