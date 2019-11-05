ce71fe503ab91972a47e5cc22ddcd4e1865e2074
David Freilich <dfreilich@pivotal.io>
2019-11-05 09:54:43 -0500

    First commit - make run method
    [#168522833]

--- go.mod
+++ go.mod
@@ -5,11 +5,12 @@ go 1.12
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/blang/semver v3.5.1+incompatible
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.1.2
 	github.com/pkg/errors v0.8.1
 	github.com/sclevine/spec v1.3.0
-	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.4.0
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-	gopkg.in/yaml.v2 v2.2.4
+	gopkg.in/yaml.v2 v2.2.5
 )
--- tasks/track-upstream-deps-timeline/main.go
+++ tasks/track-upstream-deps-timeline/main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"log"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
 func main() {
-	
+	if err := run(); err != nil {
+		log.Fatalf("failed to run: %s", err)
+	}
+	log.Println("succesfully ran")
+}
+
+// Problem Statement:
+// Display in output of some sort, the time difference between ruby-2.6.5 was released (Oct 1)
+// and when it was released on github
+// as well as pivnet
+// output the date difference
+func run() error {
+	time, err := FileDate("/Users/pivotal/workspace/public-buildpacks-ci-robots/binary-builds-new/ruby/2.6.5.json")
+	if err != nil {
+		return errors.Wrap(err, "failed to get date of file")
+	}
+	log.Println(time)
+	return nil
+}
+
+func FileDate(path string) (time.Time, error) {
+	fileDir := filepath.Dir(path)
+	fileName := filepath.Base(path)
+
+	//Running git log -1 --format="%ai" --reverse public-buildpacks-ci-robots/binary-builds-new/ruby/2.6.5.json
+	cmd := exec.Command("git", "log", "-1", "--format=\"%aI\"", "--reverse", fileName)
+	cmd.Dir = fileDir
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return time.Time{}, errors.Wrap(err, "failed to run `git log`")
+	}
+
+	fileDate, err := time.Parse(time.RFC3339, string(output))
+	if err != nil {
+		return time.Time{}, errors.Wrap(err, "failed to parse the date")
+	}
+	return fileDate, nil
 }
