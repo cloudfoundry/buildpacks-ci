5c1b3351599ba3bb70bea18bdf7038d1d46e8f93
David Freilich <dfreilich@pivotal.io>
2019-11-05 17:28:31 -0500

    Succesfully finds github and pivnet release dates using go clients
    [#168522833]

--- go.mod
+++ go.mod
@@ -6,11 +6,18 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/blang/semver v3.5.1+incompatible
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/mitchellh/mapstructure v1.1.2
+	github.com/pivotal-cf/go-pivnet/v2 v2.0.10
 	github.com/pkg/errors v0.8.1
 	github.com/sclevine/spec v1.3.0
 	github.com/stretchr/testify v1.4.0
+	golang.org/x/net v0.0.0-20191101175033-0deb6923b6d9 // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20191104094858-e8c54fb511f6 // indirect
+	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.2.5
 )
--- tasks/track-upstream-deps-timeline/main.go
+++ tasks/track-upstream-deps-timeline/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 
+	"github.com/pivotal-cf/go-pivnet/v2/logshim"
+
+	"github.com/pivotal-cf/go-pivnet/v2"
+
+	"github.com/google/go-github/github"
 	"github.com/pkg/errors"
+	"golang.org/x/oauth2"
 )
 
+var flags struct {
+	githubToken string
+}
+
 func main() {
+	flag.StringVar(&flags.githubToken, "github-token", "", "github token to authenticate")
+	flag.Parse()
+
 	if err := run(); err != nil {
 		log.Fatalf("failed to run: %s", err)
 	}
@@ -22,11 +40,27 @@ func main() {
 // as well as pivnet
 // output the date difference
 func run() error {
-	time, err := FileDate("/Users/pivotal/workspace/public-buildpacks-ci-robots/binary-builds-new/ruby/2.6.5.json")
+	upstreamDependencyReleased, err := FileDate("/Users/pivotal/workspace/public-buildpacks-ci-robots/binary-builds-new/ruby/2.6.5.json")
 	if err != nil {
 		return errors.Wrap(err, "failed to get date of file")
 	}
-	log.Println(time)
+	log.Printf("Upstream Dependency Released: %v\n", upstreamDependencyReleased)
+
+	org := "cloudfoundry"
+	repo := "ruby-buildpack"
+	storyID := "168867584"
+	_, releasedOnGithub, err := FindGithubReleaseOfStory(org, repo, storyID, flags.githubToken)
+	if err != nil {
+		return errors.Wrap(err, "failed to get date of file")
+	}
+	log.Printf("Dependency Released on Github: %v\n", releasedOnGithub)
+
+	releasedOnPivnet, err := FindPivnetReleaseDate("ruby-buildpack", "1.8.0")
+	if err != nil {
+		return errors.Wrap(err, "failed to get date of file")
+	}
+	log.Printf("Dependency Released on Pivnet: %v\n", releasedOnPivnet)
+
 	return nil
 }
 
@@ -34,17 +68,77 @@ func FileDate(path string) (time.Time, error) {
 	fileDir := filepath.Dir(path)
 	fileName := filepath.Base(path)
 
-	//Running git log -1 --format="%ai" --reverse public-buildpacks-ci-robots/binary-builds-new/ruby/2.6.5.json
+	//Running git log -1 --format="%aI" --reverse public-buildpacks-ci-robots/binary-builds-new/ruby/2.6.5.json
 	cmd := exec.Command("git", "log", "-1", "--format=\"%aI\"", "--reverse", fileName)
 	cmd.Dir = fileDir
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return time.Time{}, errors.Wrap(err, "failed to run `git log`")
 	}
+	timeString := strings.TrimSpace(string(bytes.ReplaceAll(output, []byte("\""), []byte(""))))
 
-	fileDate, err := time.Parse(time.RFC3339, string(output))
+	fileDate, err := time.Parse(time.RFC3339, timeString)
 	if err != nil {
 		return time.Time{}, errors.Wrap(err, "failed to parse the date")
 	}
 	return fileDate, nil
+}
+
+func FindGithubReleaseOfStory(org, repo, storyID, githubToken string) (string, time.Time, error) {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	opts := &github.ListOptions{}
+
+	for {
+		releases, response, err := client.Repositories.ListReleases(ctx, org, repo, opts)
+		if err != nil {
+			return "", time.Time{}, errors.Wrap(err, fmt.Sprintf("failed to get releases for %s/%s", org, repo))
+		}
+
+		for _, release := range releases {
+			if strings.Contains(*release.Body, storyID) {
+				return *release.Name, release.PublishedAt.Time, nil
+			}
+		}
+
+		if response.NextPage == 0 {
+			break
+		}
+		opts.Page = response.NextPage
+	}
+
+	return "", time.Time{}, errors.New(fmt.Sprintf("failed to find %s in releases of %s/%s", storyID, org, repo))
+}
+
+func FindPivnetReleaseDate(product, releaseName string) (time.Time, error) {
+
+	config := pivnet.ClientConfig{
+		Host: pivnet.DefaultHost,
+	}
+	token := pivnet.NewAccessTokenOrLegacyToken("", "", true)
+	client := pivnet.NewClient(token, config, logshim.LogShim{})
+
+	releases, err := client.Releases.List(product)
+	if err != nil {
+		return time.Time{}, errors.Wrap(err, fmt.Sprintf("failed to get releases of %s", product))
+	}
+	for _, release := range releases {
+		if release.Version == releaseName {
+			releaseTime, err := time.Parse(time.RFC3339, release.SoftwareFilesUpdatedAt)
+			if err != nil {
+				return time.Time{}, errors.Wrap(err, "failed to parse time from release")
+			}
+			return releaseTime, nil
+		}
+	}
+
+	return time.Time{}, errors.New("this version hasn't been released yet")
+}
+
+func CompareDates(dateA, dateB time.Time) int {
+	return 0
 }
