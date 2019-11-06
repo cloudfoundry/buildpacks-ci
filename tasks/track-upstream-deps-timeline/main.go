988eafca4f6344687f8c393257fc9bef27a6dee9
David Freilich <dfreilich@pivotal.io>
2019-11-06 11:29:30 -0500

    Compute difference in days between dates
    * Finish v2b path for ruby
    [#168522833]

--- tasks/track-upstream-deps-timeline/main.go
+++ tasks/track-upstream-deps-timeline/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 
@@ -21,75 +25,113 @@ import (
 )
 
 var flags struct {
-	githubToken string
+	githubToken  string
+	dependency   string
+	version      string
+	organization string
+	repo         string
 }
 
-func main() {
-	flag.StringVar(&flags.githubToken, "github-token", "", "github token to authenticate")
-	flag.Parse()
+type BinaryBuildsFile struct {
+	TrackerStoryID int `json:"tracker_story_id"`
+}
 
+// Problem Statement:
+// Display the time difference between the release of a dependency (ruby-2.6.5)
+// when it was released on github
+// and when it was released on pivnet
+
+const DaysReleasedThreshold = 7
+
+func main() {
+	parseFlags()
 	if err := run(); err != nil {
 		log.Fatalf("failed to run: %s", err)
 	}
 	log.Println("succesfully ran")
 }
 
-// Problem Statement:
-// Display in output of some sort, the time difference between ruby-2.6.5 was released (Oct 1)
-// and when it was released on github
-// as well as pivnet
-// output the date difference
+func parseFlags() {
+	flag.StringVar(&flags.githubToken, "github-token", "", "github token to authenticate")
+	flag.StringVar(&flags.dependency, "dependency", "", "upstream dependency")
+	flag.StringVar(&flags.version, "version", "", "version of dependency")
+	flag.StringVar(&flags.organization, "organization", "cloudfoundry", "github organization of output")
+	flag.StringVar(&flags.repo, "repo", "", "repo of output")
+	flag.Parse()
+}
+
 func run() error {
-	upstreamDependencyReleased, err := FileDate("/Users/pivotal/workspace/public-buildpacks-ci-robots/binary-builds-new/ruby/2.6.5.json")
+	filePath := fmt.Sprintf("/Users/davidfreilich/workspace/public-buildpacks-ci-robots/binary-builds-new/%s/%s.json", flags.dependency, flags.version)
+	binaryBuilds, upstreamDependencyReleased, err := GetBinaryBuildsStoryAndDate(filePath)
 	if err != nil {
 		return errors.Wrap(err, "failed to get date of file")
 	}
 	log.Printf("Upstream Dependency Released: %v\n", upstreamDependencyReleased)
 
-	org := "cloudfoundry"
-	repo := "ruby-buildpack"
-	storyID := "168867584"
-	_, releasedOnGithub, err := FindGithubReleaseOfStory(org, repo, storyID, flags.githubToken)
+	githubReleaseVersion, githubReleaseDate, err := FindGithubReleaseOfStory(flags.organization, flags.repo, flags.githubToken, binaryBuilds.TrackerStoryID)
 	if err != nil {
 		return errors.Wrap(err, "failed to get date of file")
 	}
-	log.Printf("Dependency Released on Github: %v\n", releasedOnGithub)
+	log.Printf("Dependency Released on Github: %v\n", githubReleaseDate)
 
-	releasedOnPivnet, err := FindPivnetReleaseDate("ruby-buildpack", "1.8.0")
+	pivnetReleaseDate, err := FindPivnetReleaseDate(flags.repo, strings.TrimPrefix(githubReleaseVersion, "v"))
 	if err != nil {
 		return errors.Wrap(err, "failed to get date of file")
 	}
-	log.Printf("Dependency Released on Pivnet: %v\n", releasedOnPivnet)
+	log.Printf("Dependency Released on Pivnet: %v\n", pivnetReleaseDate)
 
+	daysToGithub := DaysBetweenDates(upstreamDependencyReleased, githubReleaseDate)
+	log.Printf("It took %d day(s) for %s %s to get released on github", daysToGithub, flags.dependency, flags.version)
+	if daysToGithub > DaysReleasedThreshold {
+		log.Printf("Failed to meet our desired threshold of %d days", DaysReleasedThreshold)
+	}
+	daysToPivnet := DaysBetweenDates(upstreamDependencyReleased, pivnetReleaseDate)
+	log.Printf("It took %d day(s) for %s %s to get released on pivnet", daysToPivnet, flags.dependency, flags.version)
+	if daysToGithub > DaysReleasedThreshold {
+		log.Printf("Failed to meet our desired threshold of %d days", DaysReleasedThreshold)
+	}
 	return nil
 }
 
-func FileDate(path string) (time.Time, error) {
+func GetBinaryBuildsStoryAndDate(path string) (BinaryBuildsFile, time.Time, error) {
 	fileDir := filepath.Dir(path)
 	fileName := filepath.Base(path)
+
+	binaryBuilds := BinaryBuildsFile{}
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		return BinaryBuildsFile{}, time.Time{}, errors.Wrap(err, fmt.Sprintf("failed to read file at: %s", path))
+	}
+	if err := json.Unmarshal(contents, &binaryBuilds); err != nil {
+		return BinaryBuildsFile{}, time.Time{}, errors.Wrap(err, fmt.Sprintf("failed to parse binary build from %s", string(contents)))
+	}
 
 	//Running git log -1 --format="%aI" --reverse public-buildpacks-ci-robots/binary-builds-new/ruby/2.6.5.json
 	cmd := exec.Command("git", "log", "-1", "--format=\"%aI\"", "--reverse", fileName)
 	cmd.Dir = fileDir
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return time.Time{}, errors.Wrap(err, "failed to run `git log`")
+		return BinaryBuildsFile{}, time.Time{}, errors.Wrap(err, "failed to run `git log`")
 	}
 	timeString := strings.TrimSpace(string(bytes.ReplaceAll(output, []byte("\""), []byte(""))))
 
 	fileDate, err := time.Parse(time.RFC3339, timeString)
 	if err != nil {
-		return time.Time{}, errors.Wrap(err, "failed to parse the date")
+		return BinaryBuildsFile{}, time.Time{}, errors.Wrap(err, "failed to parse the date")
 	}
-	return fileDate, nil
+	return binaryBuilds, fileDate, nil
 }
 
-func FindGithubReleaseOfStory(org, repo, storyID, githubToken string) (string, time.Time, error) {
+func FindGithubReleaseOfStory(org, repo, githubToken string, storyID int) (string, time.Time, error) {
+	var tc *http.Client
 	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: githubToken},
-	)
-	tc := oauth2.NewClient(ctx, ts)
+
+	if githubToken != "" {
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: githubToken},
+		)
+		tc = oauth2.NewClient(ctx, ts)
+	}
 	client := github.NewClient(tc)
 	opts := &github.ListOptions{}
 
@@ -100,7 +142,7 @@ func FindGithubReleaseOfStory(org, repo, storyID, githubToken string) (string, t
 		}
 
 		for _, release := range releases {
-			if strings.Contains(*release.Body, storyID) {
+			if strings.Contains(*release.Body, strconv.Itoa(storyID)) {
 				return *release.Name, release.PublishedAt.Time, nil
 			}
 		}
@@ -111,11 +153,10 @@ func FindGithubReleaseOfStory(org, repo, storyID, githubToken string) (string, t
 		opts.Page = response.NextPage
 	}
 
-	return "", time.Time{}, errors.New(fmt.Sprintf("failed to find %s in releases of %s/%s", storyID, org, repo))
+	return "", time.Time{}, errors.New(fmt.Sprintf("failed to find %v in releases of %s/%s", storyID, org, repo))
 }
 
 func FindPivnetReleaseDate(product, releaseName string) (time.Time, error) {
-
 	config := pivnet.ClientConfig{
 		Host: pivnet.DefaultHost,
 	}
@@ -139,6 +180,7 @@ func FindPivnetReleaseDate(product, releaseName string) (time.Time, error) {
 	return time.Time{}, errors.New("this version hasn't been released yet")
 }
 
-func CompareDates(dateA, dateB time.Time) int {
-	return 0
+func DaysBetweenDates(dateA, dateB time.Time) int {
+	diff := dateB.Sub(dateA)
+	return int(diff.Hours() / 24)
 }
