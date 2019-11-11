757e3ec7f40180c579e392121ec74dc7cb37a44c
Timothy Hitchener <thitchener@pivotal.io>
2019-11-11 16:27:42 -0500

    Updates cnb pipeline template to use new structure [#169542390]

--- tasks/run-packaged-v3-bp-integration/run.sh
+++ tasks/run-packaged-v3-bp-integration/run.sh
@@ -2,12 +2,6 @@
 set -o errexit
 set -o pipefail
 
-if [[ -d "release-artifacts" ]]; then
-    BP_PACKAGED_PATH="$(realpath "$(find release-artifacts -name "*.tgz")")"
-    export BP_PACKAGED_PATH
-    echo "Buildpack is at: ${BP_PACKAGED_PATH}"
-fi
-
 PACK_PATH="$(realpath "$(find pack -name "*linux.tgz")")"
 mkdir -p buildpack/.bin
 tar xvf "${PACK_PATH}" -C buildpack/.bin > /dev/null
