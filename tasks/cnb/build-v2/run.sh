e961101dbdab2dd459fbe2e88bd6ccd72f9351a4
Ryan Moran <rmoran@pivotal.io>
2019-11-13 10:33:03 -0500

    Update cnb/build-v2 task to use compat buildpack.toml
    [#169451374]

--- tasks/cnb/build-v2/run.sh
+++ tasks/cnb/build-v2/run.sh
@@ -6,19 +6,16 @@ set -o pipefail
 readonly ROOT="${PWD}"
 
 function main() {
-  ls -al "${ROOT}/cnb-tarball"
-  # Cut off the rc part of the version, so that ultimate RC will have the correct version file
   local version
-  version=$( cut -d '-' -f 1 version/version )
+  version="$( cut -d '-' -f 1 version/version )"
 
   mkdir -p "${ROOT}/build"
-
-  # shellcheck disable=SC2086
-  tar xzvf ${ROOT}/cnb-tarball/*.tgz -C "${ROOT}/build"
 
   pushd "${ROOT}/cnb2cf" > /dev/null || return
     ./scripts/build.sh
   popd > /dev/null || return
+
+  cp "${ROOT}/repo/compat/buildpack.toml" "${ROOT}/build"
 
   pushd "${ROOT}/build" > /dev/null || return
     "${ROOT}/cnb2cf/build/cnb2cf" package \
