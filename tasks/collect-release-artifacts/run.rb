94a6260b2f11b08c92a3c94b4f2936629257951c
Daniel Thornton <dthornton@pivotal.io>
2019-11-13 14:15:43 -0500

    Adds acceptance tests for shimmed meta cnbs pipelines
    [#169451374]

--- tasks/collect-release-artifacts/run.rb
+++ tasks/collect-release-artifacts/run.rb
@@ -20,45 +20,33 @@ Octokit.configure do |c|
   c.access_token = GITHUB_ACCESS_TOKEN
 end
 
-# old_manifest_deps = {}
-# latest_release_path = ReleaseArtifacts.download_latest_release(repo)
-# if File.exist? latest_release_path
-#   old_manifest = ReleaseArtifacts.open_manifest_from_zip(latest_release_path)
-#   old_manifest_deps = ReleaseArtifacts.reduce_manifest(old_manifest)
-# end
+old_manifest_deps = {}
+latest_release_path = ReleaseArtifacts.download_latest_release(repo)
+if File.exist? latest_release_path
+  old_manifest = ReleaseArtifacts.open_manifest_from_zip(latest_release_path)
+  old_manifest_deps = ReleaseArtifacts.reduce_manifest(old_manifest)
+end
 
 # below path is needed for the shimmed buildpack
-# rc_path = Dir.glob(File.join("cnb-release-candidate", "*-v*.zip")).first
-rc_path = Dir.glob(File.join("cnb-release-candidate", "*-v*.tgz")).first
+# rc_path = Dir.glob(File.join("v2-release-candidate", "*-v*.zip")).first
+rc_path = Dir.glob(File.join("v2-release-candidate", "*-v*.tgz")).first
 absolute_rc_path = File.absolute_path(rc_path)
 
-# current_manifest = ReleaseArtifacts.open_manifest_from_zip(absolute_rc_path)
-# current_manifest_deps = ReleaseArtifacts.reduce_manifest(current_manifest)
-#
-# cnb_version_diff = ReleaseArtifacts.find_version_diff(old_manifest_deps, current_manifest_deps)
+current_manifest = ReleaseArtifacts.open_manifest_from_zip(absolute_rc_path)
+current_manifest_deps = ReleaseArtifacts.reduce_manifest(current_manifest)
+
+version_diff = ReleaseArtifacts.find_version_diff(old_manifest_deps, current_manifest_deps)
 
 version = File.read(File.join("version", "version")).strip()
 tag = "v#{version}"
-# release_notes = ReleaseArtifacts.compile_release_notes(repo, tag, cnb_version_diff)
+release_notes = ReleaseArtifacts.compile_release_notes(repo, tag, cnb_version_diff)
 
-# extract buildpack.toml
-bp_toml = ReleaseArtifacts.open_buildpacktoml_from_tgz(absolute_rc_path)
-
-bp_toml_string = TOML::Generator.new(bp_toml).body
-File.open("buildpack.toml", "w") do |file|
-  file << bp_toml_string
-end
-
-puts `buildpack/scripts/install_tools.sh`
-
-packager_path = "buildpack/.bin/packager"
-release_notes = `#{packager_path} -summary`
 puts release_notes
 
 rc_shasum = Digest::SHA256.file(absolute_rc_path).hexdigest
 
 output_dir = File.absolute_path("buildpack-artifacts")
-cnb_name = "#{repo}-#{tag}.tgz"
+cnb_name = "#{repo}-#{tag}.zip"
 sha_file_name = "#{repo}-#{tag}.SHA256SUM.txt"
 
 File.write(File.join(output_dir, "tag"), tag)
