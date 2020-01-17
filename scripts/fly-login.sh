81df5faf7394802efbbd111b83c210c685b5efbe
Mark DeLillo <mdelillo@pivotal.io>
2020-01-17 10:43:21 -0500

    fly-login will do nothing if you are already logged in

--- scripts/fly-login.sh
+++ scripts/fly-login.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-# ./fly-login username password
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
-if (( $# != 2 ))
-then
-  fly -t buildpacks login -c https://buildpacks.ci.cf-app.com
-else
-  fly -t buildpacks login -c https://buildpacks.ci.cf-app.com -u "$1" -p "$2"
+if ! fly -t buildpacks status; then
+  if [[ $# != 2 ]] || [[ "$1" == "" ]] || [[ "$2" == "" ]]; then
+    fly -t buildpacks login -c https://buildpacks.ci.cf-app.com -b
+  else
+    fly -t buildpacks login -c https://buildpacks.ci.cf-app.com -u "$1" -p "$2"
+  fi
 fi
 
 fly -t buildpacks sync
