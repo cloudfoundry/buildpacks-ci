e9bcdcf948da3d6250f44a639106b14fb394d3ea
Marty Spiewak <mspiewak@pivotal.io>
2020-01-22 15:03:58 -0500

    Fix path for pipeline config file
    [#170129705]
    
    Co-authored-by: Mark DeLillo <mdelillo@pivotal.io>

--- tasks/cnb/update-dotnet-compatibility-table/run.sh
+++ tasks/cnb/update-dotnet-compatibility-table/run.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 cp -r buildpack/. artifacts
 
-buildpack_toml="$(cat "$PWD/buildpack/buildpack.toml")"
-sdk_version="$(jq -r .version.ref "$PWD/source/data.json")"
+buildpack_toml="$(cat "buildpack/buildpack.toml")"
+sdk_version="$(jq -r .version.ref "source/data.json")"
+runtime_version="$(cat "source/runtime_version")"
 output_dir="$PWD/artifacts"
-runtime_version="$(cat "$PWD/source/runtime_version")"
 
 pushd "buildpacks-ci/tasks/cnb/update-dotnet-compatibility-table" > /dev/null
   go run . \
