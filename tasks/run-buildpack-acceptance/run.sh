f02ef03f40b66b4a768a190febdd53ab250eb883
Daniel Thornton <dthornton@pivotal.io>
2020-01-23 12:22:26 -0500

    Update buildpack acceptance tasks to allow testing of cached buildpacks

--- tasks/run-buildpack-acceptance/run.sh
+++ tasks/run-buildpack-acceptance/run.sh
@@ -14,15 +14,22 @@ function main() {
     "${ROOT}/buildpacks-ci/scripts/start-docker" >/dev/null
   fi
 
-  local artifact_path version
+  local artifact_path version cached_flag
   artifact_path="$(find "${ROOT}/candidate" -name "*.zip" | head -1)"
   version="$( cut -d '-' -f 1 version/version )"
+  cached_flag=""
+
+  if [[ ${CACHED} == "true" ]]; then
+    cached_flag="--cached"
+  fi
 
   pushd buildpack-acceptance-tests
     ./scripts/integration.sh \
       --language "${LANGUAGE}" \
       --buildpack "${artifact_path}" \
-      --buildpack-version "${version}"
+      --buildpack-version "${version}" \
+      "${cached_flag}"
+
   popd
 }
 
