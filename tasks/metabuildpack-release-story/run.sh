fc3ed65375d8c53eadcf62dfb8c0fd240b6389d8
Daniel Thornton <dthornton@pivotal.io>
2020-03-09 15:02:15 -0400

    add task for creating metabuildpack release stories
    [#171024616]

---
+++ tasks/metabuildpack-release-story/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+main() {
+  #pushd feller || echo "failed to run feller"; return
+  pushd feller
+    echo "Creating Release Story"
+    go run main.go create-release-story \
+      --tracker-project "${TRACKER_PROJECT_ID}" \
+      --tracker-token "${TRACKER_API_TOKEN}" \
+      --github-repo "cloudfoundry/${LANGUAGE}-cnb" \
+      --github-token "${GITHUB_TOKEN}" 2>&1
+    echo "Release Story Created"
+  popd
+}
+
+main
