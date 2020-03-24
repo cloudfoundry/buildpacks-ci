5fb72c9c269214d25b9bbd6bf00a88e727719bd3
Mark DeLillo <mdelillo@pivotal.io>
2020-03-24 10:53:09 -0400

    Prune workers in concourse after recreating

--- tasks/recreate-workers/run.sh
+++ tasks/recreate-workers/run.sh
@@ -5,3 +5,6 @@ set -euo pipefail
 source bp-envs/scripts/login_director_public
 
 bosh -n -d concourse recreate --skip-drain worker
+
+fly -t buildpacks login -c https://buildpacks.ci.cf-app.com -u "${CI_USERNAME}" -p "${CI_PASSWORD}"
+fly -t buildpacks prune-worker --all-stalled
