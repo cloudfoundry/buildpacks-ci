e9d5f2b61f46026a7798e39c9910cbb3c8a8234d
Kenneth DuMez <kdumez@pivotal.io>
2020-04-21 17:58:31 -0500

    Add depwatcher support for non-latest versions of Ruby
    [#172380631]

--- dockerfiles/depwatcher/src/depwatcher/ruby.cr
+++ dockerfiles/depwatcher/src/depwatcher/ruby.cr
@@ -23,29 +23,26 @@ module Depwatcher
       end.sort_by { |i| SemanticVersion.new(i.ref) }
     end
 
-    def in(ref : String) : Release | Nil
-      releases().select do |r|
-        r.ref == ref
-      end.first?
-    end
+     def in(ref : String) : Release | Nil
+      result = Release.new("","","")
+      allReleases = [] of Release
+      response = client.get("https://cache.ruby-lang.org/pub/ruby/index.txt").body
 
-    private def releases : Array(Release)
-      response = client.get("https://www.ruby-lang.org/en/downloads/").body
-      doc = XML.parse_html(response)
-      lis = doc.xpath("//li/a[starts-with(text(),'Ruby ')]")
-      raise "Could not parse ruby website" unless lis.is_a?(XML::NodeSet)
+      response.each_line do |line|
+        releaseArray = [] of String
+        line.split { |s| releaseArray << s}
+        raise "Could not parse ruby website" unless !releaseArray.empty?
+        version = releaseArray[0].lchop("ruby-")
+        url = releaseArray[1]
+        sha = releaseArray[3]
+        newRelease = Release.new(version, url, sha)
 
-      lis = lis.reject { |item| item.to_s.includes? "preview" }
-      lis = lis.select { |item| item.to_s.match(/\d+/) }
-
-      lis.map do |a|
-        parent = a.parent
-        version = a.text.gsub(/^Ruby /, "")
-        url = a["href"]
-        m = /sha256: ([0-9a-f]+)/.match(parent.text) if parent.is_a?(XML::Node)
-        sha = m[1] if m
-        Release.new(version, url, sha) if url && sha
-      end.compact
+        if ref == version && url.ends_with?("tar.gz")
+          result = newRelease
+        end
+      end
+      raise ("No release with ref:" + ref + "found") unless !result.url.empty?
+      result
     end
   end
 end
