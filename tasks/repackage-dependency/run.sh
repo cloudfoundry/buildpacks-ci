2df623be03c7dbf4c5fd101370b527ff32eee742
Kenneth DuMez <kdumez@pivotal.io>
2020-05-14 11:56:08 -0400

    Fix file-path of repackage dependency again
    [#172532894]

--- tasks/repackage-dependency/run.sh
+++ tasks/repackage-dependency/run.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 name="$(jq -r .source.name source/data.json)"
 version="$(jq -r .version.ref source/data.json)"
 metadata_file_path="builds/binary-builds-new/$name/$version-cflinuxfs3.json"
-filename="builds/binary-builds-new/$name/$version-$STACK_ID.json"
+filename="binary-builds-new/$name/$version-$STACK_ID.json"
 cp "${metadata_file_path}" "${filename}"
 git config --global user.email "cf-buildpacks-eng@pivotal.io"
 git config --global user.name "CF Buildpacks Team CI Server"
