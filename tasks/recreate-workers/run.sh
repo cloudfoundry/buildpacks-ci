06b4384fd93db577a6a9762207faf55b7d8dec41
Mark DeLillo <mdelillo@pivotal.io>
2020-05-29 10:49:29 -0400

    Modify recreate-workers task to support private pipeline
    [#171956245]

--- tasks/recreate-workers/run.sh
+++ tasks/recreate-workers/run.sh
@@ -2,9 +2,20 @@
 
 set -euo pipefail
 
-source bp-envs/scripts/login_director_public
+if [[ "${CI_INSTANCE}" == "public" ]]; then
+  login_script=bp-envs/scripts/login_director_public
+  ci_url=https://buildpacks.ci.cf-app.com
+elif [[ "${CI_INSTANCE}" == "private" ]]; then
+  login_script=bp-envs/scripts/login_director_private
+  ci_url=https://buildpacks-private.ci.cf-app.com
+else
+  echo "CI_INSTANCE must be 'public' or 'private'"
+  exit 1
+fi
+
+source "${login_script}"
 
 bosh -n -d concourse recreate --skip-drain worker
 
-fly -t buildpacks login -c https://buildpacks.ci.cf-app.com -u "${CI_USERNAME}" -p "${CI_PASSWORD}"
+fly -t buildpacks login -c "${ci_url}" -u "${CI_USERNAME}" -p "${CI_PASSWORD}"
 fly -t buildpacks prune-worker --all-stalled
