2304e2e94a0db6e56d0c27cd016300f4d6ddaa00
Mark DeLillo <mdelillo@pivotal.io>
2020-06-11 10:16:38 -0400

    Set TAR_URL in bal windows specs
    [#173116599]

--- tasks/run-bal-specs/run.ps1
+++ tasks/run-bal-specs/run.ps1
@@ -2,6 +2,7 @@
 $env:GOPATH="C:\go-" + (-join ((48..57) + (97..122) | Get-Random -Count 6 | % {[char]$_}))
 trap { $host.SetShouldExit(1); cmd.exe /c rd /q /s $env:GOPATH }
 
+$env:TAR_URL="https://s3.amazonaws.com/bosh-windows-dependencies/tar-1589933969.exe"
 $env:CREDENTIAL_FILTER_WHITELIST="SystemDrive,SystemRoot,SERVICE_ID,NUMBER_OF_PROCESSORS,PROCESSOR_LEVEL,WINSW_SERVICE_ID,__PIPE_SERVICE_NAME,GOPATH,USERPROFILE"
 $env:PATH=$env:GOPATH + "/bin;C:/go/bin;C:/var/vcap/bosh/bin;" + $env:PATH
 $buildDir=$env:GOPATH + "/src/code.cloudfoundry.org/buildpackapplifecycle"
