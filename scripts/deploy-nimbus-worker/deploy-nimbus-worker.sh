0cb71d888ee6dc53f25054cce9092d1f9dce1a35
Sophie Wigmore <swigmore@vmware.com>
2021-01-14 18:07:01 -0500

    Add usage and a --username flag
    Signed-off-by: Frankie Gallina-Jones <frankieg@vmware.com>

--- scripts/deploy-nimbus-worker/deploy-nimbus-worker.sh
+++ scripts/deploy-nimbus-worker/deploy-nimbus-worker.sh
@@ -1,27 +1,63 @@
 #!/usr/bin/env bash
 
-# accept user's username as input (e.g. frankieg)
-user_name="frankieg"
+set -eu
+set -o pipefail
 
-# TODO: Parse VM username and password from OVF metadata file
-#
+function main {
+  local username
+
+# accept user's username as input (e.g. frankieg)
+  while [[ "${#}" != 0 ]]; do
+    case "${1}" in
+      --username|-u)
+        username="${2}"
+        shift 2
+        ;;
+
+      --help|-h)
+        shift 1
+        usage
+        exit 0
+        ;;
+
+      "")
+        # skip if the argument is empty
+        shift 1
+        ;;
+
+      *)
+        echo "unknown argument \"${1}\""
+        exit 1
+    esac
+  done
+
+  if [[ -z "${username:-}" ]]; then
+    printf "\nthe --username flag is required\n"
+    usage
+    exit 1
+  fi
+
+printf "\nLogging in as: %s\n" $username
+printf "    Deploying a nimbus ovf template. This takes a few minutes...\n If the script hangs for more than a few minutes check the logs for the following:\n"
+printf "    'User svc.buildpacks cannot provision additional resources due to: X is using 100 percent CPU provisioned quota'\n"
+printf "    If this is the case, the deployment is 'scheduled' and will automatically resume when resources become available\n\n"
 # deploy the buildpacks team OVF template with the svc.buildpacks service account
 date_time=$(date +%s)
 ssh_command="USER=svc.buildpacks nimbus deploy ovf --queue --wait-scheduler buildpacks-concourse-worker-$date_time  NIMBUS_OVF_BASE/svc.buildpacks/buildpacks-concourse-worker/buildpacks-concourse-worker-ovf.ovf"
-ssh ${user_name}@nimbus-gateway bash -c "'$ssh_command'"
+ssh ${username}@nimbus-gateway bash -c "'$ssh_command'"
 
 # get IP address from output
-echo "Resolving worker IP"
-ip_json=$(ssh ${user_name}@nimbus-gateway bash -c "'USER=svc.buildpacks nimbus-ctl --outputFormat=json ip svc.buildpacks-buildpacks-concourse-worker-$date_time | grep json_info'")
+echo "Resolving worker IP Address..."
+ip_json=$(ssh ${username}@nimbus-gateway bash -c "'USER=svc.buildpacks nimbus-ctl --outputFormat=json ip svc.buildpacks-buildpacks-concourse-worker-$date_time | grep json_info'")
 ip=$(echo ${ip_json} | jq -r '.json_info[]."svc.buildpacks-buildpacks-concourse-worker-'$date_time'"')
 
 
 # ssh onto the machine and add the worker private key
-echo "Accessing concourse credhub"
+echo "Accessing Concourse Credhub..."
 source ~/workspace/bp-envs/scripts/login_director_public
 # TODO Do something reasonable if that script isnt there
 
-echo "Setting up worker credentials"
+echo "Setting up credentials for worker..."
 credhub get -n /bosh-buildpacks-public/concourse/nimbus_worker_private_key -q > /tmp/deleteme-private-key
 scp /tmp/deleteme-private-key worker@${ip}:~/worker.pem
 rm /tmp/deleteme-private-key
@@ -32,12 +68,30 @@ scp /tmp/deleteme-atc-public-key worker@${ip}:~/host.pub
 rm /tmp/deleteme-atc-public-key
 
 # (maybe) modify the concourse.service file (e.g. to change the name)
-echo "Modifying concourse config"
+echo "Modifying the Concourse configuration file"
 cat concourse.service | sed -e "s/datetime/$date_time/" > /tmp/deleteme-concourse-config
 scp /tmp/deleteme-concourse-config worker@${ip}:~/concourse.service.new
 rm /tmp/deleteme-concourse-config
 
+echo "Starting the worker up on Concourse"
 # start the concourse process
 # check that the worker has come online
 ssh worker@${ip} bash -c "'sudo mv ~/concourse.service.new /etc/systemd/system/concourse.service && sudo systemctl daemon-reload && sudo systemctl restart concourse && sudo systemctl status concourse --no-pager --full'"
 
+sleep 5
+echo "Worker should be available:"
+fly -t buildpacks workers | grep ${date_time}
+}
+
+function usage() {
+  cat <<-USAGE
+deploy-nimbus-worker.sh --username <nimbus gateway username>
+
+Deploys a nimbus vsphere worker to the public concourse.
+
+OPTIONS
+  --help                                -h                            prints the command usage
+  --username <nimbus gateway username>  -u <nimbus gateway username>  your VMware LDAP username
+USAGE
+}
+main "${@:-}"
--- scripts/deploy-nimbus-worker/deploy-private-nimbus-worker.sh
+++ scripts/deploy-nimbus-worker/deploy-private-nimbus-worker.sh
@@ -1,27 +1,62 @@
 #!/usr/bin/env bash
+set -eu
+set -o pipefail
+
+function main {
+  local username
 
 # accept user's username as input (e.g. frankieg)
-user_name="frankieg"
+  while [[ "${#}" != 0 ]]; do
+    case "${1}" in
+      --username|-u)
+        username="${2}"
+        shift 2
+        ;;
 
-# TODO: Parse VM username and password from OVF metadata file
-#
+      --help|-h)
+        shift 1
+        usage
+        exit 0
+        ;;
+
+      "")
+        # skip if the argument is empty
+        shift 1
+        ;;
+
+      *)
+        echo "unknown argument \"${1}\""
+        exit 1
+    esac
+  done
+
+  if [[ -z "${username:-}" ]]; then
+    printf "\nthe --username flag is required\n"
+    usage
+    exit 1
+  fi
+
+printf "\nLogging in as: %s\n" $username
+printf "    Deploying a nimbus ovf template. This takes a few minutes...\n If the script hangs for more than a few minutes check the logs for the following:\n"
+printf "    'User svc.buildpacks cannot provision additional resources due to: X is using 100 percent CPU provisioned quota'\n"
+printf "    If this is the case, the deployment is 'scheduled' and will automatically resume when resources become available\n\n"
 # deploy the buildpacks team OVF template with the svc.buildpacks service account
 date_time=$(date +%s)
 ssh_command="USER=svc.buildpacks nimbus deploy ovf --queue --wait-scheduler buildpacks-private-concourse-worker-$date_time  NIMBUS_OVF_BASE/svc.buildpacks/buildpacks-concourse-worker/buildpacks-concourse-worker-ovf.ovf"
-ssh ${user_name}@nimbus-gateway bash -c "'$ssh_command'"
+ssh ${username}@nimbus-gateway bash -c "'$ssh_command'"
 
 # get IP address from output
-echo "Resolving worker IP"
-ip_json=$(ssh ${user_name}@nimbus-gateway bash -c "'USER=svc.buildpacks nimbus-ctl --outputFormat=json ip svc.buildpacks-buildpacks-private-concourse-worker-$date_time | grep json_info'")
+echo "Resolving worker IP Address..."
+ip_json=$(ssh ${username}@nimbus-gateway bash -c "'USER=svc.buildpacks nimbus-ctl --outputFormat=json ip svc.buildpacks-buildpacks-private-concourse-worker-$date_time | grep json_info'")
 ip=$(echo ${ip_json} | jq -r '.json_info[]."svc.buildpacks-buildpacks-private-concourse-worker-'$date_time'"')
 
 
 # ssh onto the machine and add the worker private key
-echo "Accessing private concourse credhub"
+echo "Accessing Concourse Credhub..."
 source ~/workspace/bp-envs/scripts/login_director_private
 # TODO Do something reasonable if that script isnt there
 
-echo "Setting up worker credentials"
+echo "Setting up credentials for worker..."
 credhub get -n /bosh-buildpacks-private/concourse/nimbus_worker_private_key -q > /tmp/deleteme-private-key
 scp /tmp/deleteme-private-key worker@${ip}:~/worker.pem
 rm /tmp/deleteme-private-key
@@ -32,11 +67,12 @@ scp /tmp/deleteme-atc-public-key worker@${ip}:~/host.pub
 rm /tmp/deleteme-atc-public-key
 
 # (maybe) modify the concourse.service file (e.g. to change the name)
-echo "Modifying concourse config"
+echo "Modifying the Concourse configuration file"
 cat concourse.service.private | sed -e "s/datetime/$date_time/" > /tmp/deleteme-concourse-config
 scp /tmp/deleteme-concourse-config worker@${ip}:~/concourse.service.new
 rm /tmp/deleteme-concourse-config
 
+echo "Starting the worker up on Concourse"
 # start the concourse process
 # check that the worker has come online
 ssh worker@${ip} bash -c "'sudo mv ~/concourse.service.new /etc/systemd/system/concourse.service && sudo systemctl daemon-reload && sudo systemctl restart concourse && sudo systemctl status concourse --no-pager --full'"
@@ -44,3 +80,18 @@ ssh worker@${ip} bash -c "'sudo mv ~/concourse.service.new /etc/systemd/system/c
 sleep 5
 echo "Worker should be available:"
 fly -t buildpacks-private workers | grep ${date_time}
+}
+
+function usage() {
+  cat <<-USAGE
+deploy-private-nimbus-worker.sh --username <nimbus gateway username>
+
+Deploys a nimbus vsphere worker to the private concourse.
+
+OPTIONS
+  --help                                -h                            prints the command usage
+  --username <nimbus gateway username>  -u <nimbus gateway username>  your VMware LDAP username
+USAGE
+}
+
+main "${@:-}"
