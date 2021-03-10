c47409bf0d98f41b3393f7720e8b0b61e523f5e0
Marty Spiewak <mspiewak@pivotal.io>
2021-03-10 14:06:02 -0500

    Revert "Signoff commits updating CNB dependencies"
    This reverts commit bb45008d13390e0844927f0765cdd94738b189c5.

--- tasks/cnb/update-cnb-dependency/commit_artifacts.go
+++ tasks/cnb/update-cnb-dependency/commit_artifacts.go
@@ -16,7 +16,7 @@ func CommitArtifacts(commitMessage, outputDir, buildpackTOMLOutputPath string) e
 		return errors.Wrapf(err, "failed to add artifacts: %s", string(output))
 	}
 
-	output, err = exec.Command("git", "-C", outputDir, "commit", "--signoff", "-m", commitMessage).CombinedOutput()
+	output, err = exec.Command("git", "-C", outputDir, "commit", "-m", commitMessage).CombinedOutput()
 	if err != nil {
 		return errors.Wrapf(err, "failed to commit artifacts: %s", string(output))
 	}
