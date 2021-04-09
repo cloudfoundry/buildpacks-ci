b1946407766941b3540125e9640fe0e11a818287
Sophie Wigmore <sophiemwigmore@gmail.com>
2021-04-09 15:44:05 -0400

    Generate V2B release issues on Github (#74)
    * WIP task to create v2b release issues
    
    * functional generate release issues
    
    * Add commit log, and add to project

--- tasks/generate-buildpack-release-github-issue/run.rb
+++ tasks/generate-buildpack-release-github-issue/run.rb
@@ -2,9 +2,6 @@
 require 'octokit'
 require_relative '../../lib/release-github-issue-generator'
 
-## Required
-github_token = ENV.fetch('GITHUB_ACCESS_TOKEN') or raise 'Must supply env GITHUB_ACCESS_TOKEN'
-
 buildpack_name = ENV.fetch('BUILDPACK_NAME')
 previous_buildpack_version = `git -C buildpack-develop tag`.split("\n").map{ |i| i.strip.delete('v') }
   .select { |i| i =~ /\d+\.\d+\.\d+/ }.map { |i| Gem::Version.new(i) }.sort.last.to_s
@@ -12,16 +9,8 @@ previous_buildpack_version = `git -C buildpack-develop tag`.split("\n").map{ |i|
 old_manifest = `git -C buildpack-develop show v#{previous_buildpack_version}:manifest.yml`
 new_manifest = File.read('buildpack-develop/manifest.yml')
 
-client = Octokit::Client.new :access_token => github_token
+client = Octokit::Client.new :access_token => ENV.fetch('GITHUB_ACCESS_TOKEN')
 
-release_github_issue_creator = ReleaseGithubIssueGenerator.new(
-  client: client,
-  buildpack_name: buildpack_name,
-  previous_buildpack_version: previous_buildpack_version,
-  old_manifest:               old_manifest,
-  new_manifest:               new_manifest
-
-)
 puts "Running ReleaseGithubIssueGenerator for #{buildpack_name}"
 
-release_github_issue_creator.run!
+ReleaseGithubIssueGenerator.new(client).run(buildpack_name, previous_buildpack_version, old_manifest, new_manifest)
