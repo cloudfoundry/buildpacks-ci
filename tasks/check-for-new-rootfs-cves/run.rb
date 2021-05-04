447db4f70677dafb8f74d2d134408bc3e2f65fd1
Timothy Hitchener <thitchener@vmware.com>
2021-05-04 21:05:04 +0000

    Remove cflinuxfs2 & tiny stack logic
    Signed-off-by: Marty Spiewak <mspiewak@pivotal.io>

--- tasks/check-for-new-rootfs-cves/run.rb
+++ tasks/check-for-new-rootfs-cves/run.rb
@@ -5,9 +5,6 @@ stack = ENV['STACK']
 
 buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 stacks_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', stack.gsub(/m$/, '')))
-if stack == 'tiny'
-  stacks_dir = File.expand_path(File.join(buildpacks_ci_dir,  '..', 'tiny', 'tiny', 'dockerfile', 'run'))
-end
 cves_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'output-new-cves', 'new-cve-notifications'))
 
 require "#{buildpacks_ci_dir}/lib/rootfs-cve-notifier"
@@ -15,12 +12,8 @@ require "#{buildpacks_ci_dir}/lib/rootfs-cve-notifier"
 cve_notifier = RootFSCVENotifier.new(cves_dir, stacks_dir)
 
 case stack
-when 'cflinuxfs2'
-  cve_notifier.run!(stack, 'Ubuntu 14.04', 'ubuntu14.04', [])
 when 'cflinuxfs3'
   cve_notifier.run!(stack, 'Ubuntu 18.04', 'ubuntu18.04', [])
-when 'tiny'
-  cve_notifier.run!(stack, 'Ubuntu 18.04', 'ubuntu18.04-tiny', [])
 else
   raise "Unsupported stack: #{stack}"
 end
