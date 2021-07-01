f9ec3b05663257414214d43ecf7553c86d927549
Ryan Moran <rmoran@vmware.com>
2021-07-01 08:57:08 -0700

    Only source if the file exists

--- tasks/run-bp-brats/run.sh
+++ tasks/run-bp-brats/run.sh
@@ -11,9 +11,12 @@ pushd buildpack > /dev/null
   export PATH=$GOBIN:$PATH
 popd > /dev/null
 
-# shellcheck disable=SC1091
-source "${PWD}/buildpack/scripts/.util/tools.sh"
-util::tools::cf::install --directory "${PWD}/buildpack/.bin"
+if [[ -e "${PWD}/buildpack/scripts/.util/tools.sh" ]]; then
+  # shellcheck disable=SC1091
+  source "${PWD}/buildpack/scripts/.util/tools.sh"
+  util::tools::cf::install --directory "${PWD}/buildpack/.bin"
+fi
+
 "./cf-space/login"
 
 pushd buildpack > /dev/null
