6966a87e8c3a5ded0050dcf30743a290e8f897d9
Ryan Moran <rmoran@vmware.com>
2021-07-01 08:29:50 -0700

    Just use the ci image cf

--- tasks/run-bp-brats/run.sh
+++ tasks/run-bp-brats/run.sh
@@ -11,12 +11,6 @@ pushd buildpack > /dev/null
   export PATH=$GOBIN:$PATH
 popd > /dev/null
 
-if [[ -e "${PWD}/buildpack/scripts/.util/tools.sh" ]]; then
-  # shellcheck disable=SC1091
-  source "${PWD}/buildpack/scripts/.util/tools.sh"
-  util::tools::cf::install --directory "${PWD}/buildpack/.bin"
-fi
-
 "./cf-space/login"
 
 pushd buildpack > /dev/null
