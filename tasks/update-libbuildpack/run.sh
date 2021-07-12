b1bbaaff4695734176b40a8ae0bc9297d8efc027
Timothy Hitchener <thitchener@vmware.com>
2021-07-12 19:56:02 +0000

    update-libbuildpack:go 1.16 compatible go-install
    Co-authored-by: Arjun Sreedharan <asreedharan@vmware.com>

--- tasks/update-libbuildpack/run.sh
+++ tasks/update-libbuildpack/run.sh
@@ -20,7 +20,9 @@ pushd buildpack
   go get -u github.com/cloudfoundry/libbuildpack
   go get github.com/golang/mock/gomock
   go get -u github.com/onsi/ginkgo/ginkgo
-  go install github.com/golang/mock/mockgen
+  pushd /tmp
+    go install github.com/golang/mock/mockgen@latest
+  popd
   go mod tidy
   if [[ "$LANGUAGE" != "php" || "$SHIM" == "true" ]]; then
     go mod vendor
