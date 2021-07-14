17f42dfc6cc4af6c09288a11fe6a69aae0c1130e
Arjun Sreedharan <asreedharan@vmware.com>
2021-07-14 15:38:09 +0000

    CA-APM move src from bintray -> broadcom artifactory
    See https://github.com/cloudfoundry/php-buildpack/issues/517#issuecomment-878557653

--- dockerfiles/depwatcher/src/depwatcher/ca_apm_agent.cr
+++ dockerfiles/depwatcher/src/depwatcher/ca_apm_agent.cr
@@ -18,7 +18,7 @@ module Depwatcher
     end
 
     def check : Array(Internal)
-      response = client.get("https://ca.bintray.com/apm-agents/").body
+      response = client.get("https://packages.broadcom.com/artifactory/apm-agents/").body
       doc = XML.parse_html(response)
       links = doc.xpath("//a[@href]")
       raise "Could not parse apache httpd website" unless links.is_a?(XML::NodeSet)
@@ -32,7 +32,7 @@ module Depwatcher
     end
 
     def in(ref : String) : Release
-      url = "https://ca.bintray.com/apm-agents/CA-APM-PHPAgent-#{ref}_linux.tar.gz"
+      url = "https://packages.broadcom.com/artifactory/apm-agents/CA-APM-PHPAgent-#{ref}_linux.tar.gz"
       Release.new(ref, url, get_sha256(url))
     end
   end
