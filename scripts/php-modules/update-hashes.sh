2bbd8650438e9caba02b3e9d73043adfc7181605
Andrew Meyer <meyera@vmware.com>
2021-10-13 22:22:04 -0700

    Update php module helper script notes

--- scripts/php-modules/update-hashes.sh
+++ scripts/php-modules/update-hashes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This script will systematically recompute hashes for specified php modules
+# This script will systematically recompute hashes for specified php modules.
 #
 # To use this script, simply run it from the top-level of this repo after updating
 # 'tasks/build-binary-new/php7-base-extensions.yml' and
@@ -8,6 +8,10 @@
 #
 # 1. Update the 'version' field for each module being bumped
 # 2. Clear the 'md5' field to signal that it should be recomputed
+#
+# NOTE: This script assumes that you have cloned
+# https://github.com/cloudfoundry/binary-builder into a 'binary-builder'
+# directory next to this repo. It also assumes you have bundler installed.
 
 set -euo pipefail
 
