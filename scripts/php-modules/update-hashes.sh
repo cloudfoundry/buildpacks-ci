858a88e46272ad132cdd308978d13827f3876615
Andrew Meyer <meyera@vmware.com>
2021-10-13 22:25:04 -0700

    Update php module helper script instructions

--- scripts/php-modules/update-hashes.sh
+++ scripts/php-modules/update-hashes.sh
@@ -4,9 +4,10 @@
 #
 # To use this script, simply run it from the top-level of this repo after updating
 # 'tasks/build-binary-new/php7-base-extensions.yml' and
-# 'tasks/build-binary-new/php7-base-extensions.yml' as follows:
+# 'tasks/build-binary-new/php7-base-extensions.yml' as follows, for each module
+# being bumped:
 #
-# 1. Update the 'version' field for each module being bumped
+# 1. Update the 'version' field to the desired version
 # 2. Clear the 'md5' field to signal that it should be recomputed
 #
 # NOTE: This script assumes that you have cloned
