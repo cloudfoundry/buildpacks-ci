6794a92703df5dfa2dda244cb39e9896806f531c
Brayan Henao <bhenao@vmware.com>
2021-11-16 16:35:23 -0500

    Update deprecation stories generation from github to tracker

---
+++ tasks/create-deprecation-stories/deprecation-notifier.rb
@@ -0,0 +1,62 @@
+# encoding: utf-8
+require 'tracker_api'
+
+class DeprecationNotifier
+  attr_reader :buildpack_name, :manifest, :date, :tracker_requester_id, :buildpack_project, :before_story_id
+
+  def initialize(buildpack_name:, manifest:, date:, tracker_project_id:, tracker_requester_id:, tracker_api_token:, before_story_id:)
+    @buildpack_name = buildpack_name
+    @manifest = manifest
+    @date = date
+    @tracker_requester_id = tracker_requester_id
+    @before_story_id = before_story_id
+    @buildpack_project = TrackerApi::Client.new(token: tracker_api_token).project(tracker_project_id)
+  end
+
+  def run
+    deprecation_dates = find_dates(manifest, date)
+
+    if deprecation_dates.size == 0
+      puts 'No deprecated dependencies'
+      exit 0
+    end
+
+    deprecation_dates.each do |d|
+      version_line = d['version_line']
+      story_name = "**Dependency Deprecation** #{buildpack_name}-buildpack: #{d['name']} #{version_line}"
+      if story_exists(story_name)
+        puts 'Deprecation Story already exists'
+        next
+      end
+
+      story_description = "Deprecation Date: #{d['date']}\nLink: #{d['link']}"
+      labels = ['deprecation', "#{buildpack_name}-buildpack", "#{d['name']}-dep"]
+
+      story = buildpack_project.create_story(
+          name: story_name,
+          description: story_description,
+          estimate: 1,
+          labels: labels,
+          requested_by_id: tracker_requester_id,
+          before_id: before_story_id
+      )
+      story.save
+    end
+  end
+
+  def story_exists(name)
+    !!buildpack_project
+            .stories(filter: "label:#{buildpack_name}-buildpack AND name:#{name}", limit: 1000).first
+  end
+
+end
+
+def find_dates(manifest, today)
+  if !manifest['dependency_deprecation_dates']
+    return []
+  end
+
+  return manifest['dependency_deprecation_dates']
+             .select {|d| d['date'] <= today + 45}
+             .map {|d| d['date'] = d['date'].to_s; d}
+end
---
+++ tasks/create-deprecation-stories/run.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+#
+
+require 'yaml'
+require 'date'
+require_relative 'deprecation-notifier'
+
+buildpack_name = ENV.fetch('BUILDPACK_NAME')
+manifest = YAML.load_file(File.join('buildpack', 'manifest.yml'))
+date = Date.today
+tracker_project_id = ENV.fetch('TRACKER_PROJECT_ID')
+tracker_requester_id = ENV.fetch('TRACKER_REQUESTER_ID').to_i
+before_story_id = ENV.fetch('BEFORE_STORY_ID').to_i
+tracker_api_token = ENV.fetch('TRACKER_API_TOKEN')
+
+notifier = DeprecationNotifier.new(
+    buildpack_name: buildpack_name,
+    manifest: manifest,
+    date: date,
+    tracker_project_id: tracker_project_id,
+    tracker_requester_id: tracker_requester_id,
+    tracker_api_token: tracker_api_token,
+    before_story_id: before_story_id
+)
+
+notifier.run
