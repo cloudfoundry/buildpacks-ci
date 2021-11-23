5e70a299e71809fb39df6ba1353480900ee8b354
Arjun Sreedharan <asreedharan@vmware.com>
2021-11-23 19:23:13 +0000

    tasks/remove-gcp-parent-dns-record: Skip when no record-sets

--- tasks/remove-gcp-parent-dns-record/run.sh
+++ tasks/remove-gcp-parent-dns-record/run.sh
@@ -17,6 +17,11 @@ gcloud config set project cf-buildpacks
 # get the NS
 NAMESERVERS=$(gcloud dns record-sets list -z buildpacks | grep "$ENV_NAME" | awk '{ print $4 }'| tr , ' ')
 
+if [ -z "$NAMESERVERS" ]; then
+  echo "There are no record sets matching ${ENV_NAME}"
+  echo "Skipping..."
+fi
+
 # remove the parent zone (buildpacks zone) subdomain NS records
 gcloud dns record-sets transaction start --zone=buildpacks
   # shellcheck disable=SC2086
