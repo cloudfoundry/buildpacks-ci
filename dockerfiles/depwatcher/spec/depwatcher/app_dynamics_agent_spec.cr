ba2973b778460b64a4c3c1fa36c261fa1d02637b
Andrew Meyer <meyera@vmware.com>
2021-12-11 15:13:50 -0800

    Fix appdynamics depwatcher code and tests
    Was missing Entry filetype field
    Tests were checking for wrong URL

--- dockerfiles/depwatcher/spec/depwatcher/app_dynamics_agent_spec.cr
+++ dockerfiles/depwatcher/spec/depwatcher/app_dynamics_agent_spec.cr
@@ -6,7 +6,7 @@ Spec2.describe Depwatcher::AppDynamicsAgent do
   let(client) { HTTPClientMock.new }
   subject { described_class.new.tap { |s| s.client = client } }
   before do
-    client.stub_get("https://download.appdynamics.com/download/downloadfile/?apm=php&format=json", nil, HTTP::Client::Response.new(200, File.read(__DIR__+"/../fixtures/appd_agent.json")))
+    client.stub_get("https://download.appdynamics.com/download/downloadfilelatest?format=json", nil, HTTP::Client::Response.new(200, File.read(__DIR__+"/../fixtures/appd_agent.json")))
   end
 
   describe "#check" do
