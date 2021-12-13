d0638bc34a02ebaa412817ee2c7ca92de0509621
Andrew Meyer <meyera@vmware.com>
2021-12-13 10:02:33 -0800

    Add PHP8.0 to php module reminders

--- tasks/check-for-latest-php-module-versions/run.rb
+++ tasks/check-for-latest-php-module-versions/run.rb
@@ -6,10 +6,12 @@ require_relative '../../lib/tracker-client'
 require_relative '../../tasks/build-binary-new/merge-extensions'
 
 base_extensions = BaseExtensions.new('buildpacks-ci/tasks/build-binary-new/php7-base-extensions.yml')
+base_extensions_8 = BaseExtensions.new('buildpacks-ci/tasks/build-binary-new/php8-base-extensions.yml')
 php74_extensions = base_extensions.patch('buildpacks-ci/tasks/build-binary-new/php74-extensions-patch.yml')
 
 data = {
   'PHP7.4' => php74_extensions.base_yml,
+  'PHP8.0' => base_extensions_8.base_yml,
 }
 
 
@@ -30,15 +32,15 @@ Run the [helper scripts](https://github.com/cloudfoundry/buildpacks-ci/tree/mast
 These scripts bump as many modules as they can, but the results should still be checked manually before committing. Some must be bumped manually, and it's still important to check if a module supports a new version of PHP which it didn't previously support (in which case it should be added manually).
 DESCRIPTION
 
-description += "\n\n" + %w(Name Latest PHP7.4).join(' | ') + "\n"
-description += '--- | --- | --- | --- ' + "\n"
+description += "\n\n" + %w(Name Latest PHP7.4 PHP8.0).join(' | ') + "\n"
+description += '--- | --- | --- | --- | ---' + "\n"
 extensions.keys.sort_by(&:name).each do |key|
   name = "#{key.name} (#{key.klass.gsub(/Recipe$/,'')})"
   url = url_for_type(key.name, key.klass)
   latest = current_pecl_version(key.name) if key.klass =~ /PECL/i
   latest = current_github_version(url) if url =~ %r{^https://github.com}
   data = [url ? "[#{name}](#{url})" : name, latest]
-  %w(PHP7.4).each do |v|
+  %w(PHP7.4 PHP8.0).each do |v|
     val = extensions[key][v]
     val = "**#{val}**" if val && val != latest
     data << val
