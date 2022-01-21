84c8072d02d6bdff90bf9a78b18f992d560f3489
Arjun Sreedharan <asreedharan@vmware.com>
2022-01-21 16:45:34 +0000

    task/check-latest-php-mod: include php 8.1

--- tasks/check-for-latest-php-module-versions/run.rb
+++ tasks/check-for-latest-php-module-versions/run.rb
@@ -5,13 +5,17 @@ require_relative('common.rb')
 require_relative '../../lib/tracker-client'
 require_relative '../../tasks/build-binary-new/merge-extensions'
 
-base_extensions = BaseExtensions.new('buildpacks-ci/tasks/build-binary-new/php7-base-extensions.yml')
+base_extensions_7 = BaseExtensions.new('buildpacks-ci/tasks/build-binary-new/php7-base-extensions.yml')
+php74_extensions = base_extensions_7.patch('buildpacks-ci/tasks/build-binary-new/php74-extensions-patch.yml')
+
 base_extensions_8 = BaseExtensions.new('buildpacks-ci/tasks/build-binary-new/php8-base-extensions.yml')
-php74_extensions = base_extensions.patch('buildpacks-ci/tasks/build-binary-new/php74-extensions-patch.yml')
+php80_extensions = base_extensions_8
+php81_extensions = base_extensions_8.patch('buildpacks-ci/tasks/build-binary-new/php81-extensions-patch.yml')
 
 data = {
   'PHP7.4' => php74_extensions.base_yml,
-  'PHP8.0' => base_extensions_8.base_yml,
+  'PHP8.0' => php80_extensions.base_yml,
+  'PHP8.1' => php81_extensions.base_yml,
 }
 
 
@@ -32,15 +36,15 @@ Run the [helper scripts](https://github.com/cloudfoundry/buildpacks-ci/tree/mast
 These scripts bump as many modules as they can, but the results should still be checked manually before committing. Some must be bumped manually, and it's still important to check if a module supports a new version of PHP which it didn't previously support (in which case it should be added manually).
 DESCRIPTION
 
-description += "\n\n" + %w(Name Latest PHP7.4 PHP8.0).join(' | ') + "\n"
-description += '--- | --- | --- | --- | ---' + "\n"
+description += "\n\n" + %w(Name Latest PHP7.4 PHP8.0 PHP8.1).join(' | ') + "\n"
+description += '--- | --- | --- | --- | --- | ---' + "\n"
 extensions.keys.sort_by(&:name).each do |key|
   name = "#{key.name} (#{key.klass.gsub(/Recipe$/,'')})"
   url = url_for_type(key.name, key.klass)
   latest = current_pecl_version(key.name) if key.klass =~ /PECL/i
   latest = current_github_version(url) if url =~ %r{^https://github.com}
   data = [url ? "[#{name}](#{url})" : name, latest]
-  %w(PHP7.4 PHP8.0).each do |v|
+  %w(PHP7.4 PHP8.0 PHP8.1).each do |v|
     val = extensions[key][v]
     val = "**#{val}**" if val && val != latest
     data << val
@@ -53,8 +57,8 @@ description += <<-DESCRIPTION
 If you're updating cassandra modules (including datastax/cpp-driver) please do so in individual commits, then rebuild appropriate php versions, so integration tests can run in CI with only cassandra changes.
 This will help isolate the php cassandra module change(s) if the changes cause problems.
 
-If the release is compatible with all versions update the php7-base-extensions.yml file. Otherwise, 
-update the respective patch file. 
+If the release is compatible with all versions update the php[7|8]-base-extensions.yml file. Otherwise,
+update the respective patch file.
 
 DESCRIPTION
 
