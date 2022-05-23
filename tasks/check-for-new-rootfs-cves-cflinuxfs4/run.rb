07834ca45b6f7951706b30da735b8893339edc2f
Brayan Henao <bhenao@vmware.com>
2022-05-23 14:58:30 -0500

    Update tasks to support cflinuxfs4 cves notifications (#132)

--- tasks/check-for-new-rootfs-cves-cflinuxfs4/run.rb
+++ tasks/check-for-new-rootfs-cves-cflinuxfs4/run.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-stack = 'cflinuxfs3'
+stack = 'cflinuxfs4'
 
 buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 stacks_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', stack.gsub(/m$/, '')))
@@ -11,4 +11,4 @@ require "#{buildpacks_ci_dir}/lib/rootfs-cve-notifier"
 
 cve_notifier = RootFSCVENotifier.new(cves_dir, stacks_dir)
 
-cve_notifier.run!(stack, 'Ubuntu 18.04', 'ubuntu18.04', [])
+cve_notifier.run!(stack, 'Ubuntu 22.04', 'ubuntu22.04', [])
