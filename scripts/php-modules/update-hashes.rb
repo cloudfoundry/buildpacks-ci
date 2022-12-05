c863a54f7416e2ecc3d13ba00cac682490581e00
Brayan Henao <bhenao@vmware.com>
2022-12-05 18:49:34 -0500

    Update php modules and scripts

--- scripts/php-modules/update-hashes.rb
+++ scripts/php-modules/update-hashes.rb
@@ -6,36 +6,36 @@ require_relative('../../../binary-builder/recipe/php_common_recipes.rb')
 require_relative('../../../binary-builder/recipe/php_recipe.rb')
 
 def cache_key(name, version, klass)
-    [name, version, klass]
+  [name, version, klass]
 end
 
 def get_hash(name, version, klass, cache)
-    key = cache_key(name, version, klass)
+  key = cache_key(name, version, klass)
 
-    if !cache[key]
-        recipe = Object::const_get(klass).new(name, version)
-        file = URI.open(recipe.url)
-        cache[key] = Digest::MD5.hexdigest(file.read)
-    end
+  if !cache[key]
+    recipe = Object::const_get(klass).new(name, version)
+    file = URI.open(recipe.url)
+    cache[key] = Digest::MD5.hexdigest(file.read)
+  end
 
-    cache[key]
+  cache[key]
 end
 
 def update_hash(mod, cache)
-    name = mod['name']
-    version = mod['version']
-    klass = mod['klass']
-    md5 = mod['md5']
+  name = mod['name']
+  version = mod['version']
+  klass = mod['klass']
+  md5 = mod['md5']
 
-    if !version || version == 'nil'
-        return
-    end
+  if !version || version == 'nil'
+    return
+  end
 
-    if !md5 || md5 == 'nil'
-        puts "    > Computing hash for #{name} @ #{version}..."
-        mod['md5'] = get_hash(name, version, klass, cache)
-        puts "      Updated #{name} md5 to #{mod['md5']}"
-    end
+  if !md5 || md5 == 'nil'
+    puts "    > Computing hash for #{name} @ #{version}..."
+    mod['md5'] = get_hash(name, version, klass, cache)
+    puts "      Updated #{name} md5 to #{mod['md5']}"
+  end
 end
 
 puts 'Updating module hashes...'
