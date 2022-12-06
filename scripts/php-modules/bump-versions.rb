8732f258ee7d1ce3dfb2afda48c87f0948656d00
Brayan Henao <bhenao@vmware.com>
2022-12-06 15:27:35 -0500

    Deprecate PHP 7.4.x (#197)
    * Deprecate PHP 7.4.x
    
    * Update php modules and scripts
    
    * Add latest to markdown table
    
    * Add note to extension update policy

--- scripts/php-modules/bump-versions.rb
+++ scripts/php-modules/bump-versions.rb
@@ -2,46 +2,46 @@ require_relative('update.rb')
 require_relative('../../tasks/check-for-latest-php-module-versions/common.rb')
 
 def cache_key(name, klass)
-    [name, klass]
+  [name, klass]
 end
 
 def get_latest(name, klass, url, cache)
-    key = cache_key(name, klass)
+  key = cache_key(name, klass)
 
-    if !cache[key]
-        latest = current_pecl_version(name) if klass =~ /PECL/i
-        latest = current_github_version(url, ENV['GITHUB_TOKEN']) if url =~ %r{^https://github.com}
-        cache[key] = latest
-    end
+  if !cache[key]
+    latest = current_pecl_version(name) if klass =~ /PECL/i
+    latest = current_github_version(url, ENV['GITHUB_TOKEN']) if url =~ %r{^https://github.com}
+    cache[key] = latest
+  end
 
-    cache[key]
+  cache[key]
 end
 
 def bump_version(mod, cache)
-    name = mod['name']
-    version = mod['version']
-    klass = mod['klass']
+  name = mod['name']
+  version = mod['version']
+  klass = mod['klass']
 
-    if !version || version == 'nil'
-        return
-    end
+  if !version || version == 'nil'
+    return
+  end
 
-    puts "    > Getting latest version of #{name} (#{klass})..."
-    url = url_for_type(name, klass)
-    latest = get_latest(name, klass, url, cache)
+  puts "    > Getting latest version of #{name} (#{klass})..."
+  url = url_for_type(name, klass)
+  latest = get_latest(name, klass, url, cache)
 
-    if !latest || latest == 'Unknown'
-        puts "      WARNING! Could not determine latest version of #{name}. Manual check required (URL: #{url || '<none>'})."
-        return
-    end
+  if !latest || latest == 'Unknown'
+    puts "      WARNING! Could not determine latest version of #{name}. Manual check required (URL: #{url || '<none>'})."
+    return
+  end
 
-    if version != latest
-        puts "      Bumped #{name}: #{version} -> #{latest}"
-        mod['version'] = latest
-        mod['md5'] = nil
-    else
-        puts "      No bump required (current version: #{version})"
-    end
+  if version != latest
+    puts "      Bumped #{name}: #{version} -> #{latest}"
+    mod['version'] = latest
+    mod['md5'] = nil
+  else
+    puts "      No bump required (current version: #{version})"
+  end
 end
 
 puts 'Bumping module versions...'
--- scripts/php-modules/update.rb
+++ scripts/php-modules/update.rb
@@ -1,29 +1,24 @@
 require 'yaml'
 
 def update_modules(&f)
-    cache = {}
-    [
-        "php7-base-extensions.yml",
-        "php74-extensions-patch.yml",
-        "php8-base-extensions.yml",
-        "php81-extensions-patch.yml",
-    ].each do |ext_file|
-        path = File.expand_path("../../tasks/build-binary-new/#{ext_file}")
+  cache = {}
+  %w[php8-base-extensions.yml php81-extensions-patch.yml].each do |ext_file|
+    path = File.expand_path("../../tasks/build-binary-new/#{ext_file}")
 
-        puts "==> Processing: #{path}"
-        data = YAML.load_file(path)
+    puts "==> Processing: #{path}"
+    data = YAML.load_file(path)
 
-        extensions_ref = ['extensions']
-        native_modules_ref = ['native_modules']
+    extensions_ref = ['extensions']
+    native_modules_ref = ['native_modules']
 
-        if File.basename(path).include? 'patch'
-            extensions_ref = extensions_ref.append('additions')
-            native_modules_ref = native_modules_ref.append('additions')
-        end
-
-        (data.dig(*extensions_ref) || []).each { |mod| f.call(mod, cache) }
-        (data.dig(*native_modules_ref) || []).each { |mod| f.call(mod, cache) }
-
-        File.write(path, data.to_yaml)
+    if File.basename(path).include? 'patch'
+      extensions_ref = extensions_ref.append('additions')
+      native_modules_ref = native_modules_ref.append('additions')
     end
+
+    (data.dig(*extensions_ref) || []).each { |mod| f.call(mod, cache) }
+    (data.dig(*native_modules_ref) || []).each { |mod| f.call(mod, cache) }
+
+    File.write(path, data.to_yaml)
+  end
 end
