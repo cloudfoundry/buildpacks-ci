9298f75933112cf923f420ef390a7cfeffecd28c
Arjun Sreedharan <asreedharan@vmware.com>
2023-04-18 16:36:17 +0000

    clean up lookup of removed lpass entries
    - Clean up references to lpass entries cleaned up in the following issues:
      - [Private link](https://github.com/pivotal-cf/tanzu-buildpacks/issues/197)
      - [Private link](https://github.com/pivotal-cf/tanzu-buildpacks/issues/199)
      - [Private link](https://github.com/pivotal-cf/tanzu-buildpacks/issues/207)
    - Remove script originally used to get private keys from lpass and add
      to repos as deploy keys. We use creds from credhub now.
    - Also spec changes missed out in #258

--- lib/buildpacks-ci-configuration.rb
+++ lib/buildpacks-ci-configuration.rb
@@ -7,24 +7,12 @@ class BuildpacksCIConfiguration
     ENV.fetch('LPASS_REPOS_PRIVATE_KEYS_FILE', 'Shared-CF\ Buildpacks/buildpack-repos-private-keys.yml')
   end
 
-  def cnb_repos_private_keys_filename
-    ENV.fetch('LPASS_CNB_REPOS_PRIVATE_KEYS_FILE', 'Shared-CF\ Buildpacks/buildpack-cnb-repos-private-keys.yml')
-  end
-
   def bosh_release_private_keys_filename
     ENV.fetch('LPASS_BOSH_RELEASE_PRIVATE_KEYS_FILE', 'Shared-CF\ Buildpacks/buildpack-bosh-release-repos-private-keys.yml')
   end
 
-  def bosh_release_private_keys_filename_lts
-    ENV.fetch('LPASS_BOSH_RELEASE_PRIVATE_KEYS_FILE_LTS', 'Shared-CF\ Buildpacks/buildpack-bosh-release-repos-private-keys-lts.yml')
-  end
-
   def git_repos_private_keys_filename
     ENV.fetch('LPASS_GIT_REPOS_PRIVATE_KEYS_FILE', 'Shared-CF\ Buildpacks/git-repos-private-keys.yml')
-  end
-
-  def git_repos_private_keys_two_filename
-    ENV.fetch('LPASS_GIT_REPOS_PRIVATE_KEYS_TWO_FILE', 'Shared-CF\ Buildpacks/git-repos-private-keys-two.yml')
   end
 
   def git_repos_private_keys_three_filename
