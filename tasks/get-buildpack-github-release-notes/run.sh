6121fe1883a9c4b2ca616a9288c22561495e81eb
Tim Hitchener <thitchener@vmware.com>
2023-10-26 20:37:35 +0000

    Polish changelog creation

--- tasks/get-buildpack-github-release-notes/run.sh
+++ tasks/get-buildpack-github-release-notes/run.sh
@@ -19,16 +19,32 @@ if [ -z "${GITHUB_ACCESS_TOKEN}" ]; then
   exit 1
 fi
 
+new_version=$(cat version/version)
+if [ -z "${new_version}" ]; then
+  echo "task error: can't read version from version/version" >&2
+  exit 1
+fi
+
 pushd buildpack
 
-release_body=''
+response=$(
+  curl "https://api.github.com/repos/${BUILDPACK_REPO}/releases/tags/v${new_version}" \
+        --silent \
+        --location \
+        --header "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
+)
+
+changelog=$( echo "$response" | jq -r '.body' | sed '/^Packaged binaries:$/,$d')
+release_body_suffix=$(echo "$response" | jq -r '.body' | sed '1,/^Packaged binaries:$/d')
 
 while IFS= read -r line
 do
-  version="${line/v/}"
+  if [[ $new_version == "${line/v/}" ]]; then
+    continue
+  fi
 
   offline_release_body=$(
-     curl "https://api.github.com/repos/${OFFLINE_RELEASE}/releases/tags/${version}" \
+     curl "https://api.github.com/repos/${OFFLINE_RELEASE}/releases/tags/${line/v/}" \
        --silent \
        --location \
        --header "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
@@ -36,25 +52,26 @@ do
    )
 
   if [[ "${offline_release_body}" == "null" ]]; then
-    release_body+=$(
-      curl "https://api.github.com/repos/${BUILDPACK_REPO}/releases/tags/v${version}" \
+    missed_changelog=$(
+      curl "https://api.github.com/repos/${BUILDPACK_REPO}/releases/tags/${line}" \
         --silent \
         --location \
         --header "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
       | jq -r '.body' | sed '/^Packaged binaries:$/,$d'
     )
 
-    if [[ "${release_body}" == "null" ]]; then
+    if [[ "${changelog}" == "null" ]]; then
       echo "task error: Error retrieving release notes from github.com/${BUILDPACK_REPO}" >&2
       exit 1
     fi
 
+    changelog+="\n\n$missed_changelog"
   else
      break
   fi
-
 done < <(git tag -l --sort=-version:refname "v*")
 
+release_body="$changelog$release_body_suffix"
 popd
 
 echo -e "${release_body}" > release-body/body
