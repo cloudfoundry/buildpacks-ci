78a65deb39a997fdcf40f1a00ea8cb3a671c871f
Tim Hitchener <thitch97@users.noreply.github.com>
2023-10-26 18:41:37 -0400

    Combine release notes (#323)
    * Add logic for hoisting missed release notes into most recent release
    
    * check for existence of params; fix typo
    
    * Parse out changelog
    
    * Polish changelog creation
    
    * Add buildpack to list of task inputs

--- tasks/get-buildpack-github-release-notes/run.sh
+++ tasks/get-buildpack-github-release-notes/run.sh
@@ -8,22 +8,70 @@ if [ -z "${BUILDPACK_REPO}" ]; then
   exit 1
 fi
 
-version=$(cat version/version)
-if [ -z "${version}" ]; then
+if [ -z "${OFFLINE_RELEASE}" ]; then
+  # e.g. "pivotal-cf/go-offline-buildpack-release"
+  echo "task error: OFFLINE_RELEASE not set" >&2
+  exit 1
+fi
+
+if [ -z "${GITHUB_ACCESS_TOKEN}" ]; then
+  echo "task error: GITHUB_ACCESS_TOKEN not set" >&2
+  exit 1
+fi
+
+new_version=$(cat version/version)
+if [ -z "${new_version}" ]; then
   echo "task error: can't read version from version/version" >&2
   exit 1
 fi
 
-release_body=$(
-  curl "https://api.github.com/repos/${BUILDPACK_REPO}/releases/tags/v${version}" \
-    --silent \
-    --location \
-  | jq -r '.body'
+pushd buildpack
+
+response=$(
+  curl "https://api.github.com/repos/${BUILDPACK_REPO}/releases/tags/v${new_version}" \
+        --silent \
+        --location \
+        --header "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
 )
 
-if [[ "${release_body}" == "null" ]]; then
-  echo "task error: Error retrieving release notes from github.com/${BUILDPACK_REPO}" >&2
-  exit 1
-fi
+changelog=$( echo "$response" | jq -r '.body' | sed '/^Packaged binaries:$/,$d')
+release_body_suffix=$(echo "$response" | jq -r '.body' | sed '1,/^Packaged binaries:$/d')
+
+while IFS= read -r line
+do
+  if [[ $new_version == "${line/v/}" ]]; then
+    continue
+  fi
+
+  offline_release_body=$(
+     curl "https://api.github.com/repos/${OFFLINE_RELEASE}/releases/tags/${line/v/}" \
+       --silent \
+       --location \
+       --header "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
+     | jq -r '.body'
+   )
+
+  if [[ "${offline_release_body}" == "null" ]]; then
+    missed_changelog=$(
+      curl "https://api.github.com/repos/${BUILDPACK_REPO}/releases/tags/${line}" \
+        --silent \
+        --location \
+        --header "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
+      | jq -r '.body' | sed '/^Packaged binaries:$/,$d'
+    )
+
+    if [[ "${changelog}" == "null" ]]; then
+      echo "task error: Error retrieving release notes from github.com/${BUILDPACK_REPO}" >&2
+      exit 1
+    fi
+
+    changelog+="\n\n$missed_changelog"
+  else
+     break
+  fi
+done < <(git tag -l --sort=-version:refname "v*")
+
+release_body="$changelog$release_body_suffix"
+popd
 
 echo -e "${release_body}" > release-body/body
