5c1678a1e50f13b374bcc0598bfc4617c247deb6
Tim Hitchener <thitchener@vmware.com>
2023-10-26 13:58:15 +0000

    check for existence of params; fix typo

--- tasks/get-buildpack-github-release-notes/run.sh
+++ tasks/get-buildpack-github-release-notes/run.sh
@@ -8,6 +8,17 @@ if [ -z "${BUILDPACK_REPO}" ]; then
   exit 1
 fi
 
+if [ -z "${OFFLINE_RELEASE}" ]; then
+  # e.g. "pivotal-cf/go-offline-buildpack-release"
+  echo "task error: OFFLINE_RELEASE not set" >&2
+  exit 1
+fi
+
+if [ -z "${GITHUB_ACCESS_TOKEN}" ]; then
+  echo "task error: GITHUB_ACCESS_TOKEN not set" >&2
+  exit 1
+fi
+
 version=$(cat version/version)
 if [ -z "${version}" ]; then
   echo "task error: can't read version from version/version" >&2
@@ -19,8 +30,8 @@ pushd buildpack
 while IFS= read -r line
 do
    offline_release=$(
-     curl "https://api.github.com/repos/${BUILDPACK_REPO}/releases/tags/v${line}" \
-     --header "Authorization: Bearer GITHUB_ACCESS_TOKEN" \
+     curl "https://api.github.com/repos/${OFFLINE_RELEASE}/releases/tags/v${line}" \
+     --header "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
      | jq -r .
    )
 
