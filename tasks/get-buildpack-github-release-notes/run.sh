e506fe8b4711212808c35b280530c5376b6d440a
Tim Hitchener <thitchener@vmware.com>
2023-10-26 15:51:45 +0000

    Parse out changelog

--- tasks/get-buildpack-github-release-notes/run.sh
+++ tasks/get-buildpack-github-release-notes/run.sh
@@ -19,39 +19,42 @@ if [ -z "${GITHUB_ACCESS_TOKEN}" ]; then
   exit 1
 fi
 
-version=$(cat version/version)
-if [ -z "${version}" ]; then
-  echo "task error: can't read version from version/version" >&2
-  exit 1
-fi
-
 pushd buildpack
+
+release_body=''
 
 while IFS= read -r line
 do
-   offline_release=$(
-     curl "https://api.github.com/repos/${OFFLINE_RELEASE}/releases/tags/v${line}" \
-     --header "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
-     | jq -r .
+  version="${line/v/}"
+
+  offline_release_body=$(
+     curl "https://api.github.com/repos/${OFFLINE_RELEASE}/releases/tags/${version}" \
+       --silent \
+       --location \
+       --header "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
+     | jq -r '.body'
    )
 
-  if [[ "${offline_release}" == "null" ]]; then
+  if [[ "${offline_release_body}" == "null" ]]; then
     release_body+=$(
-      curl "https://api.github.com/repos/${BUILDPACK_REPO}/releases/tags/v${line}" \
+      curl "https://api.github.com/repos/${BUILDPACK_REPO}/releases/tags/v${version}" \
         --silent \
         --location \
-      | jq -r '.body'
+        --header "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
+      | jq -r '.body' | sed '/^Packaged binaries:$/,$d'
     )
+
+    if [[ "${release_body}" == "null" ]]; then
+      echo "task error: Error retrieving release notes from github.com/${BUILDPACK_REPO}" >&2
+      exit 1
+    fi
+
   else
      break
   fi
+
 done < <(git tag -l --sort=-version:refname "v*")
 
 popd
-
-if [[ "${release_body}" == "null" ]]; then
-  echo "task error: Error retrieving release notes from github.com/${BUILDPACK_REPO}" >&2
-  exit 1
-fi
 
 echo -e "${release_body}" > release-body/body
