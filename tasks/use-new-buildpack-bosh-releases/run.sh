a241bd7c44b58735678fabef320c3a6869e48ade
Arjun Sreedharan <asreedharan@vmware.com>
2024-08-13 19:49:13 +0000

    tasks/use-new-bp-bsh-rls: support private blobstore

--- tasks/use-new-buildpack-bosh-releases/run.sh
+++ tasks/use-new-buildpack-bosh-releases/run.sh
@@ -4,10 +4,31 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+write_private_yml() {
+  if [ -z "${AWS_ACCESS_KEY_ID:-}" ]; then
+    return
+  fi
+
+  cat >config/private.yml <<-EOF
+---
+blobstore:
+  options:
+    access_key_id: ${AWS_ACCESS_KEY_ID}
+    secret_access_key: ${AWS_SECRET_ACCESS_KEY}
+EOF
+
+if [ -n "${AWS_ASSUME_ROLE_ARN:-}" ]; then
+  cat >>config/private.yml <<-EOF
+    assume_role_arn: ${AWS_ASSUME_ROLE_ARN}
+EOF
+fi
+}
+
 version=$(date +%s)
 
 for language in binary dotnet-core go java nodejs php python ruby staticfile; do
   pushd "$language-buildpack-release"
+    write_private_yml
     bosh2 create-release --tarball "../built-buildpacks-artifacts/$language-buildpack-$version.tgz" --name "$language-buildpack" --version "$version"
   popd
 
