3ca35ac352887f05e679c0cd4eb85a91513db172
Christopher Banck <vchrisb@users.noreply.github.com>
2024-09-06 14:29:38 +0200

    build openresty with http2

--- tasks/build-binary-new/builder.rb
+++ tasks/build-binary-new/builder.rb
@@ -440,6 +440,7 @@ module DependencyBuild
               '-j2',
               '--error-log-path=stderr',
               '--with-http_ssl_module',
+              '--with-http_v2_module',
               '--with-http_realip_module',
               '--with-http_gunzip_module',
               '--with-http_gzip_static_module',
