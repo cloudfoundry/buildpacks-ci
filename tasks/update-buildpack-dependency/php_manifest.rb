11fdde63fddb86e7e66f94a85221129975721ecb
Arjun Sreedharan <asreedharan@vmware.com>
2025-04-25 17:19:48 +0000

    Fix nginx default update in PHP BP
    - We need to look at `manifest_name` (e.g. nginx), not `source_name`
      (e.g. nginx-static) when updating the manifest as the former refers to
      the name of the dependency in the manifest.
    - Only update the default value when the bump is for an NGINX mainline
      version.

--- tasks/update-buildpack-dependency/php_manifest.rb
+++ tasks/update-buildpack-dependency/php_manifest.rb
@@ -1,8 +1,16 @@
 class PHPManifest
-  def self.update_defaults(manifest, source_name, resource_version)
+  def self.update_defaults(manifest, target_name, resource_version)
     manifest['default_versions'].map do |default|
-      if default['name'] == source_name
-        default['version'] = resource_version
+      if default['name'] == target_name
+        if default['name'] == 'nginx'
+          # For nginx, we update defaults only when a mainline version updates.
+          # Mainline versions are identified by having an odd number as the minor (e.g. 1.27.2)
+          if Gem::Version.new(resource_version).segments[1].odd?
+            default['version'] = resource_version
+          end
+        else
+          default['version'] = resource_version
+        end
       end
       default
     end
