583070d1b562b9d7506115bd45a577b43f7bb91e
ramonskie <ramonmakkelie@gmail.com>
2025-10-16 11:40:36 +0200

    cleaning up some functions that doesnt make sense anymore since we only support cgroupv2

--- util/docker.sh
+++ util/docker.sh
@@ -19,51 +19,27 @@ function util::docker::start() {
     return
   fi
 
-  util::docker::privileges::elevate
-  util::docker::scratch::allocate
+  if [ ! -e /sys/fs/cgroup/cgroup.controllers ]; then
+    util::print::error "[docker] cgroup v1 detected but no longer supported"
+    util::print::error "[docker] This infrastructure requires cgroup v2 (Ubuntu 22.04+/Kubernetes 1.25+)"
+    exit 1
+  fi
+
+  if [[ "${SKIP_PRIVILEGED}" != "false" ]]; then
+    util::print::info "[docker] * elevating privileges"
+
+    if grep '/proc/sys\s\+\w\+\s\+ro,' /proc/mounts >/dev/null; then
+      mount -o remount,rw /proc/sys
+    fi
+  fi
+
+  mkdir -p "${SCRATCH_DIR}"
   util::docker::daemon::start
 }
 
 function util::docker::stop() {
   util::print::title "[docker] shutdown sequence"
-
   util::docker::daemon::stop
-  util::docker::scratch::deallocate
-}
-
-function util::docker::privileges::elevate() {
-  if [[ "${SKIP_PRIVILEGED}" != "false" ]]; then
-    return
-  fi
-
-  util::print::info "[docker] * elevating privileges"
-  util::docker::cgroups::sanitize
-
-  # check for /proc/sys being mounted readonly, as systemd does
-  if grep '/proc/sys\s\+\w\+\s\+ro,' /proc/mounts >/dev/null; then
-    mount -o remount,rw /proc/sys
-  fi
-}
-
-function util::docker::cgroups::sanitize() {
-  if [ -e /sys/fs/cgroup/cgroup.controllers ]; then
-    util::print::info "[docker]   * cgroup v2 detected"
-    return 0
-  fi
-
-  util::print::error "[docker]   * cgroup v1 detected but no longer supported"
-  util::print::error "[docker]   * This infrastructure requires cgroup v2 (Ubuntu 22.04+/Kubernetes 1.25+)"
-  exit 1
-}
-
-function util::docker::scratch::allocate() {
-  util::print::info "[docker] * allocating scratch disk"
-
-  mkdir -p "${SCRATCH_DIR}"
-}
-
-function util::docker::scratch::deallocate() {
-  util::print::info "[docker] * deallocating scratch disk"
 }
 
 function util::docker::daemon::start() {
