15479ef3e2be93f6161ed12ed55d1f1e53a05f3f
ivanovac <ivanovac@gmail.com>
2026-01-17 00:09:22 +0200

    Fix Python depwatcher to scrape MD5 from correct column

--- dockerfiles/depwatcher/src/depwatcher/python.cr
+++ dockerfiles/depwatcher/src/depwatcher/python.cr
@@ -35,7 +35,8 @@ module Depwatcher
       tr = a.xpath("./ancestor::tr")
       raise "Could not parse python release (tr) website" unless tr.is_a?(XML::NodeSet)
       tr = tr.first
-      md5_digest = tr.xpath("./td[position()=4]")
+      # Column 7 is MD5 checksum (was column 4 which was file size)
+      md5_digest = tr.xpath("./td[position()=7]")
       raise "Could not parse python release (md5_digest) website" unless md5_digest.is_a?(XML::NodeSet)
 
       Release.new(ref, a["href"], md5_digest.first.text, get_sha256(a["href"]))
