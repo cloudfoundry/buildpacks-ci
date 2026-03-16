6e6194a29e5878be0d34e8bf5ef1f53e817e3005
ivanovac <ivanovac@gmail.com>
2026-03-16 13:48:21 +0200

    feat: Add cflinuxfs5 rootfs pipeline
    - Copy cflinuxfs4.yml pipeline to cflinuxfs5.yml
    - Create check-for-new-rootfs-cves-cflinuxfs5 task
    - Update all references from cflinuxfs4 to cflinuxfs5

---
+++ tasks/check-for-new-rootfs-cves-cflinuxfs5/run.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+stack = 'cflinuxfs5'
+
+buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+stacks_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', stack.gsub(/m$/, '')))
+cves_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'output-new-cves', 'new-cve-notifications'))
+
+require "#{buildpacks_ci_dir}/lib/rootfs-cve-notifier"
+
+cve_notifier = RootFSCVENotifier.new(cves_dir, stacks_dir)
+
+cve_notifier.run!(stack, 'Ubuntu 22.04', 'ubuntu22.04', [])
